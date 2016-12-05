9ac9f4cd57809416a709cadb09f29cfa8e22bf95
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-05 16:24:21 +0100

    Add pytest, pytest-runner and pyserial
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='labgrid',
       version='0.0.1',
       description='labgrid: lab hardware and software contol layer',
       author='Rouven Czerwinski and Jan Luebbe',
       url='https://github.com/labgrid-project',
+      setup_requires=[
+        'pytest-runner',
+      ],
+      tests_require=[
+        'pytest',
+        'mock-import',
+      ],
+      install_requires=[
+        "pyserial"
+      ],
       packages=['labgrid', 'labgrid.resource', 'labgrid.protocol', 'labgrid.external'],
      )

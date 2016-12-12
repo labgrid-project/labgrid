368e125a37e3dcbd2bc00fbab25a878c35f25de5
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-12 15:11:03 +0100

    Extend timeout
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/util/timeout.py
+++ labgrid/util/timeout.py
@@ -3,4 +3,6 @@ import attr
 
 @attr.s
 class PtxTimeout(object):
+    """Saves a class specific Timeout"""
     cls = attr.ib()
+    timeout = attr.ib(default=120, validator=attr.validators.instance_of(int))

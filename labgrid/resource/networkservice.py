5b999d43be2bdab08cabe2f3aa5422a1e72e7eaf
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-12 15:10:02 +0100

    Save hostname in networkservice
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/resource/networkservice.py
+++ labgrid/resource/networkservice.py
@@ -7,6 +7,7 @@ class NetworkResource(InfoResource):
     target = attr.ib()
     addresses = attr.ib(default={},
                         validator=attr.validators.instance_of(dict))
+    hostname = attr.ib(default="", validator=attr.validators.instance_of(str))
 
     def __attrs_post_init__(self):
         pass
@@ -14,3 +15,7 @@ class NetworkResource(InfoResource):
     def get_if_addr(self, adr):
         """Returns the address for the specified interface"""
         return self.addresses[adr] #pylint: disable=unsubscriptable-object
+
+    def get_hostname(self):
+        """Returns the address for the specified interface"""
+        return self.hostname #pylint: disable=unsubscriptable-object

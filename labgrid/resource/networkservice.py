2bfe4e1d9ce3edaf88e1a4b14b61b8f57c2227d1
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-13 16:28:53 +0100

    NetworkService: Only one address
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/resource/networkservice.py
+++ labgrid/resource/networkservice.py
@@ -6,14 +6,4 @@ from .resource import InfoResource
 @target_factory.reg_resource
 @attr.s
 class NetworkService(InfoResource):
-    addresses = attr.ib(default={},
-                        validator=attr.validators.instance_of(dict))
-    hostname = attr.ib(default="", validator=attr.validators.instance_of(str))
-
-    def get_if_addr(self, adr):
-        """Returns the address for the specified interface"""
-        return self.addresses[adr] #pylint: disable=unsubscriptable-object
-
-    def get_hostname(self):
-        """Returns the address for the specified interface"""
-        return self.hostname #pylint: disable=unsubscriptable-object
+    address = attr.ib(validator=attr.validators.instance_of(str))

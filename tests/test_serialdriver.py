48b36ee4ada0f0f28502766881d537331229b968
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-13 12:42:00 +0100

    Test read, write and close
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- tests/test_serialdriver.py
+++ tests/test_serialdriver.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import Mock, MagicMock
 import pytest
 import serial
 
@@ -10,8 +10,35 @@ class TestSerialDriver:
             SerialDriver(target)
 
     def test_instanziation(self, serial_port, monkeypatch):
-        serial_mock = MagicMock
+        serial_mock = Mock()
         monkeypatch.setattr(serial, 'Serial', serial_mock)
         s = SerialDriver(serial_port)
         assert(isinstance(s, SerialDriver))
         assert(serial_port.drivers[0] == s)
+
+    def test_write(self, monkeypatch, serial_port):
+        serial_mock = Mock()
+        serial_mock.write = MagicMock()
+        monkeypatch.setattr(serial, 'Serial', serial_mock)
+        s = SerialDriver(serial_port)
+        s.serial = serial_mock
+        s.write(b"testdata")
+        serial_mock.write.assert_called_with(b"testdata")
+
+    def test_read(self, monkeypatch, serial_port):
+        serial_mock = Mock()
+        serial_mock.read = MagicMock()
+        monkeypatch.setattr(serial, 'Serial', serial_mock)
+        s = SerialDriver(serial_port)
+        s.serial = serial_mock
+        s.read()
+        assert(serial_mock.read.called)
+
+    def test_close(self, monkeypatch, serial_port):
+        serial_mock = Mock()
+        serial_mock.close = MagicMock()
+        monkeypatch.setattr(serial, 'Serial', serial_mock)
+        s = SerialDriver(serial_port)
+        s.serial = serial_mock
+        s.close()
+        assert(serial_mock.close.called)

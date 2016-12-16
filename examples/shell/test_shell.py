b3df7405538f3d1ec6e6b70356d9e06b81fdc097
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2016-12-16 15:34:26 +0100

    Example: Set prompt, run "ls /"
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- examples/shell/test_shell.py
+++ examples/shell/test_shell.py
@@ -4,4 +4,4 @@ from labgrid.protocol import ConsoleProtocol, CommandProtocol
 def test_shell(target):
      console = target.get_driver(ConsoleProtocol)
      command = target.get_driver(CommandProtocol)
-     print(command.run('ls'))
+     print(command.run('ls /'))

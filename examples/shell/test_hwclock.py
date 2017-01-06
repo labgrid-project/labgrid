2a25656c48a0c40a3aa5f1c3664e393ba1d50ed2
Jan Luebbe <jlu@pengutronix.de>
2017-01-06 12:26:20 +0100

    examples: add hwclock tests
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

---
+++ examples/shell/test_hwclock.py
@@ -0,0 +1,34 @@
+import time
+from datetime import datetime
+
+from labgrid.protocol import CommandProtocol
+
+
+def test_hwclock_rate(target):
+    """Test that the hardware clock rate is not too inaccurate."""
+    command = target.get_driver(CommandProtocol)
+    result = command.run_check('hwclock -c | head -n 3')
+    hw_time, sys_time, freq_offset_ppm, tick = result[-1].strip().split()
+    assert abs(int(freq_offset_ppm)) < 1000
+
+
+def test_hwclock_value(target):
+    """Test that the hardware clock has the correct time.
+    
+    If the time is wrong, it is set once and tested again.
+    """
+    command = target.get_driver(CommandProtocol)
+
+    def get_time():
+        result = command.run_check('hwclock --utc --show')[0].strip()
+        return datetime.strptime(result, '%Y-%m-%d %H:%M:%S.%f+0:00')
+
+    def set_time(time):
+        time = time.strftime('%Y-%m-%d %H:%M:%S.%f+0:00')
+        command.run_check('hwclock --utc --set --date "{}"'.format(time))
+
+    offset = abs((get_time() - datetime.utcnow()).total_seconds())
+    if offset > 60:
+        set_time(datetime.utcnow())
+        offset = abs((get_time() - datetime.utcnow()).total_seconds())
+    assert offset < 60

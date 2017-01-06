e51bab18fcdf2194c01c5e7ace33cff03faf68e8
Jan Luebbe <jlu@pengutronix.de>
2017-01-06 16:06:47 +0100

    driver: export ExecutionError
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/driver/__init__.py
+++ labgrid/driver/__init__.py
@@ -3,6 +3,6 @@ from .serialdriver import SerialDriver
 from .shelldriver import ShellDriver
 from .sshdriver import SSHDriver
 from .externalconsoledriver import ExternalConsoleDriver
-from .exception import CleanUpError
+from .exception import CleanUpError, ExecutionError
 from .powerdriver import ManualPowerDriver, ExternalPowerDriver
 from .common import Driver

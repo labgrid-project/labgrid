ac3027995577677667bc000552fa790cbef32e96
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-01-11 14:21:46 +0100

    USBStick: Fix test
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- tests/test_usbemulator.py
+++ tests/test_usbemulator.py
@@ -1,11 +1,12 @@
 import pytest
 
-from labgrid.driver.fake import FakeCommandDriver
+from labgrid.driver.fake import FakeCommandDriver, FakeFileTransferDriver
 from labgrid.external import USBStick
 
 
 class TestUSBStick:
     def test_create(self, target):
         d = FakeCommandDriver(target)
-        u = USBStick(target, 'imagename')
+        f = FakeFileTransferDriver(target)
+        u = USBStick(target, 'imagepath', 'imagename')
         assert (isinstance(u, USBStick))

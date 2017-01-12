3a888ffe6d90f66077ea3b1921e9084add555ae3
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-01-12 14:34:41 +0100

    Examples: Separate UBootStrategy and BareboxStrategy
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ examples/strategy/test_uboot_strategy.py
@@ -0,0 +1,54 @@
+import pytest
+import logging
+
+from labgrid.driver import UBootDriver, ShellDriver
+from labgrid.protocol import CommandProtocol
+from labgrid.strategy import UBootStrategy
+
+
+@pytest.fixture()
+def strategy(target):
+    return target.get_driver(UBootStrategy)
+
+
+@pytest.fixture(scope="function")
+def in_bootloader(strategy, capsys):
+    with capsys.disabled():
+        strategy.transition("uboot")
+
+
+@pytest.fixture(scope="function")
+def in_shell(strategy, capsys):
+    with capsys.disabled():
+        strategy.transition("shell")
+
+
+def test_uboot(target, in_bootloader):
+    #command = target.get_driver(CommandProtocol)
+    command = target.get_driver(UBootDriver)
+
+    stdout, stderr, returncode = command.run('version')
+    assert returncode == 0
+    assert len(stdout) > 0
+    assert len(stderr) == 0
+    assert 'U-Boot' in '\n'.join(stdout)
+
+
+def test_shell(target, in_shell):
+    #command = target.get_driver(CommandProtocol)
+    command = target.get_driver(ShellDriver)
+    stdout, stderr, returncode = command.run('cat /proc/version')
+    assert returncode == 0
+    assert len(stdout) > 0
+    assert len(stderr) == 0
+    assert 'Linux' in stdout[0]
+
+
+def test_uboot_2(target, in_bootloader):
+    command = target.get_driver(UBootDriver)
+
+    stdout, stderr, returncode = command.run('version')
+    assert returncode == 0
+    assert len(stdout) > 0
+    assert len(stderr) == 0
+    assert 'U-Boot' in '\n'.join(stdout)

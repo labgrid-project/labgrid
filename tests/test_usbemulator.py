82faf699445326e999020242b3b685411054a7ab
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-01-17 16:02:58 +0100

    tests: activate fake drivers for USBStick
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- tests/test_usbemulator.py
+++ tests/test_usbemulator.py
@@ -8,5 +8,7 @@ class TestUSBStick:
     def test_create(self, target):
         d = FakeCommandDriver(target)
         f = FakeFileTransferDriver(target)
+        target.activate(d)
+        target.activate(f)
         u = USBStick(target, 'imagepath', 'imagename')
         assert (isinstance(u, USBStick))

6a6343db55f2402b1175029d2cad2a12097d1f44
Jan Luebbe <jlu@pengutronix.de>
2017-02-24 17:38:43 +0100

    tests: add simple startup tests for the remote functionality
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

---
+++ tests/test_remote.py
@@ -0,0 +1,20 @@
+import pytest
+import pexpect
+
+pytest.importorskip('autobahn')
+
+def test_client_help():
+    with pexpect.spawn('python -m labgrid.remote.client --help') as spawn:
+        spawn.expect('usage')
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0
+        assert spawn.signalstatus is None
+
+def test_exporter_help():
+    with pexpect.spawn('python -m labgrid.remote.exporter --help') as spawn:
+        spawn.expect('usage')
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0
+        assert spawn.signalstatus is None

11fcbbad466ffc3839db84ed8aec45f992f39596
Jan Luebbe <jlu@pengutronix.de>
2017-03-01 12:16:46 +0100

    test_target: move tests functions to module scope
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- tests/test_target.py
+++ tests/test_target.py
@@ -4,29 +4,34 @@ from labgrid import Target
 from labgrid.exceptions import NoDriverFoundError, NoResourceFoundError
 
 
-class TestTarget:
-    def test_instanziation(self):
-        t = Target("name")
-        assert (isinstance(t, Target))
 
-    def test_get_resource(self, target):
-        class a():
-            pass
+# test basic construction
+def test_instanziation():
+    t = Target("name")
+    assert (isinstance(t, Target))
 
-        target.resources.append(a())
-        assert isinstance(target.get_resource(a), a)
 
-    def test_get_driver(self, target):
-        class a():
-            pass
+def test_get_resource(target):
+    class a():
+        pass
 
-        target.drivers.append(a())
-        assert isinstance(target.get_driver(a), a)
+    target.resources.append(a())
+    assert isinstance(target.get_resource(a), a)
 
-    def test_no_resource(self, target):
-        with pytest.raises(NoResourceFoundError):
-            target.get_resource(Target)
 
-    def test_no_driver(self, target):
-        with pytest.raises(NoDriverFoundError):
-            target.get_driver(Target)
+def test_get_driver(target):
+    class a():
+        pass
+
+    target.drivers.append(a())
+    assert isinstance(target.get_driver(a), a)
+
+
+def test_no_resource(target):
+    with pytest.raises(NoResourceFoundError):
+        target.get_resource(Target)
+
+
+def test_no_driver(target):
+    with pytest.raises(NoDriverFoundError):
+        target.get_driver(Target)

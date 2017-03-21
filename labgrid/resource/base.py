d04e77bfbb10850f53815a25047c6c535755eb00
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-03-21 12:33:17 +0100

    resource/base: add docstrings
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/resource/base.py
+++ labgrid/resource/base.py
@@ -5,10 +5,19 @@ from .common import Resource
 
 @attr.s
 class SerialPort(Resource):
+    """The basic SerialPort describes port and speed
+
+    Args:
+        port (str): port to connect to
+        speed (int): speed of the port, defaults to 115200"""
     port = attr.ib(default=None)
     speed = attr.ib(default=115200, validator=attr.validators.instance_of(int))
 
 
 @attr.s
 class EthernetInterface(Resource):
+    """The basic EthernetInterface contains an interfacename
+
+    Args:
+        ifname (str): name of the interface"""
     ifname = attr.ib(default=None)

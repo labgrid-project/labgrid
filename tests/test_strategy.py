8c5e5ab27c5122a60081260a764c1783546deff9
Jan Luebbe <jlu@pengutronix.de>
2017-03-22 18:14:03 +0100

    tests: test strategies with fake drivers
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -1,15 +1,36 @@
 import pytest
 
-from labgrid.strategy import BareboxStrategy, UBootStrategy
+from labgrid.binding import BindingState
+from labgrid.driver import BareboxDriver, UBootDriver, ShellDriver
+from labgrid.driver.fake import FakeConsoleDriver, FakePowerDriver
+from labgrid.protocol import (CommandProtocol, ConsoleProtocol,
+                              LinuxBootProtocol)
+from labgrid.strategy import Strategy, BareboxStrategy, UBootStrategy
 from labgrid.exceptions import NoDriverFoundError
 
 
 def test_create_barebox(target):
-    # we currently don't have the necessary mocks
-    with pytest.raises(NoDriverFoundError):
-        s = BareboxStrategy(target)
+    console = FakeConsoleDriver(target)
+    power = FakePowerDriver(target)
+    barebox = BareboxDriver(target)
+    shell = ShellDriver(target, prompt='root@dummy', login_prompt='login:', username='root')
+    s = BareboxStrategy(target)
+
+    assert isinstance(s, Strategy)
+    assert target.get_driver(BareboxStrategy) is s
+    assert target.get_driver(Strategy) is s
+
+    assert s.state is BindingState.bound
 
 def test_create_uboot(target):
-    # we currently don't have the necessary mocks
-    with pytest.raises(NoDriverFoundError):
-        s = UBootStrategy(target)
+    console = FakeConsoleDriver(target)
+    power = FakePowerDriver(target)
+    barebox = UBootDriver(target)
+    shell = ShellDriver(target, prompt='root@dummy', login_prompt='login:', username='root')
+    s = UBootStrategy(target)
+
+    assert isinstance(s, Strategy)
+    assert target.get_driver(UBootStrategy) is s
+    assert target.get_driver(Strategy) is s
+
+    assert s.state is BindingState.bound

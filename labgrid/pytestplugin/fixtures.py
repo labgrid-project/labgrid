e331e9f9f7c02999259c0d95c6b5b7412e5702e8
Jan Luebbe <jlu@pengutronix.de>
2017-04-05 14:27:18 +0200

    pytestplugin: add --lg- option group and support setting the coordinator
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/pytestplugin/fixtures.py
+++ labgrid/pytestplugin/fixtures.py
@@ -12,7 +12,20 @@ def pytest_addoption(parser):
         '--env-config',
         action='store',
         dest='env_config',
+        help='labgrid environment config file (deprecated).'
+    )
+    group.addoption(
+        '--lg-env',
+        action='store',
+        dest='lg_env',
         help='labgrid environment config file.'
+    )
+    group.addoption(
+        '--lg-coordinator',
+        action='store',
+        dest='lg_coordinator',
+        metavar='CROSSBAR_URL',
+        help='labgrid coordinator websocket URL.'
     )
 
 
@@ -22,9 +35,23 @@ def env(request):
     It contains the targets contained in the configuration file.
     """
     env_config = request.config.option.env_config
-    if env_config is None:
-        pytest.skip("missing environemnt config (--env-config)")
-    env = Environment(config_file=request.config.option.env_config, )
+    lg_env = request.config.option.lg_env
+    lg_coordinator = request.config.option.lg_coordinator
+
+    if lg_env is None:
+        if env_config is not None:
+            request.config.warn(
+                'LG-C1',
+                "deprecated option --env-config (use --lg-env instead)",
+                __file__
+            )
+            lg_env = env_config
+
+    if lg_env is None:
+        pytest.skip("missing environment config (use --lg-env)")
+    env = Environment(config_file=lg_env)
+    if lg_coordinator is not None:
+        env.config.set_option('crossbar_url', lg_coordinator)
     yield env
     env.cleanup()
 

6a157520b809f38b896bbc1f057d65428382b2ff
Jan Luebbe <jlu@pengutronix.de>
2017-08-22 18:32:19 +0200

    util/yaml: support dumping with OrderedDicts
    This allows us to write yaml files back out with a nice layout.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/util/__init__.py
+++ labgrid/util/__init__.py
@@ -2,4 +2,4 @@ from .dict import diff_dict, flat_dict
 from .expect import PtxExpect
 from .timeout import Timeout
 from .marker import gen_marker
-from .yaml import load
+from .yaml import load, dump
--- labgrid/util/yaml.py
+++ labgrid/util/yaml.py
@@ -8,8 +8,17 @@ yaml.SafeLoader.add_constructor(
     yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, _dict_constructor
 )
 
+def _dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+yaml.SafeDumper.add_representer(OrderedDict, _dict_representer)
+
+
 # use SafeLoader
 loader = yaml.SafeLoader
+dumper = yaml.SafeDumper
 
-def load(file):
-    return yaml.load(file, loader)
+def load(stream):
+    return yaml.load(stream, Loader=loader)
+
+def dump(data, stream=None):
+    return yaml.dump(data, stream, Dumper=dumper, default_flow_style=False)

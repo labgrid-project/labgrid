3390a168ebf9dbaa6191f66ed1ae7d64ea75a16b
Jan Luebbe <jlu@pengutronix.de>
2017-08-29 15:25:46 +0200

    util/yaml: store start and end marks for literal multiline strings
    This will allow us to produce better error messages.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/util/yaml.py
+++ labgrid/util/yaml.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import OrderedDict, UserString
 
 import yaml
 
@@ -11,6 +11,19 @@ yaml.SafeLoader.add_constructor(
 def _dict_representer(dumper, data):
     return dumper.represent_dict(data.items())
 yaml.SafeDumper.add_representer(OrderedDict, _dict_representer)
+
+
+def _str_constructor(loader, node):
+    # store location of multiline string
+    if node.style != '|':
+        return loader.construct_scalar(node)
+    obj = UserString(loader.construct_scalar(node))
+    obj.start_mark = node.start_mark
+    obj.end_mark = node.end_mark
+    return obj
+yaml.SafeLoader.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_SCALAR_TAG, _str_constructor
+)
 
 
 # use SafeLoader

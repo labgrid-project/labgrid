e8c1e1c1aa16bfdc740f0f6c3d9e20ffeb0bf5b0
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-08-31 09:34:36 +0200

    resource: add sigrok resources
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ labgrid/resource/sigrok.py
@@ -0,0 +1,17 @@
+import attr
+
+from ..factory import target_factory
+from .common import Resource
+
+@target_factory.reg_resource
+@attr.s(cmp=False)
+class SigrokDevice(Resource):
+    """The SigrokDevice describes an attached sigrok device with driver and
+    channel mapping
+
+    Args:
+        driver (str): driver to use with sigrok
+        channels (str): a sigrok channel mapping as desribed in the sigrok-cli man page
+    """
+    driver = attr.ib(default="demo")
+    channels = attr.ib(default=None, validator=attr.validators.instance_of(str))

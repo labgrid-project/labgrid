37eb5f783d10f18b5772126a28eb04bee65e0940
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2017-09-05 12:14:47 +0200

    tests: add tests for sigrok functionality
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ tests/test_sigrok.py
@@ -0,0 +1,37 @@
+import pytest
+import os.path
+from time import sleep
+from shutil import which
+
+from labgrid.resource.udev import SigrokUSBDevice
+from labgrid.resource.sigrok import SigrokDevice
+from labgrid.driver.sigrokdriver import SigrokDriver
+
+
+pytestmark = pytest.mark.skipif(not which("sigrok-cli"),
+                              reason="sigrok not available")
+
+
+def test_sigrok_resource(target):
+    r = SigrokUSBDevice(target, name=None, match={"sys_name": "1-12"}, driver='fx2lafw', channels="D0,D1")
+
+
+def test_sigrok_driver(target):
+    r = SigrokDevice(target, name=None, driver='demo', channels="D0")
+    d = SigrokDriver(target, name=None)
+    target.activate(d)
+
+
+@pytest.mark.sigrokusb
+def test_sigrok_usb_driver(target, tmpdir):
+    r = SigrokUSBDevice(target, name=None, match={"ID_MODEL_ID": "3881", "ID_VENDOR_ID": "0925"}, driver='fx2lafw', channels="D0,D1")
+    d = SigrokDriver(target, name=None)
+    target.activate(d)
+    record = tmpdir.join("output.sr")
+    d.capture(record)
+    sleep(5)
+    samples = d.stop()
+    assert os.path.getsize(record) > 0
+    assert samples != None
+    assert list(samples[0].keys()) == ['time', 'D0', 'D1']
+    assert list(samples[-1].keys()) == ['time', 'D0', 'D1']

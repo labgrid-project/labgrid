284a9f069ab96d51e8a88108a910423999643a0e
Jan Luebbe <jlu@pengutronix.de>
2017-11-22 17:12:06 +0100

    tests: improve test coverage of steps
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- tests/test_step.py
+++ tests/test_step.py
@@ -34,3 +34,48 @@ def step_sleep(*, step):
 def test_timing():
     step = step_sleep()
     assert step.duration == approx(0.25, abs=1e-2)
+
+class A:
+    @step()
+    def method_step(self, foo, *, step):
+        return step
+
+    @step(args=['foo'])
+    def method_args_step(self, foo, *, step):
+        return step
+
+    @step(title='test-title')
+    def method_title_step(self, foo, *, step):
+        return step
+
+    @step(result=True)
+    def method_result_step(self, foo, *, step):
+        return step
+
+def test_method():
+    a = A()
+    step = a.method_step('foo')
+    assert step.title == 'method_step'
+    assert step.args is None
+    assert step.result is None
+
+def test_method_args():
+    a = A()
+    step = a.method_args_step('foo')
+    assert step.title == 'method_args_step'
+    assert step.args == {'foo': 'foo'}
+    assert step.result is None
+
+def test_method_title():
+    a = A()
+    step = a.method_title_step('foo')
+    assert step.title == 'test-title'
+    assert step.args is None
+    assert step.result is None
+
+def test_method_result():
+    a = A()
+    step = a.method_result_step('foo')
+    assert step.title == 'method_result_step'
+    assert step.args is None
+    assert step.result is step

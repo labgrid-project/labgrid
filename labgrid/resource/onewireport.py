aeb2fbd2355f0f4fa0c46b40220fe950e2df66c3
Jan Luebbe <jlu@pengutronix.de>
2017-12-01 12:40:49 +0100

    driver&resource: add support for inverted PIOs
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/resource/onewireport.py
+++ labgrid/resource/onewireport.py
@@ -11,6 +11,8 @@ class OneWirePIO(Resource):
 
     Args:
         host (str): hostname of the owserver e.g. localhost:4304
-        path (str): path to the port on the owserver e.g. 29.7D6913000000/PIO.0"""
+        path (str): path to the port on the owserver e.g. 29.7D6913000000/PIO.0
+        invert (bool): optional, whether the logic level is be inverted (active-low)"""
     host = attr.ib(validator=attr.validators.instance_of(str))
     path = attr.ib(validator=attr.validators.instance_of(str))
+    invert = attr.ib(default=False, validator=attr.validators.instance_of(bool))

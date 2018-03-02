1deea1c71728f50644ffd7b7e709577f43e1d4c2
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2018-03-02 12:28:55 +0100

    driver/multiple: move run_check function
    Move run_check to the mixin and remove the implementation from other drivers.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/driver/commandmixin.py
+++ labgrid/driver/commandmixin.py
@@ -22,3 +22,34 @@ class CommandMixin:
             sleep(sleepduration)
         if timeout.expired:
             raise ExecutionError("Wait timeout expired")
+
+    def _run_check(self, cmd: str, timeout=30):
+        """
+        Internal function which runs the specified command on the shell and
+        returns the output if successful, raises ExecutionError otherwise.
+
+        Args:
+            cmd (str): command to run on the shell
+
+        Returns:
+            List[str]: stdout of the executed command
+        """
+        stdout, stderr, exitcode = self.run(cmd, timeout=timeout)
+        if exitcode != 0:
+            raise ExecutionError(cmd, stdout, stderr)
+        return stdout
+
+    @Driver.check_active
+    def run_check(self, cmd: str, timeout=30):
+        """
+        External run_check function, only available if the driver is active.
+        Runs the supplied command and returns the stdout, raises an
+        ExecutionError otherwise.
+
+        Args:
+            cmd (str): command to run on the shell
+
+        Returns:
+            List[str]: stdout of the executed command
+        """
+        return self._run_check(cmd,timeout)

labgrid-project/labgrid

commit 533cec4c061d4fd83132856fbf0b37154292fea0

linting: do miscellaneous linting
Signed-off-by: Bastian Stender <bst@pengutronix.de>

labgrid/driver/networkusbstoragedriver.py
@@ -13,7 +13,14 @@ from .common import Driver, check_file
 @target_factory.reg_driver
 @attr.s(cmp=False)
 class NetworkUSBStorageDriver(Driver):
-    bindings = {"storage": {USBMassStorage, NetworkUSBMassStorage, USBSDMuxDevice, NetworkUSBSDMuxDevice}, }
+    bindings = {
+        "storage": {
+            USBMassStorage,
+            NetworkUSBMassStorage,
+            USBSDMuxDevice,
+            NetworkUSBSDMuxDevice
+        },
+    }
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
@@ -30,13 +37,18 @@ class NetworkUSBStorageDriver(Driver):
         filename = os.path.abspath(filename)
         check_file(filename, command_prefix=self.storage.command_prefix)
         self.logger.info("pwd: %s", os.getcwd())
+        args = (
+            "dd",
+            "if={}".format(filename),
+            "of={} status=progress bs=4M conv=fdatasync"
+            .format(self.storage.path)
+        )
         subprocess.check_call(
-          self.storage.command_prefix+["dd", "if=%s" % filename, "of=%s status=progress bs=4M conv=fdatasync" % self.storage.path]
+            self.storage.command_prefix + args
         )
 
     @step(result=True)
     def get_size(self):
-        size = subprocess.check_output(
-          self.storage.command_prefix+["cat", "/sys/class/block/%s/size" % self.storage.path[5:]]
-        )
+        args = ("cat", "/sys/class/block/{}/size" % self.storage.path[5:])
+        size = subprocess.check_output(self.storage.command_prefix + args)
         return int(size)

examples/hawkbit/test_hawkbit.py
@@ -3,13 +3,14 @@ import pytest
 
 from labgrid.external import HawkbitTestClient
 
+
 @pytest.fixture()
 def hawkbit():
     # Requires a hawkbit server instance to be running.
     # See: https://github.com/eclipse/hawkbit on how to set up
     # a test instance of hawkbit
     client = HawkbitTestClient("localhost", "8080", "admin", "admin")
-    assert(isinstance(client, HawkbitTestClient))
+    assert isinstance(client, HawkbitTestClient)
     return client
 
 def test_upgrade(hawkbit):

examples/strategy/bareboxrebootstrategy.py
@@ -24,7 +24,7 @@ class Status(enum.Enum):
 @attr.s(cmp=False)
 class BareboxRebootStrategy(Strategy):
     """A Strategy to switch to barebox or shell and back via reboot
-    
+
     In your env.yaml, simply instantiate the strategy without parameters, for example:
 
     .. code-block:: yaml

examples/usb/test_usb_mxs.py
@@ -6,4 +6,3 @@ def test_usb(target):
 def test_mxs_load(target):
     bp = target.get_driver('BootstrapProtocol')
     bp.load()
-

examples/usbpower/cycle.py
@@ -2,11 +2,10 @@ from labgrid import Target
 from labgrid.resource import USBPowerPort
 from labgrid.driver import USBPowerDriver
 
+
 t = Target(name='main')
-upp = USBPowerPort(t,
-        name=None,
-        match={'ID_PATH': 'pci-0000:00:14.0-usb-0:2:1.0'},
-        index=1)
+upp = USBPowerPort(t, name=None,
+                   match={'ID_PATH': 'pci-0000:00:14.0-usb-0:2:1.0'}, index=1)
 upd = USBPowerDriver(t, name=None)
 
 t.activate(upd)

labgrid/autoinstall/main.py
@@ -90,7 +90,7 @@ class Handler(multiprocessing.Process):
         try:
             if self.initial_resource:
                 self.log.info("waiting until %s is available",
-                        self.initial_resource.display_name)
+                              self.initial_resource.display_name)
                 while True:
                     self.target.update_resources()
                     if self.initial_resource.avail:
@@ -101,13 +101,13 @@ class Handler(multiprocessing.Process):
             self.target.update_resources()
             result = self.handler()
             if result is not None:
-                self.log.warn("unexpected return value from handler: %s",
-                        repr(result))
+                self.log.warning("unexpected return value from handler: %s",
+                                 repr(result))
             self.log.info("completed handler")
 
             if self.initial_resource:
                 self.log.info("waiting until %s is unavailable",
-                    self.initial_resource.display_name)
+                              self.initial_resource.display_name)
                 while True:
                     self.target.update_resources()
                     if not self.initial_resource.avail:
@@ -117,10 +117,10 @@ class Handler(multiprocessing.Process):
         except NoResourceFoundError as e:
             if e.filter and len(e.filter) > 1:
                 self.log.warning("resources %s not found, restarting",
-                        e.filter)
+                                 e.filter)
             elif e.filter:
                 self.log.warning("resource %s not found, restarting",
-                        next(iter(e.filter)))
+                                 next(iter(e.filter)))
             else:
                 self.log.warning("resource not found, restarting")
         except:
@@ -144,22 +144,22 @@ class Manager:
     def configure(self):
         if not 'autoinstall' in self.env.config.data:
             self.log.error("no 'autoinstall' section found in '%s'",
-                    self.env.config_file)
+                           self.env.config_file)
             return False
 
         if not 'handler' in self.env.config.data['autoinstall']:
             self.log.error("no 'handler' definition found in '%s'",
-                    self.env.config_file)
+                           self.env.config_file)
             return False
 
         self.handlers = {}
         for target_name in self.config.data.get('targets', {}).keys():
             self.handlers[target_name] = Handler(self.env, self.args,
-                    target_name)
+                                                 target_name)
 
         if not self.handlers:
             self.log.error("no targets found in '%s'",
-                    self.env.config_file)
+                           self.env.config_file)
             return False
 
         return True

labgrid/config.py
@@ -33,7 +33,7 @@ class Config:
         # map LG_* variables from OS environment into YAML config file using !template $LG_*
         # Only map LG_*, to protect from weird things in environment
         for x in os.environ.keys():
-            if x.startswith( "LG_" ):
+            if x.startswith("LG_"):
                 substitutions[x] = os.environ[x]
 
         try:

labgrid/consoleloggingreporter.py
@@ -52,9 +52,8 @@ class ConsoleLoggingReporter:
                 name = 'console_{}'.format(source.target.name)
             name = os.path.join(self.logpath, name)
             try:
-                log = self._logcache[source] = open(
-                        name, mode='ab', buffering=0
-                )
+                log = self._logcache[source] = open(name, mode='ab',
+                                                    buffering=0)
             except PermissionError:
                 print("failed to open log file {}".format(name), file=sys.stderr)
                 log = self._logcache[source] = None

commit dc9ef2e18d08e403cff388afc2e32cb5fa5aba1f

driver/networkusbstoragedriver: fix concatinating tuple to list in get_size()
The cat command was moved to its own variable during linting, but
accidentally to a tuple and not a list. This leads to an error while
concatinating with self.storage.command_prefix:

  TypeError: can only concatenate list (not "tuple") to list

Fix that.

Another occurrence of the same error was fixed by 2544d1d1
("driver/networkusbstoragedriver: fix concatinating tuple to list")

Fixes: 533cec4 ("linting: do miscellaneous linting")
Signed-off-by: Bastian Stender <bst@pengutronix.de>

labgrid/driver/networkusbstoragedriver.py
@@ -49,6 +49,6 @@ class NetworkUSBStorageDriver(Driver):
 
     @step(result=True)
     def get_size(self):
-        args = ("cat", "/sys/class/block/{}/size" % self.storage.path[5:])
+        args = ["cat", "/sys/class/block/{}/size" % self.storage.path[5:]]
         size = subprocess.check_output(self.storage.command_prefix + args)
         return int(size)
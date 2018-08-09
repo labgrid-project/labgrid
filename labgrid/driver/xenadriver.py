e7e5f7d57350d8549eec797c35edb0fc96215cb6
Kasper Revsbech <krev@triax.com>
2018-08-09 09:27:11 +0200

    driver/xenadriver: Add driver to interact with Xena network testing equipment
    Using the xenacalkyrie interface library to interact with the Xena test equipment.
    
    Signed-off-by: Kasper Revsbech <krev@triax.com>

---
+++ labgrid/driver/xenadriver.py
@@ -0,0 +1,35 @@
+import logging
+from importlib import import_module
+import attr
+
+from ..factory import target_factory
+from .common import Driver
+from ..resource.xenamanager import XenaManager
+
+@target_factory.reg_driver
+@attr.s(cmp=False)
+class XenaDriver(Driver):
+    """
+    Xena Driver
+    """
+    bindings = {"xena_manager": XenaManager}
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self._xena_app = import_module('xenavalkyrie.xena_app')
+        self._tgn_utils = import_module('trafficgenerator.tgn_utils')
+        self._logger = logging.getLogger("{}".format(self))
+        self._xm = None
+
+    def on_activate(self):
+        self._xm = self._xena_app.init_xena(self._tgn_utils.ApiType.socket, self._logger, 'labgrid')
+        self._xm.session.add_chassis(self.xena_manager.hostname)
+
+    def on_deactivate(self):
+        if self._xm:
+            self._xm.session.disconnect()
+            self._xm = None
+
+    @Driver.check_active
+    def get_session(self):
+        return self._xm.session

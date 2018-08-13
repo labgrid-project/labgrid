labgrid-project/labgrid

commit 1f89ed9f4e10b0773870105ee31df8a208593121

multiple: add _run function, use _run in _run_check for the mixin
Commit 1deea1c71728f50644ffd7b7e709577f43e1d4c2 introduced a bug where the
internal _run_check function used the run function guarded by a check_active
decorator. Use _run for the run_check function, refactor _run functions for
drivers who did not have it and adjust the tests to mock the correct funtion.

Fixes #230

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/driver/sshdriver.py
@@ -101,6 +101,10 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
     @Driver.check_active
     @step(args=['cmd'])
     def run(self, cmd, codec="utf-8", decodeerrors="strict", timeout=None): # pylint: disable=unused-argument
+        return self._run(cmd, codec=codec, decodererrors=decodeerrors)
+
+    @step(args=['cmd'])
+    def _run(self, cmd, codec, decodeerrors): # pylint: disable=unused-argument
         """Execute `cmd` on the target.
 
         This method runs the specified `cmd` as a command on its target.

labgrid/driver/bareboxdriver.py
@@ -62,6 +62,9 @@ class BareboxDriver(CommandMixin, Driver, CommandProtocol, LinuxBootProtocol):
     @Driver.check_active
     @step(args=['cmd'])
     def run(self, cmd: str, *, step, timeout: int = 30):  # pylint: disable=unused-argument
+        return self._run(cmd, step=step, timeout=timeout)
+
+    def _run(self, cmd: str, *, step, timeout: int = 30):  # pylint: disable=unused-argument
         """
         Runs the specified command on the shell and returns the output.
 

labgrid/driver/commandmixin.py
@@ -34,7 +34,7 @@ class CommandMixin:
         Returns:
             List[str]: stdout of the executed command
         """
-        stdout, stderr, exitcode = self.run(cmd, timeout=timeout)
+        stdout, stderr, exitcode = self._run(cmd, timeout=timeout)
         if exitcode != 0:
             raise ExecutionError(cmd, stdout, stderr)
         return stdout

tests/test_bareboxdriver.py
@@ -20,7 +20,7 @@ class TestBareboxDriver:
         t = target_with_fakeconsole
         d = BareboxDriver(t, "barebox")
         d = t.get_driver(BareboxDriver)
-        d.run = mocker.MagicMock(return_value=[['success'],[],0])
+        d._run = mocker.MagicMock(return_value=[['success'],[],0])
         res = d.run_check("test")
         assert res == ['success']
 
@@ -28,6 +28,6 @@ class TestBareboxDriver:
         t = target_with_fakeconsole
         d = BareboxDriver(t, "barebox")
         d = t.get_driver(BareboxDriver)
-        d.run = mocker.MagicMock(return_value=[['error'],[],1])
+        d._run = mocker.MagicMock(return_value=[['error'],[],1])
         with pytest.raises(ExecutionError):
             res = d.run_check("test")

tests/test_shelldriver.py
@@ -18,7 +18,7 @@ class TestShellDriver:
         d = ShellDriver(t, "shell", prompt='dummy', login_prompt='dummy', username='dummy')
         d.on_activate = mocker.MagicMock()
         d = t.get_driver('ShellDriver')
-        d.run = mocker.MagicMock(return_value=[['success'],[],0])
+        d._run = mocker.MagicMock(return_value=[['success'],[],0])
         res = d.run_check("test")
         assert res == ['success']
 
@@ -27,7 +27,7 @@ class TestShellDriver:
         d = ShellDriver(t, "shell", prompt='dummy', login_prompt='dummy', username='dummy')
         d.on_activate = mocker.MagicMock()
         d = t.get_driver('ShellDriver')
-        d.run = mocker.MagicMock(return_value=[['error'],[],1])
+        d._run = mocker.MagicMock(return_value=[['error'],[],1])
         with pytest.raises(ExecutionError):
             res = d.run_check("test")
 
@@ -36,6 +36,6 @@ class TestShellDriver:
         d = ShellDriver(t, "shell", prompt='dummy', login_prompt='dummy', username='dummy')
         d.on_activate = mocker.MagicMock()
         d = t.get_driver('ShellDriver')
-        d.run = mocker.MagicMock(return_value=[['success'],[],0])
+        d._run = mocker.MagicMock(return_value=[['success'],[],0])
         res = d.run_check("test", timeout=30.0)
         assert res == ['success']

tests/test_sshdriver.py
@@ -39,12 +39,12 @@ class TestSSHDriver:
 
     def test_run_check(self, ssh_driver_mocked_and_activated, mocker):
         s = ssh_driver_mocked_and_activated
-        s.run = mocker.MagicMock(return_value=[['success'],[],0])
+        s._run = mocker.MagicMock(return_value=[['success'],[],0])
         res = s.run_check("test")
         assert res == ['success']
 
     def test_run_check_raise(self, ssh_driver_mocked_and_activated, mocker):
         s = ssh_driver_mocked_and_activated
-        s.run = mocker.MagicMock(return_value=[['error'],[],1])
+        s._run = mocker.MagicMock(return_value=[['error'],[],1])
         with pytest.raises(ExecutionError):
             res = s.run_check("test")

commit 7418d315dacdd25b417b40b4b54e52ec965c82f3

driver/sshdriver: fix errors introduced with commit 1f89ed9f
Signed-off-by: Kjeld Flarup <kfa@deif.com>

labgrid/driver/sshdriver.py
@@ -101,9 +101,9 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
     @Driver.check_active
     @step(args=['cmd'], result=True)
     def run(self, cmd, codec="utf-8", decodeerrors="strict", timeout=None): # pylint: disable=unused-argument
-        return self._run(cmd, codec=codec, decodererrors=decodeerrors)
+        return self._run(cmd, codec=codec, decodeerrors=decodeerrors)
 
-    def _run(self, cmd, codec, decodeerrors): # pylint: disable=unused-argument
+    def _run(self, cmd, codec="utf-8", decodeerrors="strict", timeout=None): # pylint: disable=unused-argument
         """Execute `cmd` on the target.
 
         This method runs the specified `cmd` as a command on its target.
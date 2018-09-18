labgrid-project/labgrid

commit 9d26bb2d8650ccd1a80ff1b5044c5456d0af591c

serialdriver: add a warning about the RFC2217 fixes
Warn if the user is not using the pyserial labgrid-project fork.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/driver/serialdriver.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 
 import attr
 import serial
@@ -24,6 +25,12 @@ class SerialDriver(ConsoleExpectMixin, Driver, ConsoleProtocol):
         bindings = {"port": SerialPort, }
     else:
         bindings = {"port": {SerialPort, NetworkSerialPort}, }
+    if tuple(int(x) for x in serial.__version__.split('.')) <= (3, 4, 0, 1):
+        message = ("The installed python version does not contain important RFC2217 fixes.\n"
+            "You can install the labgrid fork via:\n"
+            "pip uninstall pyserial\n"
+            "pip install https://github.com/labgrid-project/pyserial/archive/v3.4.0.1.zip#egg=pyserial\n")
+        warnings.warn(message)
 
     txdelay = attr.ib(default=0.0, validator=attr.validators.instance_of(float))
 

commit 0491b98788bb236088934aa40bb6dfd0379e313e

serialdriver: fix version detection for RFC2217
Our versioned release is 3.4.0.1, so we should only warn if the pyserial version
is lower than this , not equal and lower.

Fixes: 9d26bb2d86 ("serialdriver: add a warning about the RFC2217 fixes")
Reported-by: Jonathan Götzinger <J.Goetzinger@eckelmann.de>
Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/driver/serialdriver.py
@@ -25,7 +25,7 @@ class SerialDriver(ConsoleExpectMixin, Driver, ConsoleProtocol):
         bindings = {"port": SerialPort, }
     else:
         bindings = {"port": {SerialPort, NetworkSerialPort}, }
-    if tuple(int(x) for x in serial.__version__.split('.')) <= (3, 4, 0, 1):
+    if tuple(int(x) for x in serial.__version__.split('.')) < (3, 4, 0, 1):
         message = ("The installed python version does not contain important RFC2217 fixes.\n"
             "You can install the labgrid fork via:\n"
             "pip uninstall pyserial\n"
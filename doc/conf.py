c38e4caa826f73cede1c4e2f4d54f6ee4e59b759
Jan Luebbe <jlu@pengutronix.de>
2018-09-26 11:59:29 +0200

    doc: sphinxcontrib-napoleon is part of sphinx
    According to https://pypi.org/project/sphinxcontrib-napoleon/, napoleon
    extension is part of sphinx 1.3.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinxcontrib.napoleon',
+              'sphinx.ext.napoleon',
               'sphinx.ext.coverage',
               'sphinx.ext.viewcode',
               'sphinx.ext.autosectionlabel']

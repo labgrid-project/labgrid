labgrid-project/labgrid

commit 9ef774642896202ec5499c29cee5877ddb186ff1

resource/udev: use update function for USBSDMuxDevice
Previously the USBSDMuxDevice set up its own polling solution to find the disk
and control path. Update them after the udev device is added or removed instead
of polling constantly.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/resource/udev.py
@@ -292,44 +292,24 @@ class USBSDMuxDevice(USBResource):
     def __attrs_post_init__(self):
         self.match['ID_VENDOR_ID'] = '0424'
         self.match['ID_MODEL_ID'] = '4041'
+        self.control_path = None
+        self.disk_path = None
         super().__attrs_post_init__()
 
-    def _get_scsi_dev(self):
+    def update(self):
+        super().update()
         if not self.device:
-            return None
-        devices = pyudev.Context().list_devices()
-        devices.match_parent(self.device)
-        devices.match_subsystem('scsi_generic')
-        for child in devices:
-            return child
-        return None
-
-    def _get_block_disk_dev(self):
-        if not self.device:
-            return None
-        devices = pyudev.Context().list_devices()
-        devices.match_parent(self.device)
-        devices.match_subsystem('block')
-        devices.match_property('DEVTYPE', 'disk')
-        for child in devices:
-            return child
-        return None
-
-    @property
-    def control_path(self):
-        dev = self._get_scsi_dev()
-        if dev:
-            return dev.device_node
-
-        return None
+            self.control_path = None
+            self.path = None
+        for child in self.device.children:
+            if child.subsystem == 'block':
+                self.disk_path = child.device_node
+            elif child.subsystem == 'scsi_generic':
+                self.control_path = child.device_node
 
     @property
     def path(self):
-        dev = self._get_block_disk_dev()
-        if dev:
-            return dev.device_node
-
-        return None
+        return self.disk_path
 
 @target_factory.reg_resource
 @attr.s(cmp=False)

commit ccb25ed277614dd41b69d5624171635a9f60ba0c

resource/udev: access USBSDMuxDevice's disk_path directly
USBSDMuxDevice's path property has no setter method. So use disk_path
directly on update().

Fixes: 9ef77464 ("resource/udev: use update function for USBSDMuxDevice")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/resource/udev.py
@@ -300,7 +300,7 @@ class USBSDMuxDevice(USBResource):
         super().update()
         if not self.device:
             self.control_path = None
-            self.path = None
+            self.disk_path = None
         for child in self.device.children:
             if child.subsystem == 'block':
                 self.disk_path = child.device_node
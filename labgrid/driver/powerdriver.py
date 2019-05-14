ea02b7676a87d79df62c5baf1a4988f5b2796d06
Jan Luebbe <jlu@pengutronix.de>
2019-05-14 13:42:39 +0200

    driver/power: add proxy support
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/driver/powerdriver.py
+++ labgrid/driver/powerdriver.py
@@ -12,6 +12,7 @@ from ..resource import YKUSHPowerPort
 from ..resource.remote import NetworkUSBPowerPort
 from ..resource.udev import USBPowerPort
 from ..step import step
+from ..util.proxy import proxymanager
 from .common import Driver
 from .exception import ExecutionError
 
@@ -109,8 +110,17 @@ class NetworkPowerDriver(Driver, PowerResetMixin, PowerProtocol):
             ".power.{}".format(self.port.model),
             __package__
         )
-        self._host = self.port.host
+        self._host = None
         self._port = None
+
+    def on_activate(self):
+        # we can only forward if the backend knows which port to use
+        backend_port = getattr(self.backend, 'PORT', None)
+        if backend_port:
+            self._host, self._port = proxymanager.get_host_and_port(self.port, force_port=backend_port)
+        else:
+            self._host = self.port.host
+            self._port = None
 
     @Driver.check_active
     @step()

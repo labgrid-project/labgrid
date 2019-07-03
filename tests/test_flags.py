e7b6a7884fa84b6547806a2e895a647a7ff19f55
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2019-07-03 18:10:14 +0200

    tests: add test for flags
    Add tests for the flags introduced previously.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ tests/test_flags.py
@@ -0,0 +1,114 @@
+import pexpect
+
+def test_with_feature(tmpdir):
+    conf = tmpdir.join("config.yaml")
+    conf.write(
+"""
+targets:
+  test1:
+    features:
+      - test
+    drivers: {}
+"""
+    )
+    test = tmpdir.join("test.py")
+    test.write(
+"""
+import pytest
+
+@pytest.mark.lg_feature("test")
+def test(env):
+    assert True
+"""
+    )
+
+    with pexpect.spawn('pytest --lg-env {} {}'.format(conf, test)) as spawn:
+        spawn.expect("1 passed")
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0
+
+def test_skip_feature(tmpdir):
+    conf = tmpdir.join("config.yaml")
+    conf.write(
+"""
+targets:
+  test1:
+    features:
+      - test
+    drivers: {}
+"""
+    )
+    test = tmpdir.join("test2.py")
+    test.write(
+"""
+import pytest
+
+@pytest.mark.lg_feature("test2")
+def test(env):
+    assert True
+"""
+    )
+
+    with pexpect.spawn('pytest --lg-env {} {}'.format(conf, test)) as spawn:
+        spawn.expect("1 skipped")
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0
+
+def test_skip_feature_list(tmpdir):
+    conf = tmpdir.join("config.yaml")
+    conf.write(
+"""
+targets:
+  test1:
+    features:
+      - test
+    drivers: {}
+"""
+    )
+    test = tmpdir.join("test2.py")
+    test.write(
+"""
+import pytest
+
+@pytest.mark.lg_feature(["test2", "test3"])
+def test(env):
+    assert True
+"""
+    )
+
+    with pexpect.spawn('pytest --lg-env {} {}'.format(conf, test)) as spawn:
+        spawn.expect("1 skipped")
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0
+
+def test_match_feature_list(tmpdir):
+    conf = tmpdir.join("config.yaml")
+    conf.write(
+"""
+targets:
+  test1:
+    features:
+      - test1
+      - test2
+    drivers: {}
+"""
+    )
+    test = tmpdir.join("test2.py")
+    test.write(
+"""
+import pytest
+
+@pytest.mark.lg_feature(["test1", "test2"])
+def test(env):
+    assert True
+"""
+    )
+
+    with pexpect.spawn('pytest --lg-env {} {}'.format(conf, test)) as spawn:
+        spawn.expect("1 passed")
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 0

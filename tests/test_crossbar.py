d3f03e6a08bd8294d5e7845097a07afa34bfd4c6
Jan Luebbe <jlu@pengutronix.de>
2019-11-29 13:38:53 +0100

    tests/test_crossbar: show process output when an assertion fails
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- tests/test_crossbar.py
+++ tests/test_crossbar.py
@@ -17,118 +17,118 @@ def place(crossbar):
     with pexpect.spawn('python -m labgrid.remote.client -p test create') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test set-tags board=bar') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     yield
 
     with pexpect.spawn('python -m labgrid.remote.client -p test delete') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 @pytest.fixture(scope='function')
 def place_acquire(place, exporter):
     with pexpect.spawn('python -m labgrid.remote.client -p test add-match "*/*/*"') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test acquire') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     yield
 
     with pexpect.spawn('python -m labgrid.remote.client -p test release') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_show(place):
     with pexpect.spawn('python -m labgrid.remote.client -p test show') as spawn:
         spawn.expect("Place 'test':")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_alias(place):
     with pexpect.spawn('python -m labgrid.remote.client -p test add-alias foo') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p foo del-alias foo') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_comment(place):
     with pexpect.spawn('python -m labgrid.remote.client -p test set-comment my comment') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test show') as spawn:
         spawn.expect("Place 'test':")
         spawn.expect(" comment: my comment")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_match(place):
     with pexpect.spawn('python -m labgrid.remote.client -p test add-match "e1/g1/r1" "e2/g2/*"') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test del-match "e1/g1/r1"') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test show') as spawn:
         spawn.expect(" matches:")
         spawn.expect(" e2/g2/*")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_aquire(place):
     with pexpect.spawn('python -m labgrid.remote.client -p test acquire') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client who') as spawn:
         spawn.expect(".*test.*")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test release') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_place_add_no_name(crossbar):
     with pexpect.spawn('python -m labgrid.remote.client create') as spawn:
         spawn.expect("missing place name")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus != 0
+        assert spawn.exitstatus != 0, spawn.before.strip()
 
 def test_place_del_no_name(crossbar):
     with pexpect.spawn('python -m labgrid.remote.client delete') as spawn:
         spawn.expect("deletes require an exact place name")
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus != 0
+        assert spawn.exitstatus != 0, spawn.before.strip()
 
 def test_remoteplace_target(place_acquire, tmpdir):
     from labgrid.environment import Environment
@@ -151,30 +151,30 @@ def test_resource_conflict(place_acquire, tmpdir):
     with pexpect.spawn('python -m labgrid.remote.client -p test2 create') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test2 add-match "*/*/*"') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test2 acquire') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus != 0
+        assert spawn.exitstatus != 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test2 delete') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
 def test_reservation(place_acquire, tmpdir):
     with pexpect.spawn('python -m labgrid.remote.client reserve --shell board=bar name=test') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
         m = re.search(rb"^export LG_TOKEN=(\S+)$", spawn.before.replace(b'\r\n', b'\n'), re.MULTILINE)
-        assert m is not None
+        assert m is not None, spawn.before.strip()
         token = m.group(1)
 
     env = os.environ.copy()
@@ -183,64 +183,64 @@ def test_reservation(place_acquire, tmpdir):
     with pexpect.spawn('python -m labgrid.remote.client reservations') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
-        assert b'waiting' in spawn.before
-        assert token in spawn.before
+        assert spawn.exitstatus == 0, spawn.before.strip()
+        assert b'waiting' in spawn.before, spawn.before.strip()
+        assert token in spawn.before, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test release') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client reservations') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
-        assert b'allocated' in spawn.before
-        assert token in spawn.before
+        assert spawn.exitstatus == 0, spawn.before.strip()
+        assert b'allocated' in spawn.before, spawn.before.strip()
+        assert token in spawn.before, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client reservations') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
-        assert b'allocated' in spawn.before
-        assert token in spawn.before
+        assert spawn.exitstatus == 0, spawn.before.strip()
+        assert b'allocated' in spawn.before, spawn.before.strip()
+        assert token in spawn.before, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p + acquire', env=env) as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p + show', env=env) as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert token in spawn.before
-        assert spawn.exitstatus == 0
+        assert token in spawn.before, spawn.before.strip()
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p + release', env=env) as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client reservations') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
-        assert b'allocated' in spawn.before
-        assert token in spawn.before
+        assert spawn.exitstatus == 0, spawn.before.strip()
+        assert b'allocated' in spawn.before, spawn.before.strip()
+        assert token in spawn.before, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client cancel-reservation', env=env) as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client reservations') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
-        assert token not in spawn.before
+        assert spawn.exitstatus == 0, spawn.before.strip()
+        assert token not in spawn.before, spawn.before.strip()
 
     with pexpect.spawn('python -m labgrid.remote.client -p test acquire') as spawn:
         spawn.expect(pexpect.EOF)
         spawn.close()
-        assert spawn.exitstatus == 0
+        assert spawn.exitstatus == 0, spawn.before.strip()

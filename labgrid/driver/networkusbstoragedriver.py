labgrid-project/labgrid

commit 98d07a3933e2c9cb05f9344edb9aeae3be6c256f

driver/networkusbstorage: implement path check for methods
Add a decorator for the path checks. The SDMux device can be available but may
not be in device mode. Add an explicit check and error for this case.

Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

labgrid/driver/networkusbstoragedriver.py
@@ -10,6 +10,8 @@ from ..resource.remote import NetworkUSBMassStorage, NetworkUSBSDMuxDevice
 from ..step import step
 from ..util.managedfile import ManagedFile
 from .common import Driver
+from ..driver.exception import ExecutionError
+
 
 @target_factory.reg_driver
 @attr.s(cmp=False)
@@ -35,6 +37,10 @@ class NetworkUSBStorageDriver(Driver):
 
     @step(args=['filename'])
     def write_image(self, filename):
+        if not self.storage.path:
+            raise ExecutionError(
+                "{} is not available".format(self.storage_path)
+            )
         mf = ManagedFile(filename, self.storage)
         mf.sync_to_resource()
         self.logger.info("pwd: %s", os.getcwd())
@@ -50,6 +56,10 @@ class NetworkUSBStorageDriver(Driver):
 
     @step(result=True)
     def get_size(self):
+        if not self.storage.path:
+            raise ExecutionError(
+                "{} is not available".format(self.storage_path)
+            )
         args = ["cat", "/sys/class/block/{}/size" % self.storage.path[5:]]
         size = subprocess.check_output(self.storage.command_prefix + args)
         return int(size)

commit d2cde5cf57054a0de67bb15cd125795f90d74b23

driver: networkusbstoragedriver: fix ExecutionErrors for unavailable storage
NetworkUSBStorageDriver does not have a storage_path member. Instead
storage.path is meant here. Fix that.

Fixes: 98d07a39 ("driver/networkusbstorage: implement path check for methods")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/networkusbstoragedriver.py
@@ -52,7 +52,7 @@ class NetworkUSBStorageDriver(Driver):
     def write_image(self, filename=None, mode=Mode.DD):
         if not self.storage.path:
             raise ExecutionError(
-                "{} is not available".format(self.storage_path)
+                "{} is not available".format(self.storage.path)
             )
         if filename is None and self.image is not None:
             filename = self.target.env.config.get_image_path(self.image)
@@ -101,7 +101,7 @@ class NetworkUSBStorageDriver(Driver):
     def get_size(self):
         if not self.storage.path:
             raise ExecutionError(
-                "{} is not available".format(self.storage_path)
+                "{} is not available".format(self.storage.path)
             )
         args = ["cat", "/sys/class/block/{}/size".format(self.storage.path[5:])]
         size = processwrapper.check_output(self.storage.command_prefix + args)
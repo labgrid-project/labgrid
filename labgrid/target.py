labgrid-project/labgrid

commit 53b4b34bad8f1c3b70abcd83523231cbbda8d589

target: allow driver bindings by class name
We already allow class name strings in get_driver and similar, so we
should also allow it in the binding dict for consistency. In the longer
term, this should help to reduce the amount of required imports just for
binding to the correct types.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/target.py
@@ -312,6 +312,9 @@ class Target:
             errors = []
             suppliers = []
             for requirement in requirements:
+                # convert class name string to classes
+                if isinstance(requirement, str):
+                    requirement = self._class_from_string(requirement)
                 try:
                     if issubclass(requirement, Resource):
                         suppliers.append(
@@ -456,4 +459,4 @@ class Target:
         try:
             return self._lookup_table[string]
         except KeyError:
-            raise KeyError("No such driver/resource/protocol in lookup table, perhaps not bound?")
+            raise KeyError("No driver/resource/protocol of type '{}' in lookup table, perhaps not bound?".format(string))

commit 623cf48ccf1d587e9215037af2c82fdcb585c290

target: lookup all classes, not only by registered
Instead of using only the targets resources and drivers, use all
resources and drivers while resolving resources and drivers during
binding.

Fixes: 53b4b34bad8f (target: allow driver bindings by class name)

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -11,6 +11,7 @@ from .exceptions import NoSupplierFoundError, NoDriverFoundError, NoResourceFoun
 from .resource import Resource
 from .strategy import Strategy
 from .util import Timeout
+from .factory import target_factory
 
 
 @attr.s(eq=False)
@@ -310,7 +311,7 @@ class Target:
             for requirement in requirements:
                 # convert class name string to classes
                 if isinstance(requirement, str):
-                    requirement = target_factory._reg_class_from_string(requirement)
+                    requirement = target_factory.class_from_string(requirement)
                 try:
                     if issubclass(requirement, Resource):
                         suppliers.append(
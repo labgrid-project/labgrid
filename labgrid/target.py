labgrid-project/labgrid

commit 103eb18014b4b2f4df948b8528986d4097154ba1

target: rename lookup function
Rename to _class_from_string function to _reg_class_from_string to
indicate that only classes registered by the targetfactory can be looked
up this way.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -106,7 +106,7 @@ class Target:
         found = []
         other_names = []
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
 
         for res in self.resources:
             if not isinstance(res, cls):
@@ -139,7 +139,7 @@ class Target:
         found = []
         other_names = []
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
 
         for drv in self.drivers:
             if not isinstance(drv, cls):
@@ -228,7 +228,7 @@ class Target:
         elif len(key) == 2:
             cls, name = key
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
         if not issubclass(cls, (Driver, abc.ABC)): # all Protocols derive from ABC
             raise NoDriverFoundError(
                 "invalid driver class {}".format(cls)
@@ -310,7 +310,7 @@ class Target:
             for requirement in requirements:
                 # convert class name string to classes
                 if isinstance(requirement, str):
-                    requirement = self._class_from_string(requirement)
+                    requirement = target_factory._reg_class_from_string(requirement)
                 try:
                     if issubclass(requirement, Resource):
                         suppliers.append(
@@ -457,7 +457,7 @@ class Target:
         for res in reversed(self.resources):
             self.deactivate(res)
 
-    def _class_from_string(self, string: str):
+    def _reg_class_from_string(self, string: str):
         try:
             return self._lookup_table[string]
         except KeyError:

commit d045a4c9e9a0e56af722670a5f5d36acd8516937

target: fix missing refactor of _class_from_string
This was renamed to _reg_class_from_string, fix the missed conversion.

Fixes: 103eb18014b4 ("target: rename lookup function")

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -432,7 +432,7 @@ class Target:
         This is needed to ensure that no client has an inactive supplier.
         """
         if isinstance(client, str):
-            client = self._class_from_string(client)
+            client = self._reg_class_from_string(client)
 
         if client.state is BindingState.bound:
             return  # nothing to do
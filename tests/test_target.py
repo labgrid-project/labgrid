labgrid-project/labgrid

commit 103eb18014b4b2f4df948b8528986d4097154ba1

target: rename lookup function
Rename to _class_from_string function to _reg_class_from_string to
indicate that only classes registered by the targetfactory can be looked
up this way.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -106,7 +106,7 @@ class Target:
         found = []
         other_names = []
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
 
         for res in self.resources:
             if not isinstance(res, cls):
@@ -139,7 +139,7 @@ class Target:
         found = []
         other_names = []
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
 
         for drv in self.drivers:
             if not isinstance(drv, cls):
@@ -228,7 +228,7 @@ class Target:
         elif len(key) == 2:
             cls, name = key
         if isinstance(cls, str):
-            cls = self._class_from_string(cls)
+            cls = self._reg_class_from_string(cls)
         if not issubclass(cls, (Driver, abc.ABC)): # all Protocols derive from ABC
             raise NoDriverFoundError(
                 "invalid driver class {}".format(cls)
@@ -310,7 +310,7 @@ class Target:
             for requirement in requirements:
                 # convert class name string to classes
                 if isinstance(requirement, str):
-                    requirement = self._class_from_string(requirement)
+                    requirement = target_factory._reg_class_from_string(requirement)
                 try:
                     if issubclass(requirement, Resource):
                         suppliers.append(
@@ -457,7 +457,7 @@ class Target:
         for res in reversed(self.resources):
             self.deactivate(res)
 
-    def _class_from_string(self, string: str):
+    def _reg_class_from_string(self, string: str):
         try:
             return self._lookup_table[string]
         except KeyError:

commit 4e2f91667f2a22b8db74d8abbf407ed554ed17fd

target: enable de/activate by string
self._reg_class_from_string() only retrieves the class, not the string.
Use the _get_driver call to retrieve the instance. Also add the name
parameter to enable usage in case multiple classes are bound to the same
target. While at it implement the same syntax for activate as well.

Also add tests for this feature.

Fixes: 103eb18014b4 ("target: rename lookup function")

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -391,7 +391,7 @@ class Target:
 
         raise BindingError("object {} is not bindable".format(bindable))
 
-    def activate(self, client):
+    def activate(self, client, name=None):
         """
         Activate the client by activating all bound suppliers. This may require
         deactivating other clients.
@@ -399,6 +399,12 @@ class Target:
         # don't activate strategies, they usually have conflicting bindings
         if isinstance(client, Strategy):
             return
+
+        if isinstance(client, str):
+            cls = self._reg_class_from_string(client)
+            client = self._get_driver(cls, name=name, activate=False, active=False)
+
+        assert client is not None
 
         if client.state is BindingState.active:
             return  # nothing to do
@@ -425,14 +431,17 @@ class Target:
         client.on_activate()
         client.state = BindingState.active
 
-    def deactivate(self, client):
+    def deactivate(self, client, name=None):
         """
         Recursively deactivate the client's clients and itself.
 
         This is needed to ensure that no client has an inactive supplier.
         """
         if isinstance(client, str):
-            client = self._reg_class_from_string(client)
+            cls = self._reg_class_from_string(client)
+            client = self._get_driver(cls, name=name, activate=False, active=True)
+
+        assert client is not None
 
         if client.state is BindingState.bound:
             return  # nothing to do

tests/test_target.py
@@ -352,3 +352,26 @@ def test_get_by_default_priority(target):
     with pytest.raises(NoDriverFoundError) as e_info:
         target.get_driver(AProtocol)
     assert "multiple drivers matching" in str(e_info.value)
+
+def test_target_deactivate_by_string(target):
+
+    @attr.s
+    class A(Driver):
+        pass
+
+    a = A(target, None)
+    target.activate(a)
+    target.deactivate("A")
+
+    assert a == target.get_driver(A, activate=False)
+
+def test_target_activate_by_string(target):
+
+    @attr.s
+    class A(Driver):
+        pass
+
+    a = A(target, None)
+    target.activate("A")
+
+    assert a == target.get_active_driver(A)
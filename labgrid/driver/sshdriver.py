labgrid-project/labgrid

commit 1d7b97b838f77e5e877d4227cd019b441bbc31a2

driver/sshdriver: Allow whitespaces in filenames Prevent filenames with whitespaces from beeing split.
Signed-off-by: Jonathan Götzinger <j.goetzinger@eckelmann.de>

labgrid/driver/sshdriver.py
@@ -164,14 +164,17 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
     @Driver.check_active
     @step(args=['filename', 'remotepath'])
     def put(self, filename, remotepath=''):
-        transfer_cmd = "scp {prefix} -P {port} {filename} {user}@{host}:{remotepath}".format(
-            filename=filename,
-            user=self.networkservice.username,
-            host=self.networkservice.address,
-            remotepath=remotepath,
-            prefix=self.ssh_prefix,
-            port=self.networkservice.port
-        ).split(' ')
+        transfer_cmd = [
+            "scp",
+            self.ssh_prefix,
+            "-P", str(self.networkservice.port),
+            filename,
+            "{user}@{host}:{remotepath}".format(
+                user=self.networkservice.username,
+                host=self.networkservice.address,
+                remotepath=remotepath)
+            ]
+
         try:
             sub = subprocess.call(
                 transfer_cmd
@@ -188,14 +191,17 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
     @Driver.check_active
     @step(args=['filename', 'destination'])
     def get(self, filename, destination="."):
-        transfer_cmd = "scp {prefix} -P {port} {user}@{host}:{filename} {destination}".format(
-            filename=filename,
-            user=self.networkservice.username,
-            host=self.networkservice.address,
-            prefix=self.ssh_prefix,
-            port=self.networkservice.port,
-            destination=destination
-        ).split(' ')
+        transfer_cmd = [
+            "scp",
+            self.ssh_prefix,
+            "-P", str(self.networkservice.port),
+            "{user}@{host}:{filename}".format(
+                user=self.networkservice.username,
+                host=self.networkservice.address,
+                filename=filename),
+            destination
+            ]
+
         try:
             sub = subprocess.call(
                 transfer_cmd

commit e2fa8e47ec894988004a4bafa86291a058a665e4

driver/sshdriver: Allow filename to be a folder
Adding a "-r" in front of filename helps with folders, and it still works
with regular files.
Prior to commit 1d7b97b838f7, something like this was working:
ssh.put('-r <folder_name>', '<remote_location>')
but afterwards, scp fails with "unknown option --".
Add a similar change for ssh.put

Fixes: 1d7b97b838f7 (driver/sshdriver: Allow whitespaces in filenames Prevent filenames with whitespaces from beeing split.)

Signed-off-by: Mirela Rabulea <mirela.rabulea@nxp.com>

put

Signed-off-by: Mirela Rabulea <mirela.rabulea@nxp.com>

labgrid/driver/sshdriver.py
@@ -311,6 +311,7 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             "scp",
             *self.ssh_prefix,
             "-P", str(self.networkservice.port),
+            "-r",
             filename,
             "{user}@{host}:{remotepath}".format(
                 user=self.networkservice.username,
@@ -338,6 +339,7 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             "scp",
             *self.ssh_prefix,
             "-P", str(self.networkservice.port),
+            "-r",
             "{user}@{host}:{filename}".format(
                 user=self.networkservice.username,
                 host=self.networkservice.address,
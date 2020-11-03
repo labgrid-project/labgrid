labgrid-project/labgrid

commit 1a5c787a56aa9f7e57e6ad5cfb5236e51aa54d6d

driver/usbstoragedriver: log what actually gets written
Add log entires just before actual writing is executed. This is useful
when checking logs to figure out what exaclty got written.

Signed-off-by: Stefan Agner <stefan@agner.ch>

labgrid/driver/usbstoragedriver.py
@@ -82,13 +82,16 @@ class USBStorageDriver(Driver):
             raise ExecutionError("Timeout while waiting for medium")
 
         partition = "" if partition is None else partition
+        remote_path = mf.get_remote_path()
+        target = "{}{}".format(self.storage.path, partition)
 
         if mode == Mode.DD:
+            self.logger.info('Writing %s to %s using dd.', remote_path, target)
             block_size = '512' if skip or seek else '4M'
             args = [
                 "dd",
-                "if={}".format(mf.get_remote_path()),
-                "of={}{}".format(self.storage.path, partition),
+                "if={}".format(remote_path),
+                "of={}".format(target),
                 "oflag=direct",
                 "status=progress",
                 "bs={}".format(block_size),
@@ -99,11 +102,12 @@ class USBStorageDriver(Driver):
         elif mode == Mode.BMAPTOOL:
             if skip or seek:
                 raise ExecutionError("bmaptool does not support skip or seek")
+            self.logger.info('Writing %s to %s using bmaptool.', remote_path, target)
             args = [
                 "bmaptool",
                 "copy",
-                "{}".format(mf.get_remote_path()),
-                "{}{}".format(self.storage.path, partition),
+                "{}".format(remote_path),
+                "{}{}".format(target),
             ]
         else:
             raise ValueError

commit 9373f7ae41a502719f2f1a6a68e96dbe2916f3da

driver/usbstoragedriver: fix bmaptool call format string
Fixes: 1a5c787a ("driver/usbstoragedriver: log what actually gets written")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/usbstoragedriver.py
@@ -107,7 +107,7 @@ class USBStorageDriver(Driver):
                 "bmaptool",
                 "copy",
                 "{}".format(remote_path),
-                "{}{}".format(target),
+                "{}".format(target),
             ]
         else:
             raise ValueError
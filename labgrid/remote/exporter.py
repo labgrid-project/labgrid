labgrid-project/labgrid

commit 162e903d1b68c90277a672afb6cb16747d3d7c40

exporter: Correctly handle serial baud rates other than 115200
The exporter was ignoring the device baud rate when invoking ser2net and
always connected at 115200 baud. Change the exporter to correctly
respect the device baud rate.

Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>

labgrid/remote/exporter.py
@@ -230,7 +230,7 @@ class SerialPortExport(ResourceExport):
                 '-d',
                 '-n',
                 '-Y', 'connection: &con01#  accepter: telnet(rfc2217,mode=server),{}'.format(self.port),
-                '-Y', '  connector: serialdev(nouucplock=true),{},115200n81,local'.format(start_params['path']
+                '-Y', '  connector: serialdev(nouucplock=true),{},{}n81,local'.format(start_params['path'], start_params['speed'],
                 ),
             ])
         else:
@@ -240,8 +240,8 @@ class SerialPortExport(ResourceExport):
                 '-n',
                 '-u',
                 '-C',
-                '{}:telnet:0:{}:115200 NONE 8DATABITS 1STOPBIT LOCAL'.format(
-                    self.port, start_params['path']
+                '{}:telnet:0:{}:{} NONE 8DATABITS 1STOPBIT LOCAL'.format(
+                    self.port, start_params['path'], start_params['speed']
                 ),
             ])
         try:

commit 41872064ecc00c3edb36554fca3baed898d2719a

exporter: fix SerialPortExport start params
The start params do not contain the speed for the serial port, but the
local SerialPort resource does, so fetch it from there.

Fixes: 162e903d1b68 ("exporter: Correctly handle serial baud rates other than 115200")

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Tested-by: Gavin Schenk <g.schenk@eckelmann.de>

labgrid/remote/exporter.py
@@ -230,7 +230,7 @@ class SerialPortExport(ResourceExport):
                 '-d',
                 '-n',
                 '-Y', 'connection: &con01#  accepter: telnet(rfc2217,mode=server),{}'.format(self.port),
-                '-Y', '  connector: serialdev(nouucplock=true),{},{}n81,local'.format(start_params['path'], start_params['speed'],
+                '-Y', '  connector: serialdev(nouucplock=true),{},{}n81,local'.format(start_params['path'], self.local.speed,
                 ),
             ])
         else:
@@ -241,7 +241,7 @@ class SerialPortExport(ResourceExport):
                 '-u',
                 '-C',
                 '{}:telnet:0:{}:{} NONE 8DATABITS 1STOPBIT LOCAL'.format(
-                    self.port, start_params['path'], start_params['speed']
+                    self.port, start_params['path'], self.local.speed
                 ),
             ])
         try:
labgrid-project/labgrid

commit 5cbb21e4e43d840902e9dd7b1fbda34ef4f4a286

resource: add EthernetPort managed resources
These monitor the status of an Ethernet port via SNMP and also report
the associated IP addresses.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/resource/ethernetport.py
@@ -1,0 +1,336 @@
+import logging
+import subprocess
+import attr
+from time import time
+
+from ..factory import target_factory
+from .common import Resource, ManagedResource, ResourceManager
+
+@attr.s
+class SNMPSwitch:
+    """SNMPSwitch describes a switch accessible over SNMP. This class
+    implements functions to query ports and the forwarding database."""
+    hostname = attr.ib(validator=attr.validators.instance_of(str))
+
+    def __attrs_post_init__(self):
+        self.logger = logging.getLogger("{}".format(self))
+        self.ports = {}
+        self.fdb = {}
+        self.macs_by_port = {}
+        self._autodetect()
+
+    def _autodetect(self):
+        from pysnmp import hlapi
+
+        for (errorIndication, errorStatus, errorIndex, varBindTable) in hlapi.getCmd(
+                hlapi.SnmpEngine(),
+                hlapi.CommunityData('public'),
+                hlapi.UdpTransportTarget((self.hostname, 161)),
+                hlapi.ContextData(),
+                hlapi.ObjectType(hlapi.ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))):
+            if errorIndication:
+                Exception("snmp error {}".format(errorIndication))
+            elif errorStatus:
+                Exception("snmp error {}".format(errorStatus))
+            else:
+                sysDescr = str(varBindTable[0][1])
+
+        if sysDescr.startswith("HPE OfficeConnect Switch 1820 24G J9980A,"):
+            self._get_fdb = self._get_fdb_dot1q
+        elif sysDescr.startswith("HP 1810-24G,"):
+            self._get_fdb = self._get_fdb_dot1d
+        else:
+            Exception("unsupported switch {}".format(sysDescr))
+
+        self.logger.debug("autodetected switch{}: {} {}".format(
+            sysDescr, self._get_ports, self._get_fdb))
+
+    def _get_ports(self):
+        """Fetch ports and their values via SNMP
+
+        Returns:
+            Dict[Dict[]]: ports and their values
+        """
+        from pysnmp import hlapi
+
+        vars = [
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifIndex')),'index'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifDescr')), 'descr'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifSpeed')), 'speed'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifOperStatus')), 'status'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifInErrors')), 'inErrors'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifHCInOctets')), 'inOctets'),
+            (hlapi.ObjectType(hlapi.ObjectIdentity('IF-MIB', 'ifHCOutOctets')), 'outOctets'),
+        ]
+        ports = {}
+
+        for (errorIndication, errorStatus, errorIndex, varBindTable) in hlapi.bulkCmd(
+                hlapi.SnmpEngine(),
+                hlapi.CommunityData('public'),
+                hlapi.UdpTransportTarget((self.hostname, 161)),
+                hlapi.ContextData(),
+                0, 20,
+                *[x[0] for x in vars],
+                lexicographicMode=False):
+            if errorIndication:
+                Exception("snmp error {}".format(errorIndication))
+            elif errorStatus:
+                Exception("snmp error {}".format(errorStatus))
+            else:
+                port = {}
+                for (key, val), (base, label) in zip(varBindTable, vars):
+                    index = key.getMibSymbol()[-1][0].prettyPrint()
+                    val = val.prettyPrint()
+                    if label == 'status':
+                        val = val.strip("'")
+                    port[label] = val
+                ports[port.pop('index')] = port
+
+        return ports
+
+    def _get_fdb_dot1d(self):
+        """Fetch the forwarding database via SNMP using the BRIDGE-MIB
+
+        Returns:
+            Dict[List[str]]: ports and their values
+        """
+        from pysnmp import hlapi
+
+        ports = {}
+
+        for (errorIndication, errorStatus, errorIndex, varBindTable) in hlapi.bulkCmd(
+                hlapi.SnmpEngine(),
+                hlapi.CommunityData('public'),
+                hlapi.UdpTransportTarget((self.hostname, 161)),
+                hlapi.ContextData(),
+                0, 50,
+                hlapi.ObjectType(hlapi.ObjectIdentity('BRIDGE-MIB', 'dot1dTpFdbPort')),
+                lexicographicMode=False):
+            if errorIndication:
+                Exception("snmp error {}".format(errorIndication))
+            elif errorStatus:
+                Exception("snmp error {}".format(errorStatus))
+            else:
+                for varBinds in varBindTable:
+                    key, val = varBinds
+                    if not val:
+                        continue
+                    mac = key.getMibSymbol()[-1][0].prettyPrint()
+                    interface = str(int(val))
+                    ports.setdefault(interface, []).append(mac)
+
+        return ports
+
+    def _get_fdb_dot1q(self):
+        """Fetch the forwarding database via SNMP using the Q-BRIDGE-MIB
+
+        Returns:
+            Dict[List[str]]: ports and their values
+        """
+        from pysnmp import hlapi
+
+        ports = {}
+
+        for (errorIndication, errorStatus, errorIndex, varBindTable) in hlapi.bulkCmd(
+                hlapi.SnmpEngine(),
+                hlapi.CommunityData('public'),
+                hlapi.UdpTransportTarget((self.hostname, 161)),
+                hlapi.ContextData(),
+                0, 50,
+                hlapi.ObjectType(hlapi.ObjectIdentity('Q-BRIDGE-MIB', 'dot1qTpFdbPort')),
+                lexicographicMode=False):
+            if errorIndication:
+                Exception("snmp error {}".format(errorIndication))
+            elif errorStatus:
+                Exception("snmp error {}".format(errorStatus))
+            else:
+                for varBinds in varBindTable:
+                    key, val = varBinds
+                    if not val:
+                        continue
+                    mac = key.getMibSymbol()[-1][1].prettyPrint()
+                    interface = str(int(val))
+                    ports.setdefault(interface, []).append(mac)
+
+        return ports
+
+    def _update_macs(self):
+        """remember the first time we've seen a MAC on a port"""
+        for removed in self.macs_by_port.keys() - self.ports.keys():
+            del self.macs_by_port[removed]
+        for interface, macs in self.fdb.items():
+            seen = self.macs_by_port.setdefault(interface, {})
+            for removed in seen.keys() - set(macs):
+                del seen[removed]
+            for mac in macs:
+                seen.setdefault(mac, int(time()))
+
+    def update(self):
+        """Update port status and forwarding database status
+
+        Returns:
+            None
+        """
+        self.logger.debug("polling switch FDB")
+        self.fdb = self._get_fdb()
+        self.logger.debug("polling switch ports")
+        self.ports = self._get_ports()
+        self.logger.debug("updating macs by port")
+        self._update_macs()
+
+
+@attr.s
+class EthernetPortManager(ResourceManager):
+    """The EthernetPortManager periodically polls the switch for new updates."""
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.logger = logging.getLogger("{}".format(self))
+        self.loop = None
+        self.poll_tasks = []
+        self.switches = {}
+        self.neighbors = {}
+
+    def on_resource_added(self, resource):
+        """Handler to execute when the resource is added
+
+        Checks whether the resource can be managed by this Manager and starts
+        the event loop.
+
+        Args:
+            resource(Resource): resource to check against
+
+        Returns:
+            None
+        """
+        if not isinstance(resource, SNMPEthernetPort):
+            return
+        self._start()
+        resource.avail=True
+
+    def _start(self):
+        """Internal function to register as task and attach/start the event
+        loop
+
+        Returns:
+            None
+        """
+        import asyncio
+
+        if self.poll_tasks:
+            return
+
+        @asyncio.coroutine
+        def poll_neighbour(self):
+            self.logger.debug("polling neighbor table")
+            self.neighbors = self._get_neigh()
+
+            yield from asyncio.sleep(1.0)
+
+        @asyncio.coroutine
+        def poll_switches(self):
+            current = set(resource.switch for resource in self.resources)
+            removed = set(self.switches) - current
+            new = current - set(self.switches)
+            for switch in removed:
+                del self.switches[switch]
+            for switch in new:
+                self.switches[switch] = SNMPSwitch(switch)
+            for switch in current:
+                self.switches[switch].update()
+                yield from asyncio.sleep(1.0)
+
+            yield from asyncio.sleep(2.0)
+
+        @asyncio.coroutine
+        def poll(self, handler):
+            while True:
+                try:
+                    yield from asyncio.sleep(1.0)
+                    yield from handler(self)
+                except asyncio.CancelledError:
+                    break
+                except:
+                    import traceback
+                    traceback.print_exc()
+
+        self.loop = asyncio.get_event_loop()
+        self.poll_tasks.append(self.loop.create_task(poll(self, poll_neighbour)))
+        self.poll_tasks.append(self.loop.create_task(poll(self, poll_switches)))
+
+    def _get_neigh(self):
+        """Internal function to retrieve the neighbors on the test machine
+
+        Returns:
+            Dict[Tuple[(str,str,str)]]: dictionary with mac addresses as keys
+            and a Tuple of address, device and state as values
+        """
+        neighbors = {}
+
+        for line in subprocess.check_output(['ip', 'neigh', 'show']).splitlines():
+            line = line.decode('ascii').strip().split()
+            addr = line.pop(0)
+            if line[0] == 'dev':
+                line.pop(0)
+                dev = line.pop(0)
+            else:
+                dev = None
+            if line[0] == 'lladdr':
+                line.pop(0)
+                lladdr = line.pop(0)
+            else:
+                lladdr = None
+            if line[0] == 'router':
+                line.pop()
+            state = line.pop(0)
+            assert not line
+            # TODO: check if we could use the device and state information
+            neighbors.setdefault(lladdr, []).append(addr)
+        for value in neighbors.values():
+            value.sort()
+
+        return neighbors
+
+    def poll(self):
+        """Updates the state with new information from the event loop
+
+        Returns:
+            None
+        """
+        import asyncio
+        if not self.loop.is_running():
+            self.loop.run_until_complete(asyncio.sleep(0.0))
+        for resource in self.resources:
+            switch = self.switches.get(resource.switch)
+            if not switch:
+                resource.extra = None
+                continue
+            extra = {}
+            for mac, timestamp in switch.macs_by_port.get(resource.interface, {}).items():
+                extra.setdefault('macs', {})[mac] = {
+                    'timestamp': timestamp,
+                    'ips': self.neighbors.get(mac, []),
+                }
+            extra.update(switch.ports.get(resource.interface, {}))
+            if resource.extra != extra:
+                resource.extra = extra
+                self.logger.debug("new information for {}: {}".format(resource, extra))
+
+
+@target_factory.reg_resource
+@attr.s
+class SNMPEthernetPort(ManagedResource):
+    """SNMPEthernetPort describes an ethernet port which can be queried over
+    SNMP.
+
+    Args:
+        switch (str): hostname of the switch to query
+        interface (str): name of the interface to query
+    """
+    manager_cls = EthernetPortManager
+
+    switch = attr.ib(validator=attr.validators.instance_of(str))
+    interface = attr.ib(validator=attr.validators.instance_of(str))
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.extra = {}

labgrid/remote/exporter.py
@@ -187,6 +187,28 @@ exports["MXSUSBLoader"] = USBGenericExport
 exports["AlteraUSBBlaster"] = USBGenericExport
 
 
+@attr.s
+class EthernetPortExport(ResourceExport):
+    """ResourceExport for a ethernet interface"""
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        from ..resource.ethernetport import SNMPEthernetPort
+        self.data['cls'] = "EthernetPort"
+        self.local = SNMPEthernetPort(target=None, name=None,
+                **self.local_params)
+
+    def _get_params(self):
+        """Helper function to return parameters"""
+        return {
+            'switch': self.local.switch,
+            'interface': self.local.interface,
+            'extra': self.local.extra
+        }
+
+exports["SNMPEthernetPort"] = EthernetPortExport
+
+
 class ExporterSession(ApplicationSession):
     def onConnect(self):
         """Set up internal datastructures on successful connection:

labgrid/resource/__init__.py
@@ -1,4 +1,5 @@
-from .base import SerialPort
+from .base import SerialPort, EthernetInterface, EthernetPort
+from .ethernetport import SNMPEthernetPort
 from .serialport import RawSerialPort, NetworkSerialPort
 from .networkservice import NetworkService
 from .onewireport import OneWirePIO

labgrid/resource/base.py
@@ -1,5 +1,6 @@
 import attr
 
+from ..factory import target_factory
 from .common import Resource
 
 
@@ -21,3 +22,14 @@ class EthernetInterface(Resource):
     Args:
         ifname (str): name of the interface"""
     ifname = attr.ib(default=None)
+
+@target_factory.reg_resource
+@attr.s
+class EthernetPort(Resource):
+    """The basic EthernetPort describes a switch and interface
+
+    Args:
+        switch (str): name of the switch
+        interface (str): name of the interface"""
+    switch = attr.ib(default=None)
+    interface = attr.ib(default=None)

setup.py
@@ -17,7 +17,8 @@ setup(
     python_requires='>=3.4',
     extras_require={
         'onewire': ['onewire>=0.2'],
-        'coordinator': ['crossbar', 'idna==2.5']
+        'coordinator': ['crossbar', 'idna==2.5'],
+        'snmp': ['pysnmp', 'pysnmp-mibs'],
     },
     setup_requires=['pytest-runner', 'setuptools_scm'],
     tests_require=['pytest-mock', ],

tests/test_ethernetport.py
@@ -1,0 +1,7 @@
+import pytest
+
+from labgrid.resource import SNMPEthernetPort
+
+def test_instance(target):
+    s = SNMPEthernetPort(target, 'port-1', switch='dummy-switch', interface='1')
+    assert (isinstance(s, SNMPEthernetPort))

commit 330d13d1a67495a84548c6ff0989dc03582fd1c1

resource: ethernetport: raise exceptions rather than only instantiating them
Instantiating an exception, but not raising it, has no effect.

Fixes: 5cbb21e4 ("resource: add EthernetPort managed resources")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/resource/ethernetport.py
@@ -29,9 +29,9 @@ class SNMPSwitch:
                 hlapi.ContextData(),
                 hlapi.ObjectType(hlapi.ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))):
             if errorIndication:
-                Exception("snmp error {}".format(errorIndication))
+                raise Exception("snmp error {}".format(errorIndication))
             elif errorStatus:
-                Exception("snmp error {}".format(errorStatus))
+                raise Exception("snmp error {}".format(errorStatus))
             else:
                 sysDescr = str(varBindTable[0][1])
 
@@ -40,7 +40,7 @@ class SNMPSwitch:
         elif sysDescr.startswith("HP 1810-24G,"):
             self._get_fdb = self._get_fdb_dot1d
         else:
-            Exception("unsupported switch {}".format(sysDescr))
+            raise Exception("unsupported switch {}".format(sysDescr))
 
         self.logger.debug("autodetected switch%s: %s %s", sysDescr, self._get_ports, self._get_fdb)
 
@@ -72,9 +72,9 @@ class SNMPSwitch:
                 *[x[0] for x in variables],
                 lexicographicMode=False):
             if errorIndication:
-                Exception("snmp error {}".format(errorIndication))
+                raise Exception("snmp error {}".format(errorIndication))
             elif errorStatus:
-                Exception("snmp error {}".format(errorStatus))
+                raise Exception("snmp error {}".format(errorStatus))
             else:
                 port = {}
                 for (_, val), (_, label) in zip(varBindTable, variables):
@@ -105,9 +105,9 @@ class SNMPSwitch:
                 hlapi.ObjectType(hlapi.ObjectIdentity('BRIDGE-MIB', 'dot1dTpFdbPort')),
                 lexicographicMode=False):
             if errorIndication:
-                Exception("snmp error {}".format(errorIndication))
+                raise Exception("snmp error {}".format(errorIndication))
             elif errorStatus:
-                Exception("snmp error {}".format(errorStatus))
+                raise Exception("snmp error {}".format(errorStatus))
             else:
                 for varBinds in varBindTable:
                     key, val = varBinds
@@ -138,9 +138,9 @@ class SNMPSwitch:
                 hlapi.ObjectType(hlapi.ObjectIdentity('Q-BRIDGE-MIB', 'dot1qTpFdbPort')),
                 lexicographicMode=False):
             if errorIndication:
-                Exception("snmp error {}".format(errorIndication))
+                raise Exception("snmp error {}".format(errorIndication))
             elif errorStatus:
-                Exception("snmp error {}".format(errorStatus))
+                raise Exception("snmp error {}".format(errorStatus))
             else:
                 for varBinds in varBindTable:
                     key, val = varBinds
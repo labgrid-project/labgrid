labgrid-project/labgrid

commit e21a5eba405d0d4c4cfb8d2b235a2cf00ab700b8

resource: rename SerialPort to RawSerialPort
This prepares for additional SerialPort implementations.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/resource/serialport.py
@@ -2,10 +2,13 @@ import attr
 
 from ..factory import target_factory
 from .common import Resource
+from .base import SerialPort
 
 
 @target_factory.reg_resource
 @attr.s
-class SerialPort(Resource):
-    port = attr.ib(validator=attr.validators.instance_of(str))
-    speed = attr.ib(default=115200, validator=attr.validators.instance_of(int))
+class RawSerialPort(SerialPort, Resource):
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        if self.port is None:
+            ValueError("RawSerialPort must be configured with a port")

examples/barebox/local.env
@@ -1,6 +1,6 @@
 main:
   resources:
-    SerialPort:
+    RawSerialPort:
       port: "/dev/ttyUSB0"
   drivers:
     ManualPowerDriver:

examples/infoprotocol/env.yaml
@@ -1,6 +1,6 @@
 main:
   resources:
-    SerialPort: { 'port': '/dev/ttyUSB0' }
+    RawSerialPort: { 'port': '/dev/ttyUSB0' }
   drivers:
     SerialDriver: { }
     ShellDriver: { 'prompt': 'root@DistroKit:~ ', 'login_prompt': 'DistroKit login: ', 'username': 'root', 'keyfile': 'riot.pub'}

examples/strategy/local.env
@@ -1,6 +1,6 @@
 main:
   resources:
-    SerialPort:
+    RawSerialPort:
       port: "/dev/ttyUSB0"
   drivers:
     ManualPowerDriver:

examples/usbstick/env.yaml
@@ -1,6 +1,6 @@
 main:
   resources:
-    SerialPort:
+    RawSerialPort:
       port: '/dev/ttyUSB0'
     NetworkService:
       address: '192.168.24.135'

labgrid/resource/__init__.py
@@ -1,3 +1,4 @@
-from .serialport import SerialPort
+from .base import SerialPort
+from .serialport import RawSerialPort
 from .networkservice import NetworkService
 from .common import Resource, ResourceManager, ManagedResource

labgrid/resource/base.py
@@ -1,0 +1,9 @@
+import attr
+
+from .common import Resource
+
+
+@attr.s
+class SerialPort(Resource):
+    port = attr.ib(default=None)
+    speed = attr.ib(default=115200, validator=attr.validators.instance_of(int))

tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 from labgrid import Target, target_factory
 from labgrid.driver import SerialDriver
 from labgrid.protocol import CommandProtocol, ConsoleProtocol
-from labgrid.resource import SerialPort
+from labgrid.resource import RawSerialPort
 
 
 @pytest.fixture(scope='function')
@@ -16,7 +16,7 @@ def target():
 
 @pytest.fixture(scope='function')
 def serial_port(target):
-    return SerialPort(target, '/dev/test')
+    return RawSerialPort(target, '/dev/test')
 
 
 @pytest.fixture(scope='function')

commit a99a93fb0b836aef96c059a7dab713c7559735ea

resource: serialport: raise exception rather than only instantiating it
Instantiating an exception, but not raising it, has no effect.

Fixes: e21a5eba ("resource: rename SerialPort to RawSerialPort")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/resource/serialport.py
@@ -12,7 +12,7 @@ class RawSerialPort(SerialPort, Resource):
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
         if self.port is None:
-            ValueError("RawSerialPort must be configured with a port")
+            raise ValueError("RawSerialPort must be configured with a port")
 
 # This does not derive from SerialPort because it is not directly accessible
 @target_factory.reg_resource
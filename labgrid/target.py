labgrid-project/labgrid

commit 0e9ddabc0817e5eed627466df21229ec9ba6b415

target: add atexit handler to ensure cleanup
Register an atexit handler to ensure that the cleanup function on
drivers is called.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/target.py
@@ -1,4 +1,5 @@
 import abc
+import atexit
 import logging
 from time import monotonic, sleep
 from collections import Counter
@@ -32,6 +33,7 @@ class Target:
         self._lookup_table = {
             Strategy.__name__: Strategy,
         }
+        atexit.register(self._atexit_cleanup)
 
     def interact(self, msg):
         if self.env:
@@ -465,6 +467,14 @@ class Target:
         """Deactivates all drivers in reversed order they were activated"""
         for drv in reversed(self.drivers):
             self.deactivate(drv)
+
+    def _atexit_cleanup(self):
+        try:
+            self.cleanup()
+        except Exception as e:
+            print("An exception occured during cleanup, call the cleanup() "
+                  "method on targets yourself to handle exceptions explictly.")
+            print("Error: {}", e)
 
     def cleanup(self):
         """Clean up conntected drivers and resources in reversed order"""

commit 0ca24ef9a9143bb88df68c69fbfdef4ce2baf4c0

target: fix broken format string in print statement
We do not support python3.5 anymore, so we can use an f-string here.

Fixes: 0e9ddabc ("target: add atexit handler to ensure cleanup")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/target.py
@@ -475,7 +475,7 @@ class Target:
         except Exception as e:
             print("An exception occured during cleanup, call the cleanup() "
                   "method on targets yourself to handle exceptions explictly.")
-            print("Error: {}", e)
+            print(f"Error: {e}")
 
     def cleanup(self):
         """Clean up conntected drivers and resources in reversed order"""
68a1c65fd5399871851a10cbd58fa6367d223c63
Chris Fiege <cfi@pengutronix.de>
2021-04-30 17:58:03 +0200

    driver/power/digipower: Improve readability
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- labgrid/driver/power/digipower.py
+++ labgrid/driver/power/digipower.py
@@ -12,18 +12,8 @@ def power_set(host, port, index, value):
         cgi = "offs.cgi"
 
     suffixstring = "0000000000000000"
-    portstring = {
-        1: "10000000",
-        2: "01000000",
-        3: "00100000",
-        4: "00010000",
-        5: "00001000",
-        6: "00000100",
-        7: "00000010",
-        8: "00000001",
-    }
     r = requests.get(
-        "http://{}:{}/{}?led={}".format(host, port, cgi, portstring[index] + suffixstring),
+        f"http://{host}:{port}/{cgi}?led={1 << 8 - index:08b}{suffixstring}",
         auth=("snmp", "1234"),
     )
     r.raise_for_status()
@@ -33,18 +23,11 @@ def power_get(host, port, index):
     index = int(index)
     assert 1 <= index <= 8
 
-    # get the contents of the status page
-    r = requests.get("http://{}:{}/status.xml".format(host, port), auth=("snmp", "1234"))
+    r = requests.get(
+        f"http://{host}:{port}/status.xml",
+        auth=("snmp", "1234"),
+    )
     r.raise_for_status()
-    states = {"0": False, "1": True}
-    ports = {
-        1: 10,
-        2: 11,
-        3: 12,
-        4: 13,
-        5: 14,
-        6: 15,
-        7: 16,
-        8: 17
-    }
-    return states[r.text.split(',')[ports[index]]]
+
+    state = r.text.split(',')[9 + index]
+    return bool(int(state, 2))

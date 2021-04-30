e9db7a0ef835b5845bcd83b59f1bf3c4eaff52ba
Chris Fiege <c.fiege@pengutronix.de>
2021-04-30 12:28:34 +0200

    driver/power/digipower: Make driver work
    This changes basically make this driver work:
    * Indexing of the `portstring` and `ports` -dicts has probably never worked
      the way it was done: `index` is explicitly casted to `int` but indices
      have been `str`.
    * `response.content` is the bytes-response from the server.
      The way it was used before probably never worked the way it was done:
      A byte-array can not be splitted using a string.
      When using `response.text` decoding is done by `requests` what is
      probably the best idea here.
    
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- labgrid/driver/power/digipower.py
+++ labgrid/driver/power/digipower.py
@@ -13,14 +13,14 @@ def power_set(host, port, index, value):
 
     suffixstring = "0000000000000000"
     portstring = {
-        "1": "10000000",
-        "2": "01000000",
-        "3": "00100000",
-        "4": "00010000",
-        "5": "00001000",
-        "6": "00000100",
-        "7": "00000010",
-        "8": "00000001",
+        1: "10000000",
+        2: "01000000",
+        3: "00100000",
+        4: "00010000",
+        5: "00001000",
+        6: "00000100",
+        7: "00000010",
+        8: "00000001",
     }
     r = requests.get(
         "http://{}:{}/{}?led={}".format(host, port, cgi, portstring[index] + suffixstring),
@@ -38,13 +38,13 @@ def power_get(host, port, index):
     r.raise_for_status()
     states = {"0": False, "1": True}
     ports = {
-        "1": 10,
-        "2": 11,
-        "3": 12,
-        "4": 13,
-        "5": 14,
-        "6": 15,
-        "7": 16,
-        "8": 17
+        1: 10,
+        2: 11,
+        3: 12,
+        4: 13,
+        5: 14,
+        6: 15,
+        7: 16,
+        8: 17
     }
-    return states[r.content.split(',')[ports[index]]]
+    return states[r.text.split(',')[ports[index]]]

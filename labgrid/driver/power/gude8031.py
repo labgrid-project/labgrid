3bec1b6ec30a6ddbd9b6be7fdec34a6bccb4b0bb
Chris Fiege <c.fiege@pengutronix.de>
2021-05-12 16:29:34 +0200

    driver/power/gude8031: Make requests less expensive
    With this change we reduce the amount of information requested from the
    power switch per request to the bare minimum. There is no need to
    request more information than we will actually consume.
    
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- labgrid/driver/power/gude8031.py
+++ labgrid/driver/power/gude8031.py
@@ -3,7 +3,14 @@ import requests
 # Driver has been tested with:
 # Gude Expert Power Control 8031()
 
-# Components Parameter is static, defines how to interact whith power controller
+# HTTP-GET API is defined in the Gude EPC-HTTP-Interface specification:
+# http://wiki.gude.info/EPC_HTTP_Interface
+#
+# The `components=<N>` parameter defines which status information are
+# included into the returned JSON.
+# * `components=0` happily returns an empty response but still switches the
+#   outputs as requestd.
+# * `components=1` only includes the output's state into the JSON.
 
 PORT = 80
 
@@ -13,7 +20,7 @@ def power_set(host, port, index, value):
     # access the web interface...
     value = 1 if value else 0
     r = requests.get(
-        "http://{}:{}/status.json?components=769&cmd=1&p={}&s={}".format(host, port, index, value)
+        "http://{}:{}/status.json?components=0&cmd=1&p={}&s={}".format(host, port, index, value)
     )
     r.raise_for_status()
 
@@ -22,7 +29,7 @@ def power_get(host, port, index):
     assert 1 <= index <= 8
 
     # get the component status
-    r = requests.get("http://{}:{}/status.json?components=575235".format(host, port))
+    r = requests.get("http://{}:{}/status.json?components=1".format(host, port))
     r.raise_for_status()
 
     state = r.json()['outputs'][index - 1]['state']

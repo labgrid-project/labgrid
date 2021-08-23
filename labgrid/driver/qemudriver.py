labgrid-project/labgrid

commit b3faf8e7367a17ca46aff65b291673ceb34bc1af

driver/qemudriver: configure paths and images via config
To support relative paths in a labgrid environment, such as a labgrid test suite
embedded in a ptxdist project, the QEMUDriver needs to support relative paths
via the configuration file.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/driver/qemudriver.py
@@ -79,22 +79,27 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
         self._socket.bind(sockpath)
         self._socket.listen(0)
 
+        self.qemu_bin = self.target.env.config.get_tool(self.qemu_bin)
+        if self.qemu_bin is None:
+            raise KeyError(
+                "QEMU Binary Path not configured in tools configuration key")
         self._cmd = [self.qemu_bin]
 
-        if self.kernel:
+        if self.kernel is not None:
             self._cmd.append("-kernel")
-            self._cmd.append(self.kernel)
-        if self.rootfs:
+            self._cmd.append(
+                self.target.env.config.get_image_path(self.kernel))
+        if self.rootfs is not None:
             self._cmd.append("-fsdev")
             self._cmd.append(
                 "local,id=rootfs,security_model=none,path={}".format(
-                    self.rootfs))
+                    self.target.env.config.get_path(self.rootfs)))
             self._cmd.append("-device")
             self._cmd.append(
                 "virtio-9p-device,fsdev=rootfs,mount_tag=/dev/root")
-        if self.dtb:
+        if self.dtb is not None:
             self._cmd.append("-dtb")
-            self._cmd.append(self.dtb)
+            self._cmd.append(self.target.env.config.get_image_path(self.dtb))
 
         self._cmd.extend(shlex.split(self.extra_args))
         self._cmd.append("-S")
@@ -106,8 +111,6 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
         self._cmd.append(self.cpu)
         self._cmd.append("-m")
         self._cmd.append(self.memory)
-        self._cmd.append("-kernel")
-        self._cmd.append(self.kernel)
         self._cmd.append("-nographic")
         self._cmd.append("-append")
         self._cmd.append(
@@ -161,7 +164,8 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
     def monitor_command(self, command):
         """Execute a monitor_command via the QMP"""
         if not self.status:
-            raise ExecutionError("Can't use monitor command on non-running target")
+            raise ExecutionError(
+                "Can't use monitor command on non-running target")
         self.qmp.execute(command)
 
     def _read(self, size=1, timeout=10):

commit 4eff302c0943e50200df2da3156f5feea6fdb909

driver: qemudriver: fix tool lookup for multiple calls to on_activate()
When the QEmuDriver is activated, then deactivated and activated again,
the tool lookup for "qemu_bin" is only done correctly for the first
activation. For all subsequent activations, the path is used incorrectly
as the tool label (key).

Fix that by storing the tool path in a different variable than the key,
so multiple activations work correctly.

Fixes: b3faf8e7 ("driver/qemudriver: configure paths and images via config")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/qemudriver.py
@@ -99,11 +99,11 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
         self._socket.bind(sockpath)
         self._socket.listen(0)
 
-        self.qemu_bin = self.target.env.config.get_tool(self.qemu_bin)
-        if self.qemu_bin is None:
+        qemu_bin = self.target.env.config.get_tool(self.qemu_bin)
+        if qemu_bin is None:
             raise KeyError(
                 "QEMU Binary Path not configured in tools configuration key")
-        self._cmd = [self.qemu_bin]
+        self._cmd = [qemu_bin]
 
         boot_args = []
 
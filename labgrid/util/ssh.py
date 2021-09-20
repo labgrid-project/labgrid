labgrid-project/labgrid

commit 2c0eb9d78a014bff97d4318d31d55bdb23f8499e

util/ssh: add more specific SSH error message
Newer SSH versions have a more specific error message for the
control-master check, add it to the output check.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/util/ssh.py
@@ -392,7 +392,7 @@ class SSHConnection:
         if check == 0:
             self._logger.debug("Found existing control socket")
             return True
-        if b"No such file or directory" in stdout:
+        if b"No such file or directory" or b"No ControlPath specified" in stdout:
             self._logger.debug("No existing control socket found")
             return False
 

commit eda360e1ff182a98c7b082347832b6916d50ccb6

util/ssh: fix control master check
The first part of the condition always evaluates to True, so the output
of the SSH check command is never considered.

Fortunately, this only results in wrong messages being logged in the
worst case.

Fix that by actually checking if stdout contains
b"No such file or directory".

Fixes: 2c0eb9d7 ("util/ssh: add more specific SSH error message")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/util/ssh.py
@@ -395,7 +395,7 @@ class SSHConnection:
         if check == 0:
             self._logger.debug("Found existing control socket")
             return True
-        if b"No such file or directory" or b"No ControlPath specified" in stdout:
+        if b"No such file or directory" in stdout or b"No ControlPath specified" in stdout:
             self._logger.debug("No existing control socket found")
             return False
 
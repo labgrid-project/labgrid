labgrid-project/labgrid

commit ef401836bda6937c49fe7677cbc71baa3e0c9d91

remote/client: catch common errors and show a help message
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>
[r.czerwinski@pengutronix.de: Renamed duplicated test name]
Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/remote/client.py
@@ -20,7 +20,7 @@ from autobahn.asyncio.wamp import ApplicationRunner, ApplicationSession
 
 from .common import ResourceEntry, ResourceMatch, Place, enable_tcp_nodelay
 from ..environment import Environment
-from ..exceptions import NoDriverFoundError
+from ..exceptions import NoDriverFoundError, NoResourceFoundError, InvalidConfigError
 from ..resource.remote import RemotePlaceManager, RemotePlace
 from ..util.timeout import Timeout
 from ..util.dict import diff_dict, flat_dict
@@ -784,7 +784,7 @@ def main():
         '--debug',
         action='store_true',
         default=False,
-        help="enable debug mode"
+        help="enable debug mode (show python tracebacks)"
     )
     parser.add_argument(
         '-v',
@@ -945,12 +945,43 @@ def main():
                 session.loop.run_until_complete(args.func(session))
             else:
                 args.func(session)
+        except NoResourceFoundError as e:
+            if args.debug:
+                traceback.print_exc()
+            else:
+                print("{}: error: {}".format(parser.prog, e), file=sys.stderr)
+            print('\n'.join(["",
+                "This may be caused by disconnected exporter or wrong match entries.",
+                "You can use the 'show' command to all matching resources.",
+            ]), file=sys.stderr)
+            exitcode = 1
+        except NoDriverFoundError as e:
+            if args.debug:
+                traceback.print_exc()
+            else:
+                print("{}: error: {}".format(parser.prog, e), file=sys.stderr)
+            print('\n'.join(["",
+                "This is likely caused by an error or missing driver in the environment configuration.",
+            ]), file=sys.stderr)
+            exitcode = 1
+        except InvalidConfigError as e:
+            if args.debug:
+                traceback.print_exc()
+            else:
+                print("{}: error: {}".format(parser.prog, e), file=sys.stderr)
+            print('\n'.join(["",
+                "This is likely caused by an error in the environment configuration or invalid",
+                "resource information provided by the coordinator.",
+            ]), file=sys.stderr)
+            exitcode = 1
         except Error as e:
             if args.debug:
                 traceback.print_exc()
             else:
                 print("{}: error: {}".format(parser.prog, e), file=sys.stderr)
             exitcode = 1
+        except KeyboardInterrupt:
+            exitcode = 0
         except:
             traceback.print_exc()
             exitcode = 2

commit fc277de5dd73fad456b11bc9afe0cf9174a0d3f7

remote/client: exit with 1 on KeyboardInterrupt
When concatenating commands with '&&' or using 'set -e', a keyboard
interrupt should lead to no further commands being executed.

To allow this, rather exit with 1 than 0 on keyboard interrupt.

Fixes: ef401836 ("remote/client: catch common errors and show a help message")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/client.py
@@ -1904,7 +1904,7 @@ def main():
                 print(f"{parser.prog}: error: {e}", file=sys.stderr)
             exitcode = 1
         except KeyboardInterrupt:
-            exitcode = 0
+            exitcode = 1
         except Exception:  # pylint: disable=broad-except
             traceback.print_exc()
             exitcode = 2
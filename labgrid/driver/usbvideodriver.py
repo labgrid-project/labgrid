13dce1c3bfc016a347f489ff6e8c44a2d06ab963
Jan Luebbe <jlu@pengutronix.de>
2021-10-28 10:13:25 +0200

    driver/usbvideodriver: fix typos
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/driver/usbvideodriver.py
+++ labgrid/driver/usbvideodriver.py
@@ -40,7 +40,7 @@ class USBVideoDriver(Driver, VideoProtocol):
                 ("mid", "image/jpeg,width=1280,height=720,framerate=10/1"),
                 ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
                 ])
-        raise InvalidConfigError("Unkown USB video device {:04x}:{:04x}".format(*match))
+        raise InvalidConfigError("Unknown USB video device {:04x}:{:04x}".format(*match))
 
     def select_caps(self, hint=None):
         default, variants = self.get_qualities()
@@ -49,7 +49,7 @@ class USBVideoDriver(Driver, VideoProtocol):
             if name == variant:
                 return caps
         raise InvalidConfigError(
-            f"Unkown video format {variant} for device {self.video.vendor_id:04x}:{self.video.model_id:04x}"  # pylint: disable=line-too-long
+            f"Unknown video format {variant} for device {self.video.vendor_id:04x}:{self.video.model_id:04x}"  # pylint: disable=line-too-long
         )
 
     def get_pipeline(self, path, caps, controls=None):
@@ -66,7 +66,7 @@ class USBVideoDriver(Driver, VideoProtocol):
         elif match == (0x534d, 0x2109):
             inner = None  # just forward the jpeg frames
         else:
-            raise InvalidConfigError("Unkown USB video device {:04x}:{:04x}".format(*match))
+            raise InvalidConfigError("Unknown USB video device {:04x}:{:04x}".format(*match))
 
         pipeline = f"v4l2src device={path} "
         if controls:
@@ -98,7 +98,7 @@ class USBVideoDriver(Driver, VideoProtocol):
             stdout=subprocess.DEVNULL,
         )
 
-        # wait until one subprocess has termianted
+        # wait until one subprocess has terminated
         while True:
             try:
                 tx.wait(timeout=0.1)

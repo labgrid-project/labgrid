ada2f9be0020a071a90e94e0957d2111e18d0b87
kiran ostrolenk <kiran.ostrolenk@codethink.co.uk>
2021-11-08 17:26:08 +0000

    driver/power: add network power driver for TP-Link power strips
    Implemented using the python-kasa library.
    
    Signed-off-by: kiran ostrolenk <kiran.ostrolenk@codethink.co.uk>
    [r.czerwinski@pengutronix.de: added kasa-requirements to dev-requirements.txt]
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

---
+++ labgrid/driver/power/tplink.py
@@ -0,0 +1,34 @@
+""" Tested with TP Link KP303, and should be compatible with any strip supported by kasa """
+
+import asyncio
+from kasa import SmartStrip
+
+
+async def _power_set(host, port, index, value):
+    """We embed the coroutines in an `async` function to minimise calls to `asyncio.run`"""
+    assert port is None
+    index = int(index)
+    strip = SmartStrip(host)
+    await strip.update()
+    assert (
+        len(strip.children) > index
+    ), "Trying to access non-existant plug socket on strip"
+    if value is True:
+        await strip.children[index].turn_on()
+    elif value is False:
+        await strip.children[index].turn_off()
+
+
+def power_set(host, port, index, value):
+    asyncio.run(_power_set(host, port, index, value))
+
+
+def power_get(host, port, index):
+    assert port is None
+    index = int(index)
+    strip = SmartStrip(host)
+    asyncio.run(strip.update())
+    assert (
+        len(strip.children) > index
+    ), "Trying to access non-existant plug socket on strip"
+    return strip.children[index].is_on

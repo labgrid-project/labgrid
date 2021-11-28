labgrid-project/labgrid

commit 5204c4a04ccf0fbd7a9b1b3f2aed18037c4a554a

doc-requirements: update sphinx to 1.8.1
This needed a local docstring for PtxExpect, as it would otherwise fail
to resolve references from pexpect's docstring in our sphinx context.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/util/expect.py
@@ -9,6 +9,7 @@ class PtxExpect(pexpect.spawn):
     """
 
     def __init__(self, driver, logfile=None, timeout=30, cwd=None):
+        "Initializes a pexpect spawn instanse with required configuration"
         self.driver = driver
         self.logfile = logfile
         self.linesep = b"\n"
@@ -30,7 +31,7 @@ class PtxExpect(pexpect.spawn):
         return self.driver.write(b)
 
     def read_nonblocking(self, size=1, timeout=-1):
-        """ Pexpects needs a nonblocking read function, simply use pyserial with a timeout of 0"""
+        "Pexpects needs a nonblocking read function, simply use pyserial with a timeout of 0"
         assert timeout is not None
         if timeout == -1:
             timeout = self.timeout

commit 755324cc0cbfc1d704f38b336b3006eb4377c1f2

util/expect: Improve __init__ method for wrapper
Fix a typo, improve the sentence and remove unneeded newlines. The
newlines seem to be left over from a previous state.

Fixes: 5204c4a0 update sphinx to 1.8.1
Fixes: 56d5947a configure and run yapf

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

labgrid/util/expect.py
@@ -9,14 +9,10 @@ class PtxExpect(pexpect.spawn):
     """
 
     def __init__(self, driver):
-        "Initializes a pexpect spawn instanse with required configuration"
+        "Initializes a pexpect spawn instance with the required configuration"
         self.driver = driver
         self.linesep = b"\n"
-        pexpect.spawn.__init__(
-            self,
-            None,
-            maxread=1,
-        )
+        pexpect.spawn.__init__(self, None, maxread=1)
 
     def send(self, s):
         "Write to underlying transport, return number of bytes written"
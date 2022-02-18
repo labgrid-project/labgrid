labgrid-project/labgrid

commit b5740cfc6182d7e6cc105f591f3789e4a7e93a15

driver/power: add networkpowerurl model for NetworkPowerPort
This is a generic power driver which use the "host" value of the
NetworkPowerPort resource as a REST API URL.

Signed-off-by: Kjeld Flarup <kfa@deif.com>

labgrid/driver/power/simplerest.py
@@ -1,0 +1,25 @@
+""" Simple rest interface for Power Port. Used for ex. misc Raspberry Pi configs
+    Author: Kjeld Flarup <kfa@deif.com>
+
+    The URL given in hosts in exporter.yaml must replace {value} with '0' or '1'
+    It is optional whether to use {index} or not.
+
+  NetworkPowerPort:
+      model: simplerest
+      host: 'http://172.17.180.53:9999/relay/{index}/{value}'
+      index: 0
+"""
+
+import requests
+
+def set(host, index, value):
+    index = int(index)
+    value = 1 if value else 0
+    r = requests.get(host.format(value=value,index=index))
+
+def get(host, index):
+    index = int(index)
+    # remove trailing /
+    r = requests.get(host.format(value='',index=index).rstrip('/'))
+    r.raise_for_status()
+    return r.text == '1'

commit 1567c7dca4298edc7f8d20ca6a61332ae15b0a08

driver: power: simplerest: raise error on power_set() if one occurred
Fixes: b5740cfc ("driver/power: add networkpowerurl model for NetworkPowerPort")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/power/simplerest.py
@@ -17,7 +17,8 @@ def power_set(host, port, index, value):
 
     index = int(index)
     value = 1 if value else 0
-    requests.get(host.format(value=value, index=index))
+    r = requests.get(host.format(value=value, index=index))
+    r.raise_for_status()
 
 def power_get(host, port, index):
     assert port is None
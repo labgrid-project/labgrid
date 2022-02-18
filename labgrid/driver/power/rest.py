labgrid-project/labgrid

commit 2a8ac47c4e58e0dba139c786a4d18541cc03f9f9

driver/power: add support for REST based relays controlling power
This is different from simplerest driver, as it relies on REST interface using
GET to query status, and PUT to change status, where as simplerest driver uses
GET for both purposes.

These two models are therefore inherently incompatible, and supplements each
other.

Signed-off-by: Esben Haabendal <esben@geanix.com>

labgrid/driver/power/rest.py
@@ -1,0 +1,26 @@
+"""Rest interface for controlling power port, using PUT / GET on a URL.
+
+  NetworkPowerPort:
+      model: rest
+      host: 'http://192.168.0.42/relay/{index}/value'
+      index: 3
+
+  Will do a GET request to http://192.168.0.42/relay/3/value to get current
+  relay state, expecting a response of either '0' (relay off) or '1' (relay
+  on), and a PUT request to http://192.168.0.42/relay/3/value with request
+  data of '0' or '1' to change relay state.
+
+"""
+
+import requests
+
+def power_set(host, port, index, value):
+    assert port is None
+    value = b"1" if value else b"0"
+    requests.put(host.format(index=index), data=value)
+
+def power_get(host, port, index):
+    assert port is None
+    r = requests.get(host.format(index=index))
+    r.raise_for_status()
+    return r.text == "1"

tests/test_powerdriver.py
@@ -167,6 +167,7 @@ class TestNetworkPowerDriver:
         import labgrid.driver.power.gude24
         import labgrid.driver.power.netio
         import labgrid.driver.power.netio_kshell
+        import labgrid.driver.power.rest
         import labgrid.driver.power.sentry
 
     def test_import_backend_siglent(self):

commit 94b636a2204d833201271188fb982decfb8653b0

driver: power: rest: raise error on power_set() if one occurred
Fixes: 2a8ac47c ("driver/power: add support for REST based relays controlling power")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/power/rest.py
@@ -17,7 +17,8 @@ import requests
 def power_set(host, port, index, value):
     assert port is None
     value = b"1" if value else b"0"
-    requests.put(host.format(index=index), data=value)
+    r = requests.put(host.format(index=index), data=value)
+    r.raise_for_status()
 
 def power_get(host, port, index):
     assert port is None
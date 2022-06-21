cde4e2a35ed471fad769ef4bf75bf4d062e9e1d8
Bastian Krause <bst@pengutronix.de>
2022-06-21 16:29:21 +0200

    tests/qemudriver: add on/off and read/write tests
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- tests/test_qemudriver.py
+++ tests/test_qemudriver.py
@@ -41,9 +41,46 @@ def qemu_driver(qemu_target):
         rootfs='rootfs')
     return q
 
+@pytest.fixture
+def qemu_mock(mocker):
+    popen_mock = mocker.patch('subprocess.Popen')
+    popen_mock.return_value.wait.return_value = 0
+    popen_mock.return_value.stdout.readline.return_value = b"""
+    {
+      "QMP": {
+        "version": {}
+      },
+      "return": {}
+    }
+    """
+
+    select_mock = mocker.patch('select.select')
+    select_mock.return_value = True, None, None
+
+    socket_mock = mocker.patch('socket.socket')
+    socket_mock.return_value.accept.return_value = mocker.MagicMock(), ''
+
 def test_qemu_instance(qemu_target, qemu_driver):
     assert (isinstance(qemu_driver, QEMUDriver))
 
 def test_qemu_activate_deactivate(qemu_target, qemu_driver):
     qemu_target.activate(qemu_driver)
+    qemu_target.deactivate(qemu_driver)
+
+def test_qemu_on_off(qemu_target, qemu_driver, qemu_mock):
+    qemu_target.activate(qemu_driver)
+
+    qemu_driver.on()
+    qemu_driver.off()
+
+    qemu_target.deactivate(qemu_driver)
+
+def test_qemu_read_write(qemu_target, qemu_driver, qemu_mock):
+    qemu_target.activate(qemu_driver)
+
+    qemu_driver.on()
+    qemu_driver.read()
+    qemu_driver.read(max_size=10)
+    qemu_driver.write(b'abc')
+
     qemu_target.deactivate(qemu_driver)

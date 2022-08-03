labgrid-project/labgrid

commit da0a81d9c639e19b75ff7ebb3db19241262e3d11

driver/power: add support for Siglent SPD3000X series power supplies
Signed-off-by: Esben Haabendal <esben@geanix.com>

labgrid/driver/power/siglent.py
@@ -1,0 +1,21 @@
+""" tested with Siglent SPD3303X-E, and should be compatible with all SPD3000X series modules"""
+
+import vxi11
+
+
+def power_set(host, index, value):
+    index = int(index)
+    assert 1 <= index <= 2
+    value = "ON" if value else "OFF"
+    psu = vxi11.Instrument(host)
+    psu.write("OUTPUT CH{},{}".format(index, value))
+
+
+def power_get(host, index):
+    index = int(index)
+    assert 1 <= index <= 2
+    psu = vxi11.Instrument(host)
+    state = psu.ask("SYSTEM:STATUS?")
+    state = int(state, 16)
+    bitmask = 1 << (index + 3)
+    return bool(state & bitmask)

tests/test_powerdriver.py
@@ -166,3 +166,4 @@ class TestNetworkPowerDriver:
         import labgrid.driver.power.netio
         import labgrid.driver.power.netio_kshell
         import labgrid.driver.power.sentry
+        import labgrid.driver.power.siglent

commit 82ca4abcd930835c1d2cf57158dbede907427e47

driver/power/siglent: use backend function signatures expected by NetworkPowerDriver
When using the siglent power backend with the NetworkPowerDriver, this
error occurs:

  self = NetworkPowerDriver(target=Target(name='Test', env=None), name='power', state=<BindingState.active: 2>, delay=2.0)

      @Driver.check_active
      @step()
      def off(self):
  >       self.backend.power_set(self._host, self._port, self.port.index, False)
  E       TypeError: power_set() takes 3 positional arguments but 4 were given

  labgrid/driver/powerdriver.py:219: TypeError

The siglent backend does not follow the function signatures expected by
the NetworkPowerDriver, because it's missing the port parameter. Fix that
by adding the port parameter and making sure that it's None, just like
other backends do (apc, rest, sentry, simplerest, tplink).

Fixes: da0a81d9 ("driver/power: add support for Siglent SPD3000X series power supplies")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/power/siglent.py
@@ -3,7 +3,8 @@
 import vxi11
 
 
-def power_set(host, index, value):
+def power_set(host, port, index, value):
+    assert port is None
     index = int(index)
     assert 1 <= index <= 2
     value = "ON" if value else "OFF"
@@ -11,7 +12,8 @@ def power_set(host, index, value):
     psu.write(f"OUTPUT CH{index},{value}")
 
 
-def power_get(host, index):
+def power_get(host, port, index):
+    assert port is None
     index = int(index)
     assert 1 <= index <= 2
     psu = vxi11.Instrument(host)
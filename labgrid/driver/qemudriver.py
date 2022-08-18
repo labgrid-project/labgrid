labgrid-project/labgrid

commit 9bdacb264df62dc2c5f266598a3543496706c75c

driver/qemudriver: add support for Q35 machine
Signed-off-by: Emil Kronborg Andersen <emil.kronborg@protonmail.com>

labgrid/driver/qemudriver.py
@@ -121,6 +121,10 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
                 self._cmd.append(
                     f"if=sd,format={disk_format},file={disk_path},id=mmc0")
                 boot_args.append("root=/dev/mmcblk0p1 rootfstype=ext4 rootwait")
+            if self.machine == "q35":
+                self._cmd.append("-drive")
+                self._cmd.append(
+                    f"format={disk_format},file={disk_path}")
             elif self.machine == "pc":
                 self._cmd.append("-drive")
                 self._cmd.append(

commit 0230fd56dc41384c0c270d29105ab728d7722d2c

driver/qemudriver: fix disk attribute for vexpress-a9 machine
Since the q35 machine was added, using the vexpress-a9 machine with a
disk attribute ends up with a NotImplementedError. Fix that.

Fixes: 9bdacb26 ("driver/qemudriver: add support for Q35 machine")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/qemudriver.py
@@ -121,7 +121,7 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
                 self._cmd.append(
                     f"if=sd,format={disk_format},file={disk_path},id=mmc0")
                 boot_args.append("root=/dev/mmcblk0p1 rootfstype=ext4 rootwait")
-            if self.machine == "q35":
+            elif self.machine == "q35":
                 self._cmd.append("-drive")
                 self._cmd.append(
                     f"if=virtio,format={disk_format},file={disk_path}")
a9f0f9a72dac43edb0ab3e6e8bc49a5906cad368
Bastian Krause <bst@pengutronix.de>
2022-09-14 11:26:16 +0200

    remote/coordinator: drop unneeded u string prefix
    "The u prefix for strings is no longer necessary in Python >=3.0." [1]
    
    [1] https://pylint.pycqa.org/en/latest/user_guide/messages/warning/redundant-u-string-prefix.html
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- labgrid/remote/coordinator.py
+++ labgrid/remote/coordinator.py
@@ -889,7 +889,7 @@ class CoordinatorComponent(ApplicationSession):
 
 if __name__ == '__main__':
     runner = ApplicationRunner(
-        url=environ.get("WS", u"ws://127.0.0.1:20408/ws"),
+        url=environ.get("WS", "ws://127.0.0.1:20408/ws"),
         realm="realm1",
     )
     runner.run(CoordinatorComponent)

6eda7b181d4209a907bd45814c73f915c73fe5f2
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2022-10-19 09:47:03 +0200

    logging: use composition instead of inheritance
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/logging.py
+++ labgrid/logging.py
@@ -18,37 +18,18 @@ def basicConfig(**kwargs):
 
 
 
-class StepFormatter(Formatter):
+# Use composition instead of inheritance
+class StepFormatter:
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, color=None):
+        self.formatter = Formatter(*args)
+        self.color = color
 
     def format(self, record):
         if hasattr(record, "step"):
             return self.formt_step(record)
         else:
-            return self.format_normal(record)
-
-    def format_normal(self, record):
-        record.message = record.getMessage()
-        if self.usesTime():
-            record.asctime = self.formatTime(record, self.datefmt)
-        s = self.formatMessage(record)
-        if record.exc_info:
-            # Cache the traceback text to avoid converting it multiple times
-            # (it's constant anyway)
-            if not record.exc_text:
-                record.exc_text = self.formatException(record.exc_info)
-        if record.exc_text:
-            if s[-1:] != "\n":
-                s = s + "\n"
-            s = s + record.exc_text
-        if record.stack_info:
-            if s[-1:] != "\n":
-                s = s + "\n"
-            s = s + self.formatStack(record.stack_info)
-        return s
-
+            return self.formatter.format(record)
 
     def format_step(self, record):
         return f"  {record.getMessage}"

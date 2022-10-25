dbe9938cbba8742d71b56bdea9410144b1c3437b
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2022-10-25 14:58:23 +0200

    stepreporter: remove unused
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- labgrid/stepreporter.py
+++ labgrid/stepreporter.py
@@ -31,27 +31,3 @@ class StepReporter:
         step = event.step
         indent = '  '*step.level
         print(f"{indent}{event}")
-
-
-class StepLogger:
-    instance = None
-
-    def __init__(self):
-        self.logger = getLogger("StepLogger")
-        steps.subscribe(self.notify)
-
-    @classmethod
-    def start(cls):
-        """starts the StepLogger"""
-        assert cls.instance is None
-        cls.instance = cls()
-
-    @classmethod
-    def stop(cls):
-        """stops the StepLogger"""
-        assert cls.instance is not None
-        steps.unsubscribe(cls.notify)
-        cls.instance = None
-
-    def notify(self, event):
-        self.logger.log(15, event)

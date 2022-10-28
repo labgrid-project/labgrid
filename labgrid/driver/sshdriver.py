labgrid-project/labgrid

commit e874ae620eb7ee5aa4436fbe1c78807b55d61626

driver: close pipes from processes explicitly
When operating with pipes, we should close them expicitly when the
subprocess has terminated. This can be done easily with communicate().

Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/sshdriver.py
@@ -130,7 +130,8 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             subprocess_timeout = timeout + 5
             return_value = self.process.wait(timeout=subprocess_timeout)
             if return_value != 0:
-                stdout = self.process.stdout.readlines()
+                stdout, _ = self.process.communicate(timeout=subprocess_timeout)
+                stdout = stdout.split("\n")
                 for line in stdout:
                     self.logger.warning("ssh: %s", line.rstrip().decode(encoding="utf-8", errors="replace"))
 
@@ -453,6 +454,8 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
         if res != 0:
             self.logger.info("Socket already closed")
         shutil.rmtree(self.tmpdir)
+
+        self.process.communicate()
 
     def _start_keepalive(self):
         """Starts a keepalive connection via the own or external master."""

labgrid/driver/externalconsoledriver.py
@@ -54,6 +54,7 @@ class ExternalConsoleDriver(ConsoleExpectMixin, Driver, ConsoleProtocol):
         if not self.status:
             return
         if self._child.poll() is not None:
+            self._child.communicate()
             raise ExecutionError("child has vanished")
         self._child.terminate()
         try:

labgrid/driver/qemudriver.py
@@ -87,10 +87,10 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
             return
         self._child.terminate()
         try:
-            self._child.wait(1.0)
+            self._child.communicate(timeout=1)
         except subprocess.TimeoutExpired:
             self._child.kill()
-            self._child.wait(1.0)
+            self._child.communicate(timeout=1)
 
     def on_activate(self):
         self._tempdir = tempfile.mkdtemp(prefix="labgrid-qemu-tmp-")
@@ -204,6 +204,7 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
             self.qmp = QMPMonitor(self._child.stdout, self._child.stdin)
         except QMPError as exc:
             if self._child.poll() is not None:
+                self._child.communicate()
                 raise IOError(
                     f"QEMU process terminated with exit code {self._child.returncode}"
                 ) from exc
@@ -219,6 +220,7 @@ class QEMUDriver(ConsoleExpectMixin, Driver, PowerProtocol, ConsoleProtocol):
             return
         self.monitor_command('quit')
         if self._child.wait() != 0:
+            self._child.communicate()
             raise IOError
         self._child = None
         self.status = 0

labgrid/driver/usbaudiodriver.py
@@ -201,3 +201,6 @@ class USBAudioInputDriver(Driver):
 
         rx.terminate()
         tx.terminate()
+
+        rx.communicate()
+        tx.communicate()

labgrid/driver/usbvideodriver.py
@@ -161,3 +161,6 @@ class USBVideoDriver(Driver, VideoProtocol):
 
         rx.terminate()
         tx.terminate()
+
+        rx.communicate()
+        tx.communicate()

commit 237f4f24fdcae22808620201f4eb6bb5b2b82a91

driver/sshdriver: fix splitting bytestring on SSH master error
stdout is still a bytestring at this point, so we have to also use a
bytestring as delimiter.

Fixes: e874ae62 ("driver: close pipes from processes explicitly")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/sshdriver.py
@@ -131,7 +131,7 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             return_value = self.process.wait(timeout=subprocess_timeout)
             if return_value != 0:
                 stdout, _ = self.process.communicate(timeout=subprocess_timeout)
-                stdout = stdout.split("\n")
+                stdout = stdout.split(b"\n")
                 for line in stdout:
                     self.logger.warning("ssh: %s", line.rstrip().decode(encoding="utf-8", errors="replace"))
 
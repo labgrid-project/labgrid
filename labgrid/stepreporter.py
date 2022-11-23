3c1c259bbcfcb05b4ca2479d680b3abcdaf99ca4
Jan Luebbe <jlu@pengutronix.de>
2022-11-23 15:13:34 +0100

    stepreporter: fix singleton logic
    This avoids duplicated output if StepReporter instances are created by mistake
    and is still compatible with existing code.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/stepreporter.py
+++ labgrid/stepreporter.py
@@ -4,23 +4,21 @@ from .step import steps
 
 
 class StepReporter:
-    instance = None
+    _started = False
 
     @classmethod
     def start(cls):
         """starts the StepReporter"""
-        assert cls.instance is None
-        cls.instance = cls()
+        assert not cls._started
+        steps.subscribe(cls.notify)
+        cls._started = True
 
     @classmethod
     def stop(cls):
         """stops the StepReporter"""
-        assert cls.instance is not None
+        assert cls._started
         steps.unsubscribe(cls.notify)
-        cls.instance = None
-
-    def __init__(self):
-        steps.subscribe(StepReporter.notify)
+        cls._started = False
 
     @staticmethod
     def notify(event):

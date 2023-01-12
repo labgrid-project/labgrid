labgrid-project/labgrid

commit 99f83f84c34c122c82aa8833ec4cab3725335bf6

util/ssh: use rsync with --compress=1 --sparse
Especially disk images can be quite big because unallocated space between
partitions is written out as all zeroes. In my particular case, I had
a 3.3 GB eMMC image and rsync --compress=1 got the time down to nearly
a fifth and --sparse shrinked allocated block count on target tmpfs for
the same image to a tenth.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

labgrid/util/ssh.py
@@ -225,7 +225,7 @@ class SSHConnection:
     @_check_connected
     def put_file(self, local_file, remote_path):
         """Put a file onto the remote host"""
-        complete_cmd = ["rsync", "--copy-links", "-e",
+        complete_cmd = ["rsync", "--compress=1", "--sparse", "--copy-links", "-e",
                         " ".join(['ssh'] + self._get_ssh_args())]
         complete_cmd += [
             "{}".format(local_file),

commit 6340d7528fc4b4fb81f1dc521ffa89d95034b539

util/ssh: do not pass unexpected argument to rsync --compress
On rsync v3.2.7 --compress=1 fails with:

  rsync: --compress=1: option does not take an argument
  rsync error: syntax or usage error (code 1) at main.c(1795) [client=3.2.7]

rsync never expected an argument for --compress, but in previous
versions it was ignored, now it fails.

Fix this by dropping the argument.

Fixes: 99f83f84 ("util/ssh: use rsync with --compress=1 --sparse")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/util/ssh.py
@@ -322,7 +322,7 @@ class SSHConnection:
     @_check_connected
     def put_file(self, local_file, remote_path):
         """Put a file onto the remote host"""
-        complete_cmd = ["rsync", "--compress=1", "--sparse", "--copy-links", "--verbose", "--progress", "--times", "-e",
+        complete_cmd = ["rsync", "--compress", "--sparse", "--copy-links", "--verbose", "--progress", "--times", "-e",
                         " ".join(['ssh'] + self._get_ssh_args())]
         complete_cmd += [
             f"{local_file}",
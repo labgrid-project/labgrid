c413de1561b7bdcbb797d6bed8bcb3c67f7cae4c
Lukasz Mrugala (X) <lukaszx.mrugala@intel.com>
2023-05-04 10:36:19 +0200

    driver/power: add raritan pdu network power driver
    Based on the Eaton PDU NetworkPowerDriver implementation, uses SimpleSNMP.
    
    Signed-off-by: Lukasz Mrugala (X) <lukaszx.mrugala@intel.com>

---
+++ labgrid/driver/power/raritan.py
@@ -0,0 +1,38 @@
+"""Driver for the Raritan PDUs
+
+Documentation sources:
+* https://www.circitor.fr/Mibs/Html/P/PDU2-MIB.php
+* http://support.raritan.com/px3/version-3.0.3/user-guides/PX3-0B-v3.0-E.pdf
+* https://cdn1.raritan.com/download/pdu-g2/4.0.20/MIB_Usage_4.0.20_49038.pdf
+"""
+from ..exception import ExecutionError
+from ...util.snmp import SimpleSNMP
+
+
+OID = ".1.3.6.1.4.1.13742.6.4.1.2.1"
+NUMBER_OF_OUTLETS = 16
+
+
+def power_set(host, port, index, value):
+    assert 1 <= int(index) <= NUMBER_OF_OUTLETS
+
+    _snmp = SimpleSNMP(host, 'private', port=port)
+    outlet_control_oid = "{}.2.1.{}".format(OID, index)
+
+    _snmp.set(outlet_control_oid, str(int(value)))
+
+
+def power_get(host, port, index):
+    assert 1 <= int(index) <= NUMBER_OF_OUTLETS
+
+    _snmp = SimpleSNMP(host, 'public', port=port)
+    output_status_oid = "{}.3.1.{}".format(OID, index)
+
+    value = _snmp.get(output_status_oid)
+
+    if value == 7:  # On
+        return True
+    if value == 8:  # Off
+        return False
+
+    raise ExecutionError("failed to get SNMP value")

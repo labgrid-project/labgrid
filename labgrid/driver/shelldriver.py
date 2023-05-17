labgrid-project/labgrid

commit 40154266ba3a466821eaf26ff7a6a6e1aa570616

shelldriver: Silence kernel earlier
Moves up the call to silence the kernel to the console expect loop. This
ensures that the kernel has been silenced before the prompt is checked,
as dmesg output can interrupt it and cause failures.

Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>

labgrid/driver/shelldriver.py
@@ -79,9 +79,6 @@ class ShellDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
 
             self._put_ssh_key(keyfile_path)
 
-        # Turn off Kernel Messages to the console
-        self._run("dmesg -n 1")
-
     def on_deactivate(self):
         self._status = 0
 
@@ -133,6 +130,7 @@ class ShellDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
         # occours, we can't lose any data this way.
         last_before = b''
         did_login = False
+        did_silence_kernel = False
 
         while True:
             index, before, _, _ = self.console.expect(
@@ -141,10 +139,15 @@ class ShellDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             )
 
             if index == 0:
-                # we got a promt. no need for any further action to activate
-                # this driver.
-                self.status = 1
-                break
+                if did_login and not did_silence_kernel:
+                    # Silence the kernel and wait for another prompt
+                    self.console.sendline("dmesg -n 1")
+                    did_silence_kernel = True
+                else:
+                    # we got a prompt. no need for any further action to
+                    # activate this driver.
+                    self.status = 1
+                    break
 
             elif index == 1:
                 # we need to login

commit 1209a65e18402386d5e7054a5bad25c918bf8b91

shelldriver: fix missing kernel silencing on password less consoles
I've noticed increased test failures on my OpenWrt based systems, which
have passwordless root login by default due to more chatty kernel
console, which interleaves command outputs. I've found out, that there
is no `dmesg -n 1` command run anymore and tracked it down to the
changes done in commit 40154266ba3a ("shelldriver: Silence kernel
earlier"). This change wrongly assumes, that `dmesg -n 1` should be run
only on consoles which have login enabled. So fix it, by removing that
wrong assumption and thus run kernel silencer on all consoles.

Cc: Joshua Watt <Joshua.Watt@garmin.com>
Fixes: 40154266ba3a ("shelldriver: Silence kernel earlier")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

labgrid/driver/shelldriver.py
@@ -136,7 +136,7 @@ class ShellDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
             )
 
             if index == 0:
-                if did_login and not did_silence_kernel:
+                if not did_silence_kernel:
                     # Silence the kernel and wait for another prompt
                     self.console.sendline("dmesg -n 1")
                     did_silence_kernel = True
labgrid-project/labgrid

commit a3df4e6698880ab1f3408b2f7cea43dbbc886da8

stepreporter: deprecate it
The StepReporter should be replaced by the StepLogger and basicConfig
from labgrid.logging. It uses the python logging infrastructure and a
better output formatting.

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

labgrid/stepreporter.py
@@ -16,6 +16,15 @@ class StepReporter:
     @classmethod
     def start(cls):
         """starts the StepReporter"""
+        from warnings import warn
+        warn(
+            """
+            StepLogger is deprecated, use the StepLogger and basicConfig from labgrid.logging
+            instead which integrates with the python logging infrastructure.
+            """,
+            DeprecationWarning,
+            stacklevel=2,
+        )
         assert not cls._started
         steps.subscribe(cls.notify)
         cls._started = True

commit 4d48dd5aaa020849f8337865f9532c1461bb9649

stepreporter: fix class name in DeprecationWarning
Fixes: a3df4e66 ("stepreporter: deprecate it")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/stepreporter.py
@@ -19,7 +19,7 @@ class StepReporter:
         from warnings import warn
         warn(
             """
-            StepLogger is deprecated, use the StepLogger and basicConfig from labgrid.logging
+            StepReporter is deprecated, use the StepLogger and basicConfig from labgrid.logging
             instead which integrates with the python logging infrastructure.
             """,
             DeprecationWarning,
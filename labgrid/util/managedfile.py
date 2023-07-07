labgrid-project/labgrid

commit d42ac82b7a70eacb12575daadc859cec2dc45127

util/managedfile: heuristically detect NFS to avoid copy
With #381, the fastboot client command would always attempt rsync'ing
the local file to the remote. It can be preferable to avoid this copy
if we the file is located on a NFS accessible to both the local and
remote system. This improves user experience because uploading to DUT
and download from labgrid-client system can happen in parallel.

To detect if the file is the same we use stat(1) on the same path on
both local and remote system and compare inode number, total size
and birth/modification timestamps.

This approach requires GNU coreutils' stat(1) to be available on
both local and remote system. Besides parsing ls -l, there seems
to be no fully POSIX alternative to get the required information
without writing C.

If detect_nfs is False, stat(1) is unavailable or incompatible with
--printf, or the stat(1) output of both systems don't match,
the rsync would be attempted as usual.

Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

labgrid/util/managedfile.py
@@ -1,5 +1,7 @@
 import hashlib
+import logging
 import os
+import subprocess
 
 import attr
 
@@ -29,20 +31,15 @@ class ManagedFile:
     resource = attr.ib(
         validator=attr.validators.instance_of(Resource),
     )
+    detect_nfs = attr.ib(default=True, validator=attr.validators.instance_of(bool))
 
     def __attrs_post_init__(self):
         if not os.path.isfile(self.local_path):
             raise FileNotFoundError("Local file {} not found".format(self.local_path))
-
-        username = get_user()
-        hasher = hashlib.sha256()
-        with open(self.local_path, 'rb') as f:
-            for block in iter(lambda: f.read(1048576), b''):
-                hasher.update(block)
-        self.hash = hasher.hexdigest()
-        self.rpath = "/tmp/labgrid-{user}/{hash}/".format(
-            user=username, hash=self.hash
-        )
+        self.logger = logging.getLogger("{}".format(self))
+        self.hash = None
+        self.rpath = None
+        self._on_nfs_cached = None
 
     def sync_to_resource(self):
         """sync the file to the host specified in a resource
@@ -53,11 +50,53 @@ class ManagedFile:
         if isinstance(self.resource, NetworkResource):
             host = self.resource.host
             conn = sshmanager.open(host)
+
+            if self._on_nfs(conn):
+                return # nothing to do
+
+            self.rpath = "/tmp/labgrid-{user}/{hash}/".format(
+                user=get_user(), hash=self.get_hash()
+            )
             conn.run_check("mkdir -p {}".format(self.rpath))
             conn.put_file(
                 self.local_path,
                 "{}{}".format(self.rpath, os.path.basename(self.local_path))
             )
+
+    def _on_nfs(self, conn):
+        if self._on_nfs_cached is not None:
+            return self._on_nfs_cached
+
+        if not self.detect_nfs:
+            return False
+
+        self._on_nfs_cached = False
+
+        fmt = "inode=%i,size=%s,birth=%W,modified=%Y"
+        local = subprocess.run(["stat", "--format", fmt, self.local_path],
+                               stdout=subprocess.PIPE)
+        if local.returncode != 0:
+            self.logger.debug("local: stat: unsuccessful error code %d", local.returncode)
+            return False
+
+        remote = conn.run("stat --format '{}' {}".format(fmt, self.local_path),
+                          decodeerrors="backslashreplace")
+        if remote[2] != 0:
+            self.logger.debug("remote: stat: unsuccessful error code %d", remote[2])
+            return False
+
+        localout = local.stdout.decode("utf-8", "backslashreplace").split('\n')
+        localout.pop() # remove trailing empty element
+
+        if remote[0] != localout:
+            self.logger.debug("state: local (%s) and remote (%s) output don't match",
+                              remote[0], localout)
+            return False
+
+        self.rpath = os.path.dirname(self.local_path) + "/"
+        self._on_nfs_cached = True
+
+        return True
 
     def get_remote_path(self):
         """Retrieve the remote file path
@@ -76,4 +115,14 @@ class ManagedFile:
         Returns:
             str: SHA256 hexdigest of the file
         """
+
+        if self.hash is not None:
+            return self.hash
+
+        hasher = hashlib.sha256()
+        with open(self.local_path, 'rb') as f:
+            for block in iter(lambda: f.read(1048576), b''):
+                hasher.update(block)
+        self.hash = hasher.hexdigest()
+
         return self.hash

tests/test_util.py
@@ -271,12 +271,26 @@ Test
 """
     )
     hash = hashlib.sha256(t.read().encode("utf-8")).hexdigest()
-    mf = ManagedFile(t, res)
+    mf = ManagedFile(t, res, detect_nfs=False)
     mf.sync_to_resource()
 
     assert os.path.isfile("/tmp/labgrid-{}/{}/test".format(getpass.getuser(), hash))
     assert hash == mf.get_hash()
     assert "/tmp/labgrid-{}/{}/test".format(getpass.getuser(), hash) == mf.get_remote_path()
+
+@pytest.mark.localsshmanager
+def test_remote_managedfile_on_nfs(target, tmpdir):
+    res = NetworkResource(target, "test", "localhost")
+    t = tmpdir.join("test")
+    t.write(
+"""
+Test
+"""
+    )
+    mf = ManagedFile(t, res, detect_nfs=True)
+    mf.sync_to_resource()
+
+    assert str(t) == mf.get_remote_path()
 
 def test_local_managedfile(target, tmpdir):
     import hashlib
@@ -289,7 +303,7 @@ Test
 """
     )
     hash = hashlib.sha256(t.read().encode("utf-8")).hexdigest()
-    mf = ManagedFile(t, res)
+    mf = ManagedFile(t, res, detect_nfs=False)
     mf.sync_to_resource()
 
     assert hash == mf.get_hash()

commit 22028070518e5ac799856c93a7bf1faa2826b94d

util/managedfile: resolve symlinks before comparing NFS paths
ManagedFile has an optimization to detect if the "local" file is
available over NFS at the remote side. In that case, rsync is skipped
and get_remote_path will just return the NFS path.

This is done by comparing stat(1) output of the absolute path on local
and remote host. If the absolute paths contains symlinks, this
comparison may not happen if the remote host doesn't contain the exact
same symlinks. Let's avoid this issue by canonicalizing the local path
first.

Fixes: d42ac82b7a70 ("util/managedfile: heuristically detect NFS to avoid copy")
Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

labgrid/util/managedfile.py
@@ -31,7 +31,7 @@ class ManagedFile:
     """
     local_path = attr.ib(
         validator=attr.validators.instance_of(str),
-        converter=lambda x: os.path.abspath(str(x))
+        converter=lambda x: os.path.realpath(str(x))
     )
     resource = attr.ib(
         validator=attr.validators.instance_of(Resource),

tests/test_flashscript.py
@@ -2,6 +2,7 @@ import pytest
 import subprocess
 import tempfile
 import attr
+import os
 from pathlib import Path
 from labgrid.driver.flashscriptdriver import FlashScriptDriver
 from labgrid.resource.common import ManagedResource
@@ -69,4 +70,4 @@ def test_argument_device_expansion(target, resource, driver):
 
 def test_argument_file_expansion(target, driver):
     value = capture_argument_expansion(driver, "file.local_path")
-    assert value == "/bin/sh"
+    assert os.path.samefile(value, "/bin/sh")
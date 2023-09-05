labgrid-project/labgrid

commit f9d4ccd72fd6fedfba0c830e79e742fd94775acf

remote/client: print full resource identifier on monitor
`labgrid-client monitor` prints resources as
`<exporter>/<group>/<name>`, while the resource match pattern has the
format `<exporter>/<group>/<class>/<name>`. The `/<name>` is optional.

So in `labgrid-client monitor`, the class name is missing. This is
confusing. Resource match patterns cannot be copy/pasted into
`labgrid-client add-match` or `labgrid-client add-named-match`.

Fix that by printing the full resource identifier for the monitor
subcommand.

Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/client.py
@@ -117,13 +117,13 @@ class ClientSession(ApplicationSession):
             group[resource_name].data = resource
         if self.monitor:
             if resource and not old:
-                print(f"Resource {exporter}/{group_name}/{resource_name} created: {resource}")
+                print(f"Resource {exporter}/{group_name}/{resource['cls']}/{resource_name} created: {resource}")
             elif resource and old:
-                print(f"Resource {exporter}/{group_name}/{resource_name} changed:")
+                print(f"Resource {exporter}/{group_name}/{resource['cls']}/{resource_name} changed:")
                 for k, v_old, v_new in diff_dict(flat_dict(old), flat_dict(resource)):
                     print(f"  {k}: {v_old} -> {v_new}")
             else:
-                print(f"Resource {exporter}/{group_name}/{resource_name} deleted")
+                print(f"Resource {exporter}/{group_name}/{resource['cls']}/{resource_name} deleted")
 
     async def on_place_changed(self, name, config):
         if not config:

commit 9e4fdc65b5dd8c34d43f8fe416cdd29b46333e63

remote/client: do not print deleted resource's class on monitor
On delete, resource is an empty dictionary:

  ERROR   labgrid.remote.: ClientSession.onUserError(): "While firing <bound method ClientSession.on_resource_changed of <labgrid.remote.client.ClientSession object at 0x7fcf1cbdff90>> subscribed under 8031289985454517."
  Traceback (most recent call last):
    File "/usr/ptx-venvs/labgrid/lib/python3.11/site-packages/txaio/aio.py", line 487, in done
      res = f.result()
            ^^^^^^^^^^
    File "/usr/ptx-venvs/labgrid/lib/python3.11/site-packages/labgrid/remote/client.py", line 126, in on_resource_changed
      print(f"Resource {exporter}/{group_name}/{resource['cls']}/{resource_name} deleted")
                                                ~~~~~~~~^^^^^^^
  KeyError: 'cls'

This can be reproduced easily by restarting an exporter while running
`labgrid-client monitor`.

We could publish the class, but that would only work for new exporters.
For now, simply replace the class name with "???".

Fixes: f9d4ccd7 ("remote/client: print full resource identifier on monitor")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/client.py
@@ -123,7 +123,7 @@ class ClientSession(ApplicationSession):
                 for k, v_old, v_new in diff_dict(flat_dict(old), flat_dict(resource)):
                     print(f"  {k}: {v_old} -> {v_new}")
             else:
-                print(f"Resource {exporter}/{group_name}/{resource['cls']}/{resource_name} deleted")
+                print(f"Resource {exporter}/{group_name}/???/{resource_name} deleted")
 
     async def on_place_changed(self, name, config):
         if not config:
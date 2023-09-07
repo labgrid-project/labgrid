b8f059fb4277e3ca01684b7a71ac89974ff1cc36
Joshua Watt <Joshua.Watt@garmin.com>
2023-09-07 08:36:05 -0600

    sshdriver: Prevent timeout from deadlock
    Using Popen.wait() on a process that has output sent to a pipe can
    potentially deadlock if the process produces enough output to fill the
    pipe, since it will stall and never terminate waiting for the pipe to
    have more space.
    
    Instead, use Popen.communicate() with the timeout parameter. This will
    consume all output until EOF (preventing the process from stalling due
    to a full pipe), and then check the return code. In the event of a
    timeout error, Popen.communicate() doesn't loose any data, so it's safe
    to call it again after the Popen.kill() in the exception handler.
    
    This likely was done this way because the timeout parameter was new in
    Python 3.3, but this shouldn't be a concern anymore
    
    Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>

--- tests/test_sshdriver.py
+++ tests/test_sshdriver.py
@@ -17,6 +17,8 @@ def ssh_driver_mocked_and_activated(target, mocker):
     instance_mock = mocker.MagicMock()
     popen.return_value = instance_mock
     instance_mock.wait = mocker.MagicMock(return_value=0)
+    instance_mock.communicate = mocker.MagicMock(return_value=(b"", b""))
+    instance_mock.returncode = 0
     SSHDriver(target, "ssh")
     s = target.get_driver("SSHDriver")
     return s
@@ -35,6 +37,8 @@ def test_create(target, mocker):
     instance_mock = mocker.MagicMock()
     popen.return_value = instance_mock
     instance_mock.wait = mocker.MagicMock(return_value=0)
+    instance_mock.communicate = mocker.MagicMock(return_value=(b"", b""))
+    instance_mock.returncode = 0
     s = SSHDriver(target, "ssh")
     assert isinstance(s, SSHDriver)
 

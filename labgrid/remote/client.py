labgrid-project/labgrid

commit cdebb1ccb9396f3dea759406bda0bb28862a02d2

remote/client: return exit code for ssh/scp/rsync/telnet/video/audio/console
Correctly set the exitcode for these subcommands.
For this we introduce an InteractiveCommandError, which only sets the
exitcode to the one received from the program.

Fixes https://github.com/labgrid-project/labgrid/issues/660

Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/client.py
@@ -47,6 +47,10 @@ class ServerError(Error):
     pass
 
 
+class InteractiveCommandError(Error):
+    pass
+
+
 class ClientSession(ApplicationSession):
     """The ClientSession encapsulates all the actions a Client can Invoke on
     the coordinator."""
@@ -842,14 +846,17 @@ class ClientSession(ApplicationSession):
                 raise
         if p.returncode:
             print("connection lost", file=sys.stderr)
-            return False
-        return True
+        return p.returncode
 
     async def console(self, place, target):
         while True:
             res = await self._console(place, target, 10.0, logfile=self.args.logfile,
                                       loop=self.args.loop, listen_only=self.args.listenonly)
-            if res or not self.args.loop:
+            if res:
+                exc = InteractiveCommandError("microcom error")
+                exc.exitcode = res
+                raise exc
+            if not self.args.loop:
                 break
             await asyncio.sleep(1.0)
     console.needs_target = True
@@ -1033,20 +1040,28 @@ class ClientSession(ApplicationSession):
         drv = self._get_ssh()
 
         res = drv.interact(self.args.leftover)
-        if res == 255:
-            print("connection lost (SSH error)", file=sys.stderr)
-        elif res:
-            print(f"connection lost (remote exit code {res})", file=sys.stderr)
+        if res:
+            exc = InteractiveCommandError("ssh error")
+            exc.exitcode = res
+            raise exc
 
     def scp(self):
         drv = self._get_ssh()
 
-        drv.scp(src=self.args.src, dst=self.args.dst)
+        res = drv.scp(src=self.args.src, dst=self.args.dst)
+        if res:
+            exc = InteractiveCommandError("scp error")
+            exc.exitcode = res
+            raise exc
 
     def rsync(self):
         drv = self._get_ssh()
 
-        drv.rsync(src=self.args.src, dst=self.args.dst, extra=self.args.leftover)
+        res = drv.rsync(src=self.args.src, dst=self.args.dst, extra=self.args.leftover)
+        if res:
+            exc = InteractiveCommandError("rsync error")
+            exc.exitcode = res
+            raise exc
 
     def sshfs(self):
         drv = self._get_ssh()
@@ -1084,7 +1099,9 @@ class ClientSession(ApplicationSession):
         args = ['telnet', str(ip)]
         res = subprocess.call(args)
         if res:
-            print("connection lost", file=sys.stderr)
+            exc = InteractiveCommandError("telnet error")
+            exc.exitcode = res
+            raise exc
 
     def video(self):
         place = self.get_acquired_place()
@@ -1115,14 +1132,22 @@ class ClientSession(ApplicationSession):
                 mark = '*' if default == name else ' '
                 print(f"{mark} {name:<10s} {caps:s}")
         else:
-            drv.stream(quality, controls=controls)
+            res = drv.stream(quality, controls=controls)
+            if res:
+                exc = InteractiveCommandError("gst-launch-1.0 error")
+                exc.exitcode = res
+                raise exc
 
     def audio(self):
         place = self.get_acquired_place()
         target = self._get_target(place)
         name = self.args.name
         drv = self._get_driver_or_new(target, "USBAudioInputDriver", name=name)
-        drv.play()
+        res = drv.play()
+        if res:
+            exc = InteractiveCommandError("gst-launch-1.0 error")
+            exc.exitcode = res
+            raise exc
 
     def _get_tmc(self):
         place = self.get_acquired_place()
@@ -1864,6 +1889,10 @@ def main():
         except ConnectionError as e:
             print(f"Could not connect to coordinator: {e}", file=sys.stderr)
             exitcode = 1
+        except InteractiveCommandError as e:
+            if args.debug:
+                traceback.print_exc(file=sys.stderr)
+            exitcode = e.exitcode
         except Error as e:
             if args.debug:
                 traceback.print_exc(file=sys.stderr)

commit 6df298f79a8346cf25e2b6c2322eebd3dfd36ae3

client: fix console --loop on microcom error
In certain situation, e.g. if the serial port vanishes (i.e. on-board
FTDI chips on power off), microcom returns with 1 and:

  Got EOF from port
  connection lost

The loop should still continue, so do not raise an exception in this
case.

This uncovers another problem: if the corresponding place is released,
ser2net is stopped from the exporter and the port is set to None. Check
that after updating the resource to prevent a crude AssertionError later
due to an unavailable port.

Fixes: cdebb1cc ("remote/client: return exit code for ssh/scp/rsync/telnet/video/audio/console")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/client.py
@@ -813,6 +813,10 @@ class ClientSession(ApplicationSession):
         # use zero timeout to prevent blocking sleeps
         target.await_resources([resource], timeout=0.0)
 
+        if not place.acquired:
+            print("place released")
+            return 255
+
         host, port = proxymanager.get_host_and_port(resource)
 
         # check for valid resources
@@ -856,11 +860,14 @@ class ClientSession(ApplicationSession):
         while True:
             res = await self._console(place, target, 10.0, logfile=self.args.logfile,
                                       loop=self.args.loop, listen_only=self.args.listenonly)
-            if res:
-                exc = InteractiveCommandError("microcom error")
-                exc.exitcode = res
-                raise exc
+            # place released
+            if res == 255:
+                break
             if not self.args.loop:
+                if res:
+                    exc = InteractiveCommandError("microcom error")
+                    exc.exitcode = res
+                    raise exc
                 break
             await asyncio.sleep(1.0)
     console.needs_target = True
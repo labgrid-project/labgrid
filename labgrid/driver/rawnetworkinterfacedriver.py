79235db08936ed9d594e447b776cabbf8f105a56
Joshua Watt <Joshua.Watt@garmin.com>
2024-01-19 13:45:17 -0700

    rawnetworkinterfacedriver: Move timeout to tcpdump
    Reworks the way that timeouts are handled so that instead of terminating
    the process in stop_record, tcpdump will exit after a set time. This is
    will allow for live streaming of packets in a future patch
    
    Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>

--- labgrid/driver/rawnetworkinterfacedriver.py
+++ labgrid/driver/rawnetworkinterfacedriver.py
@@ -53,14 +53,15 @@ class RawNetworkInterfaceDriver(Driver):
             )
 
     @Driver.check_active
-    @step(args=["filename", "count"])
-    def start_record(self, filename, *, count=None):
+    @step(args=["filename", "count", "timeout"])
+    def start_record(self, filename, *, count=None, timeout=None):
         """
         Starts tcpdump on bound network interface resource.
 
         Args:
             filename (str): name of a file to record to
             count (int): optional, exit after receiving this many number of packets
+            timeout (int): optional, number of seconds to capture packets before tcpdump exits
         Returns:
             Popen object of tcpdump process
         """
@@ -69,20 +70,18 @@ class RawNetworkInterfaceDriver(Driver):
         cmd = ["tcpdump", self.iface.ifname]
         if count is not None:
             cmd.append(str(count))
+        if timeout is not None:
+            cmd.append("--duration")
+            cmd.append(str(timeout))
         cmd = self._wrap_command(cmd)
         with open(filename, "wb") as outdata:
             self._record_handle = subprocess.Popen(cmd, stdout=outdata, stderr=subprocess.PIPE)
         return self._record_handle
 
     @Driver.check_active
-    @step(args=["timeout"])
     def stop_record(self, *, timeout=None):
         """
         Stops previously started tcpdump on bound network interface resource.
-
-        Args:
-            timeout (int): optional, maximum number of seconds to wait for the tcpdump process to
-                           terminate
         """
         try:
             self._stop(self._record_handle, timeout=timeout)
@@ -99,15 +98,16 @@ class RawNetworkInterfaceDriver(Driver):
         Args:
             filename (str): name of a file to record to
             count (int): optional, exit after receiving this many number of packets
-            timeout (int): optional, maximum number of seconds to wait for the tcpdump process to
-                           terminate
+            timeout (int): optional, number of seconds to capture packets before tcpdump exits
+        Returns:
+            Popen object of tcpdump process.
         """
         assert count or timeout
 
         try:
-            yield self.start_record(filename, count=count)
+            yield self.start_record(filename, count=count, timeout=timeout)
         finally:
-            self.stop_record(timeout=timeout)
+            self.stop_record()
 
     @Driver.check_active
     @step(args=["filename"])

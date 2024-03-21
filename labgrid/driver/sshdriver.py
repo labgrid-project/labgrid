labgrid-project/labgrid

commit 2c062e3ac6bb55308ddc9b2bdd3602b09f50f3fe

driver/sshdriver: store OpenSSH version in cached property
We will use the version multiple times and should not call ssh each time
since we do not expect the ssh version to suddenly change while running
labgrid.

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

labgrid/driver/sshdriver.py
@@ -9,6 +9,7 @@ import shutil
 import subprocess
 import tempfile
 import time
+from functools import cached_property
 
 import attr
 
@@ -432,10 +433,14 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
         """The SSHDriver is always connected, return 1"""
         return 1
 
-    def _scp_supports_explicit_sftp_mode(self):
+    @cached_property
+    def _ssh_version(self):
         version = subprocess.run(["ssh", "-V"], capture_output=True, text=True)
         version = re.match(r"^OpenSSH_(\d+)\.(\d+)", version.stderr)
-        major, minor = map(int, version.groups())
+        return map(int, version.groups())
+
+    def _scp_supports_explicit_sftp_mode(self):
+        major, minor = self._ssh_version
 
         # OpenSSH >= 8.6 supports explicitly using the SFTP protocol via -s
         if major == 8 and minor >= 6:

commit 7bba58936a337cf52e2d81753221395cdd97e7c6

driver/sshdriver: fix caching of _ssh_version
map() returns an iterator, which only works once. Return and cache a
tuple instead.

Fixes: 2c062e3ac6bb ("driver/sshdriver: store OpenSSH version in cached property")
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/driver/sshdriver.py
@@ -447,7 +447,7 @@ class SSHDriver(CommandMixin, Driver, CommandProtocol, FileTransferProtocol):
     def _ssh_version(self):
         version = subprocess.run(["ssh", "-V"], capture_output=True, text=True)
         version = re.match(r"^OpenSSH_(\d+)\.(\d+)", version.stderr)
-        return map(int, version.groups())
+        return tuple(int(x) for x in version.groups())
 
     def _scp_supports_explicit_sftp_mode(self):
         major, minor = self._ssh_version
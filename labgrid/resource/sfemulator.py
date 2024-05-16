19816f0fd02978a89a68ee13e81414796d652cb4
Simon Glass <sjg@chromium.org>
2024-05-16 14:28:35 -0600

    driver: Add driver for Dediprog SPI-flash emulator
    The EM100-pro is a SPI-flash emulator which supports a variety of chips.
    It can be used to provide an image to a booting system.
    
    Add a driver which allows images to be loaded ready for use. Provide a
    trace facility for use when the board does not boot.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

---
+++ labgrid/resource/sfemulator.py
@@ -0,0 +1,33 @@
+import attr
+
+from ..factory import target_factory
+from .common import NetworkResource, Resource
+from .base import SerialPort
+
+@target_factory.reg_resource
+@attr.s(eq=False)
+class SFEmulator(Resource):
+    """"This resource describes a Dediprog em100 SPI-Flash Emulator
+
+    This provides serial consoles along with reset control
+
+    Args:
+        serial (str): serial number of the em100 device, e.g. DP025143
+        chip (str): SPI-flash chip to emulate, e.g. W25Q64CV
+    """
+    serial = attr.ib(validator=attr.validators.instance_of(str))
+    chip = attr.ib(validator=attr.validators.instance_of(str))
+
+@target_factory.reg_resource
+@attr.s(eq=False)
+class NetworkSFEmulator(NetworkResource):
+    """"This resource describes a remote Dediprog em100 SPI-Flash Emulator
+
+    This provides serial consoles along with reset control
+
+    Args:
+        serial (str): serial number of the em100 device, e.g. DP025143
+        chip (str): SPI-flash chip to emulate, e.g. W25Q64CV
+    """
+    serial = attr.ib(validator=attr.validators.instance_of(str))
+    chip = attr.ib(validator=attr.validators.instance_of(str))

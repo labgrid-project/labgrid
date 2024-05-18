2d4614aba675367d6a121a176ff62288e8427d4a
Simon Glass <sjg@chromium.org>
2024-05-18 14:34:22 -0600

    strategy: Allow a board to be bootstrapped
    It is useful to write a new U-Boot version to the board before starting
    it up. This is particularly valuable when using labgrid interactively,
    to test out / debug U-Boot on various boards.
    
    We end up with several different paths through the strategy:
    
    - just power on/reset the board
    - bootstrap: write an existing U-Boot to the board, then power it on
    - build and bootstrap: build a new U-Boot, write it to the board, then
      power it on
    
    All of these are useful in an interactive context.
    
    Implement this by calling the U-Boot provider to build U-Boot, then the
    U-Boot writer to write it to the board. Finally, power on/reset the
    board as normal.
    
    This is controlled by two variables:
    
       do-bootstrap: 1 to bootstrap the board, 0 to leave it along
       do-build:     1 to build U-Boot, 0 to use the existing build
    
    Series-change: 8
    - Check for None with image_dirs[1] instead of 'None'
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- labgrid/strategy/ubootstrategy.py
+++ labgrid/strategy/ubootstrategy.py
@@ -6,6 +6,7 @@ import attr
 
 from ..factory import target_factory
 from .common import Strategy, StrategyError
+from labgrid.var_dict import get_var
 
 
 class Status(enum.Enum):
@@ -13,17 +14,23 @@ class Status(enum.Enum):
 
         unknown: State is not known
         off: Power is off
+        bootstrap: U-Boot has been written to the board
         start: Board has started booting
         uboot: Board has stopped at the U-Boot prompt
         shell: Board has stopped at the Linux prompt
     """
-    unknown, off, start, uboot, shell = range(5)
+    unknown, off, bootstrap, start, uboot, shell = range(6)
 
 
 @target_factory.reg_driver
 @attr.s(eq=False)
 class UBootStrategy(Strategy):
-    """UbootStrategy - Strategy to switch to uboot or shell"""
+    """UbootStrategy - Strategy to switch to uboot or shell
+
+    Variables:
+        do-build: Build U-Boot before bootstrapping it
+        do-bootstrap: Bootstrap U-Boot by writing it to the board
+    """
     bindings = {
         "power": "PowerProtocol",
         "console": "ConsoleProtocol",
@@ -36,9 +43,32 @@ class UBootStrategy(Strategy):
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
+        self.bootstrapped = False
+
+    def bootstrap(self):
+        builder = self.target.get_driver("UBootProviderDriver")
+        if get_var('do-build', '0') == '1':
+            image_dirs = builder.build()
+        else:
+            image_dirs = builder.get_build_paths()
+        if len(image_dirs) == 1 or not image_dirs[1]:
+            msg = f'dir {image_dirs[0]}'
+        else:
+            msg = f'dirs {image_dirs[0]} and {image_dirs[1]}'
+        print(f"Bootstrapping U-Boot from {msg}")
+
+        writer = self.target.get_driver("UBootWriterDriver")
+        writer.write(image_dirs)
+        self.bootstrapped = True
 
     def start(self):
         """Start U-Boot, by powering on / resetting the board"""
+        if not self.bootstrapped and get_var('do-bootstrap', '0') == '1':
+            self.transition(Status.bootstrap)
+        else:
+            writer = self.target.get_driver("UBootWriterDriver")
+            writer.prepare_boot()
+
         self.target.activate(self.console)
         if self.reset:
             self.target.activate(self.reset)
@@ -63,6 +93,8 @@ class UBootStrategy(Strategy):
             self.target.deactivate(self.console)
             self.target.activate(self.power)
             self.power.off()
+        elif status == Status.bootstrap:
+            self.bootstrap()
         elif status == Status.start:
             self.transition(Status.off)
             self.start()

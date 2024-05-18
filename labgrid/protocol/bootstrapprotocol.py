9f32c4612b0f11ae9cc7db01464ba1390d3ff45b
Simon Glass <sjg@chromium.org>
2024-05-18 07:41:30 -0600

    bootstrapprotocol: Support a boot phase
    Some SoCs need multiple USB-loading steps to function. For example,
    Samsung devices require BL1, then SPL then U-Boot.
    
    Add a new parameter to the protocol to support this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- labgrid/protocol/bootstrapprotocol.py
+++ labgrid/protocol/bootstrapprotocol.py
@@ -3,5 +3,11 @@ import abc
 
 class BootstrapProtocol(abc.ABC):
     @abc.abstractmethod
-    def load(self, filename: str):
+    def load(self, filename: str, phase=None):
+        """Load a file into the DUT
+
+        Args:
+            filename (str): Filename to load
+            phase (str): Loading phase, e.g. 'bl1'
+        """
         raise NotImplementedError

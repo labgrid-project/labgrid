05c995d10bba92aa5fe3097eff58e7425822bfd6
Simon Glass <sjg@chromium.org>
2024-05-21 07:42:52 -0600

    strategy: Show console output on failure
    When Labgrid fails to get to the U-Boot command line, show the output
    that was received, so that the user can see what is going on.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- labgrid/strategy/ubootstrategy.py
+++ labgrid/strategy/ubootstrategy.py
@@ -1,6 +1,6 @@
-import enum
-
 import attr
+import enum
+from pexpect import TIMEOUT
 import sys
 import time
 
@@ -143,11 +143,19 @@ class UBootStrategy(Strategy):
             self.start()
         elif status == Status.uboot:
             self.transition(Status.start)
+
             start = time.time()
-            # interrupt uboot
-            self.target.activate(self.uboot)
-            output = self.console.read_output()
-            sys.stdout.buffer.write(output)
+            try:
+                # interrupt uboot
+                self.target.activate(self.uboot)
+            except TIMEOUT:
+                output = self.console.read_output()
+                sys.stdout.buffer.write(output)
+                raise
+
+            # For debugging
+            #output = self.console.read_output()
+            #sys.stdout.buffer.write(output)
             duration = time.time() - start
             print(f'\n{{lab ready in {duration:.1f}s: {self.uboot.version}}}')
         elif status == Status.shell:

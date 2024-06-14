7acfa35dcd555dbcbb4df321f0d332d010ccfac7
Jan Luebbe <jlu@pengutronix.de>
2024-06-14 17:44:07 +0200

    use setuptools_scm to generate a version file
    This avoids the overhead of using importlib.metadata to search for the
    version.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/__init__.py
+++ labgrid/__init__.py
@@ -6,3 +6,8 @@ from .factory import target_factory
 from .step import step, steps
 from .stepreporter import StepReporter
 from .consoleloggingreporter import ConsoleLoggingReporter
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"

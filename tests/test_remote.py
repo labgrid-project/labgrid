df8df522b152d81ed95bebd224901a71553bbdce
Bastian Krause <bst@pengutronix.de>
2024-07-17 12:59:21 +0200

    tests/test_remote: add test_exporter_start_coordinator_unreachable
    Previously the exporter had blocking issues when the coordinator was not
    available. Add a test to prevent future regressions.
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -15,3 +15,23 @@ def test_exporter_help():
         spawn.close()
         assert spawn.exitstatus == 0
         assert spawn.signalstatus is None
+
+def test_exporter_start_coordinator_unreachable(monkeypatch, tmpdir):
+    monkeypatch.setenv("LG_COORDINATOR", "coordinator.invalid")
+
+    config = "exports.yaml"
+    p = tmpdir.join(config)
+    p.write(
+        """
+    Testport:
+        NetworkSerialPort:
+          host: 'localhost'
+          port: 4000
+    """
+    )
+
+    with pexpect.spawn(f"python -m labgrid.remote.exporter {config}", cwd=tmpdir) as spawn:
+        spawn.expect("coordinator is unavailable", timeout=10)
+        spawn.expect(pexpect.EOF)
+        spawn.close()
+        assert spawn.exitstatus == 100, spawn.before

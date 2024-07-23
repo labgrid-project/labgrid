42305915e79722e2c59914ba8d3a504afef8d8cb
Bastian Krause <bst@pengutronix.de>
2024-07-23 14:10:47 +0200

    remote/client: drop redundant checks
    These checks are already performed by get_idle_place().
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -597,8 +597,6 @@ class ClientSession:
         """Add a match for a place, making fuzzy matching available to the
         client"""
         place = self.get_idle_place()
-        if place.acquired:
-            raise UserError(f"can not change acquired place {place.name}")
         for pattern in self.args.patterns:
             if not 2 <= pattern.count("/") <= 3:
                 raise UserError(f"invalid pattern format '{pattern}' (use 'exporter/group/cls/name')")
@@ -617,8 +615,6 @@ class ClientSession:
     async def del_match(self):
         """Delete a match for a place"""
         place = self.get_idle_place()
-        if place.acquired:
-            raise UserError(f"can not change acquired place {place.name}")
         for pattern in self.args.patterns:
             if not 2 <= pattern.count("/") <= 3:
                 raise UserError(f"invalid pattern format '{pattern}' (use 'exporter/group/cls/name')")
@@ -638,8 +634,6 @@ class ClientSession:
 
         Fuzzy matching is not allowed to avoid accidental names conflicts."""
         place = self.get_idle_place()
-        if place.acquired:
-            raise UserError(f"can not change acquired place {place.name}")
         pattern = self.args.pattern
         name = self.args.name
         if not 2 <= pattern.count("/") <= 3:

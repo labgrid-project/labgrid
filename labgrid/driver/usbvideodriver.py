70525a33c92587a03b0e7ae683d9434c0693c3b8
Jan Luebbe <jlu@pengutronix.de>
2024-08-26 16:43:14 +0200

    driver/usbvideodriver: use elif if a sequence of conditions
    Only one of these can match anyway, so using 'if' is just confusing.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- labgrid/driver/usbvideodriver.py
+++ labgrid/driver/usbvideodriver.py
@@ -27,43 +27,43 @@ class USBVideoDriver(Driver, VideoProtocol):
                 ("mid", "video/x-h264,width=1280,height=720,framerate=15/2"),
                 ("high", "video/x-h264,width=1920,height=1080,framerate=10/1"),
                 ])
-        if match == (0x046d, 0x0892):
+        elif match == (0x046d, 0x0892):
             return ("mid", [
                 ("low", "image/jpeg,width=640,height=360,framerate=5/1"),
                 ("mid", "image/jpeg,width=1280,height=720,framerate=15/2"),
                 ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
                 ])
-        if match == (0x046d, 0x08e5): # Logitech HD Pro Webcam C920
+        elif match == (0x046d, 0x08e5): # Logitech HD Pro Webcam C920
             return ("mid", [
                 ("low", "image/jpeg,width=640,height=360,framerate=5/1"),
                 ("mid", "image/jpeg,width=1280,height=720,framerate=15/2"),
                 ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
                 ])
-        if match == (0x1224, 0x2825): # LogiLink UA0371
+        elif match == (0x1224, 0x2825): # LogiLink UA0371
             return ("mid", [
                 ("low", "image/jpeg,width=640,height=480,framerate=30/1"),
                 ("mid", "image/jpeg,width=1280,height=720,framerate=30/1"),
                 ("high", "image/jpeg,width=1920,height=1080,framerate=30/1"),
                 ])
-        if match == (0x05a3, 0x9331): # WansView Webcam 102
+        elif match == (0x05a3, 0x9331): # WansView Webcam 102
             return ("mid", [
                 ("low","video/x-h264,width=640,height=360,framerate=30/1"),
                 ("mid","video/x-h264,width=1280,height=720,framerate=30/1"),
                 ("high","video/x-h264,width=1920,height=1080,framerate=30/1"),
                 ])
-        if match == (0x534d, 0x2109): # MacroSilicon
+        elif match == (0x534d, 0x2109): # MacroSilicon
             return ("mid", [
                 ("low", "image/jpeg,width=720,height=480,framerate=10/1"),
                 ("mid", "image/jpeg,width=1280,height=720,framerate=10/1"),
                 ("high", "image/jpeg,width=1920,height=1080,framerate=10/1"),
                 ])
-        if match == (0x1d6c, 0x0103): # HD 2MP WEBCAM
+        elif match == (0x1d6c, 0x0103): # HD 2MP WEBCAM
             return ("mid", [
                 ("low", "video/x-h264,width=640,height=480,framerate=25/1"),
                 ("mid", "video/x-h264,width=1280,height=720,framerate=25/1"),
                 ("high", "video/x-h264,width=1920,height=1080,framerate=25/1"),
                 ])
-        if match == (0x0c45, 0x636d): # AUKEY PC-LM1E
+        elif match == (0x0c45, 0x636d): # AUKEY PC-LM1E
             return ("mid", [
                 ("low", "image/jpeg,width=640,height=480,pixel-aspect-ratio=1/1,framerate=30/1"),
                 ("mid", "image/jpeg,width=864,height=480,pixel-aspect-ratio=1/1,framerate=30/1"),
@@ -130,8 +130,7 @@ class USBVideoDriver(Driver, VideoProtocol):
 
         tx_cmd = self.video.command_prefix + ["gst-launch-1.0", "-q"]
         tx_cmd += pipeline.split()
-        rx_cmd = ["gst-launch-1.0"]
-        rx_cmd += "playbin3 buffer-duration=0 uri=fd://0".split()
+        rx_cmd = ["gst-launch-1.0", "playbin3", "buffer-duration=0", "uri=fd://0"]
 
         tx = subprocess.Popen(
             tx_cmd,

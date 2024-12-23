0414bb0cb1e11fc79b37a3f03e82a8c64a8b6bfe
Rainer Poisel <rainer@honeytreelabs.com>
2024-12-23 09:59:42 +0100

    feat: implement __all__ datastructure for pytest plugin
    Signed-off-by: Rainer Poisel <rainer@honeytreelabs.com>

--- labgrid/pytestplugin/__init__.py
+++ labgrid/pytestplugin/__init__.py
@@ -1,2 +1,13 @@
 from .fixtures import pytest_addoption, env, target, strategy
 from .hooks import pytest_configure, pytest_collection_modifyitems, pytest_cmdline_main, pytest_runtest_setup
+
+__all__ = [
+    "pytest_addoption",
+    "env",
+    "target",
+    "strategy",
+    "pytest_configure",
+    "pytest_collection_modifyitems",
+    "pytest_cmdline_main",
+    "pytest_runtest_setup",
+]

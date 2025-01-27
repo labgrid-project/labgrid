9b92f0bd34b04e01620e94004b255806cbc13fb0
Perry Melange <isprotejesvalkata@gmail.com>
2025-01-27 10:49:45 +0100

    exporter: add new invert (active-low) attribute to GPIOSysFSExport
    Signed-off-by: Perry Melange <isprotejesvalkata@gmail.com>

--- labgrid/remote/exporter.py
+++ labgrid/remote/exporter.py
@@ -642,16 +642,19 @@ class GPIOSysFSExport(ResourceExport):
         return {
             "host": self.host,
             "index": self.local.index,
+            "invert": self.local.invert,
         }
 
     def _get_start_params(self):
         return {
             "index": self.local.index,
+            "invert": self.local.invert,
         }
 
     def _start(self, start_params):
         """Start a GPIO export to userspace"""
         index = start_params["index"]
+        invert = start_params["invert"]
 
         if self.export_path.exists():
             self.system_exported = True
@@ -671,7 +674,6 @@ class GPIOSysFSExport(ResourceExport):
         export_sysfs_path = os.path.join(GPIOSysFSExport._gpio_sysfs_path_prefix, "unexport")
         with open(export_sysfs_path, mode="wb") as unexport:
             unexport.write(str(index).encode("utf-8"))
-
 
 exports["SysfsGPIO"] = GPIOSysFSExport
 exports["MatchedSysfsGPIO"] = GPIOSysFSExport

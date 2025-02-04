labgrid-project/labgrid

commit 17765c7942c120a59b007ae04ee6358dd53c2977

dockerfiles: support for docker/podman buildx
This allows for building docker images for different platforms such as
linux/arm64 (Raspberry Pi 3/4).

Signed-off-by: Rainer Poisel <rainer@honeytreelabs.com>

dockerfiles/build.sh
@@ -1,21 +1,98 @@
-#!/bin/sh
+#!/bin/bash
 
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE:-$0}")")"
-
-cd "${SCRIPT_DIR}/.."
-
-set -ex
-if [ -z "${DOCKER}" ]; then
-    command -v docker >/dev/null 2>&1 && DOCKER=docker
-fi
-if [ -z "${DOCKER}" ]; then
-    command -v podman >/dev/null 2>&1 && DOCKER=podman
-fi
 export DOCKER_BUILDKIT=1
 
-VERSION="$(python -m setuptools_scm)"
+die () {
+    local msg
+    msg="${1}"
 
-for t in client exporter coordinator; do
-    ${DOCKER} build --build-arg VERSION="$VERSION" \
-        --target labgrid-${t} -t labgrid-${t} -f "${SCRIPT_DIR}/Dockerfile" .
-done
+    echo "[fatal] ${msg}" >&2
+    exit 1
+}
+
+log_info() {
+    local msg
+    msg="${1}"
+
+    echo "[info] ${msg}"
+}
+
+has_docker() {
+    command -v docker >/dev/null 2>&1
+}
+
+has_podman() {
+    command -v podman /dev/null 2>&1
+}
+
+has_buildx() {
+    local docker_cmd
+    docker_cmd="${1}"
+
+    "${docker_cmd}" buildx --help >/dev/null 2>&1
+}
+
+get_docker_cmd() {
+    local docker_cmd
+    docker_cmd="${1}"
+
+    if [ -n "${docker_cmd}" ]; then
+        echo "${docker_cmd}"
+        return
+    fi
+
+    if has_docker; then
+        echo "docker"
+    else
+        echo "podman"
+    fi
+}
+
+perform_regular_build() {
+    local docker_cmd script_dir version
+    docker_cmd="${1}"
+    script_dir="${2}"
+    version="${3}"
+
+    log_info "building for native platform only."
+
+    for t in client exporter coordinator; do
+        "${docker_cmd}" build --build-arg VERSION="${version}" \
+            --target labgrid-${t} -t labgrid-${t}:latest -f "${script_dir}/Dockerfile" .
+    done
+}
+
+perform_docker_buildx_build() {
+    local docker_cmd script_dir version
+    docker_cmd="${1}"
+    script_dir="${2}"
+    version="${3}"
+
+    for t in client exporter coordinator; do
+        "${docker_cmd}" buildx build --platform "${platform}" --build-arg VERSION="${version}" \
+            --target labgrid-${t} -t labgrid-${t}:latest -f "${script_dir}/Dockerfile" .
+    done
+}
+
+main() {
+    local platform script_dir version
+    platform="${1}"
+
+    if ! has_docker && ! has_podman; then
+        die "Neither docker nor podman could be found."
+    fi
+
+    script_dir="$(dirname "$(realpath "${BASH_SOURCE:-$0}")")"
+    version="$(python -m setuptools_scm)"
+    docker_cmd="$(get_docker_cmd "${DOCKER}")"
+
+    cd "${script_dir}/.." || die "Could not cd into repo root dir"
+
+    if has_buildx "${docker_cmd}" && [ -n "${platform}" ]; then
+        perform_docker_buildx_build "${docker_cmd}" "${script_dir}" "${version}" "${platform}"
+    else
+        perform_regular_build "${docker_cmd}" "${script_dir}" "${version}"
+    fi
+}
+
+main "${1}"

commit d6ebde1ce7d7d80bce4b860d73fc1675b2e4f061

dockerfiles/build.sh: fix buildx detection
`docker <subcommand> --help` cannot be used to detect whether a sub
command is available because it exits with 0 no matter if the sub
command exists or not.

Use `docker buildx version` as a test for buildx support instead.

Fixes: 17765c79 ("dockerfiles: support for docker/podman buildx")
Reported-by: Sebastian Groß <sebastian.gross@emlix.com>
Signed-off-by: Bastian Krause <bst@pengutronix.de>

dockerfiles/build.sh
@@ -31,7 +31,7 @@ has_buildx() {
     local docker_cmd
     docker_cmd="${1}"
 
-    "${docker_cmd}" buildx --help >/dev/null 2>&1
+    "${docker_cmd}" buildx version >/dev/null 2>&1
 }
 
 get_docker_cmd() {
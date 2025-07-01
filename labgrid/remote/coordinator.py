labgrid-project/labgrid

commit 8cb8ecd997de8c79d064549afc05d4cfb3852f2c

remote/coordinator: show error reason for failed exporter commands
If an acquire or release failed on the exporter, we should add the
reason to the exception.

Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/remote/coordinator.py
@@ -592,7 +592,7 @@ class Coordinator(labgrid_coordinator_pb2_grpc.CoordinatorServicer):
         self.get_exporter_by_name(resource.path[0]).queue.put_nowait(cmd)
         await cmd.wait()
         if not cmd.response.success:
-            raise ExporterError("failed to acquire {resource}")
+            raise ExporterError("failed to acquire {resource} ({cmd.response.reason})")
         if resource.acquired != place.name:
             logging.warning("resource %s not acquired by this place after acquire request", resource)
 
@@ -648,7 +648,7 @@ class Coordinator(labgrid_coordinator_pb2_grpc.CoordinatorServicer):
                     self.get_exporter_by_name(resource.path[0]).queue.put_nowait(cmd)
                     await cmd.wait()
                     if not cmd.response.success:
-                        raise ExporterError(f"failed to release {resource}")
+                        raise ExporterError(f"failed to release {resource} ({cmd.response.reason})")
                     if resource.acquired:
                         logging.warning("resource %s still acquired after release request", resource)
             except (ExporterError, TimeoutError):

commit 6560c354e772981d8bf0455fc039ff12691f0f9d

remote/coordinator: fix ExporterError message
The message should have been an f-string to contain any useful
information.

Fixes: 8cb8ecd9 ("remote/coordinator: show error reason for failed exporter commands")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/remote/coordinator.py
@@ -626,7 +626,7 @@ class Coordinator(labgrid_coordinator_pb2_grpc.CoordinatorServicer):
         self.get_exporter_by_name(resource.path[0]).queue.put_nowait(cmd)
         await cmd.wait()
         if not cmd.response.success:
-            raise ExporterError("failed to acquire {resource} ({cmd.response.reason})")
+            raise ExporterError(f"failed to acquire {resource} ({cmd.response.reason})")
         if resource.acquired != place.name:
             logging.warning("resource %s not acquired by this place after acquire request", resource)
 
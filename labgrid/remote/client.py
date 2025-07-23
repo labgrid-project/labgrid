9d3eaf323b955d4b686db05548a4db008a72faad
Simon Glass <sjg@chromium.org>
2025-07-23 06:35:03 -0600

    remote/client: Allow showing console output on exit
    In the case of tests running via pytest, the output may be captured and
    thus not visible to the user.
    
    Provide a new --show-output option which dumps the board-console output
    on exit, to provide this visibility. It is only supported with the
    internal console.
    
    Series-changes: 8
    - Add new patch to allow showing console output on exit
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -1056,6 +1056,8 @@ class ClientSession:
             console = target.get_driver(ConsoleProtocol, name=name)
             returncode = await term.internal(lambda: self.is_allowed(place),
                                              console, logfile, listen_only)
+            if self.args.show_output:
+                sys.stdout.buffer.write(console.read_output())
         else:
             from ..resource import NetworkSerialPort
 
@@ -2090,6 +2092,7 @@ def get_parser(auto_doc_mode=False) -> "argparse.ArgumentParser | AutoProgramArg
     )
     subparser.add_argument("name", help="optional resource name", nargs="?")
     subparser.add_argument("--logfile", metavar="FILE", help="Log output to FILE", default=None)
+    subparser.add_argument("--show-output", help="Show console output even on success", default=None)
     subparser.set_defaults(func=ClientSession.console)
 
     subparser = subparsers.add_parser("dfu", help="communicate with device in DFU mode")

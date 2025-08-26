5d6c4624729fde461eeb376b3e3a1f22281462f4
Martin Sabol <martin.sabol@seznam.cz>
2025-08-26 14:47:15 +0200

    driver: add pyVISA remote resource support
    Introduce NetworkPyVISADevice for remote access to pyVISA resources.
    Use an agent wrapper to support both local and remote resource access.
    
    Signed-off-by: Martin Sabol <martin.sabol@seznam.cz>

--- labgrid/resource/pyvisa.py
+++ labgrid/resource/pyvisa.py
@@ -1,7 +1,7 @@
 import attr
 
 from ..factory import target_factory
-from .common import Resource
+from .common import Resource, NetworkResource
 
 
 @target_factory.reg_resource
@@ -11,10 +11,24 @@ class PyVISADevice(Resource):
 
     Args:
         type (str): device resource type following the pyVISA resource syntax, e.g. ASRL, TCPIP...
-        url (str): device identifier on selected resource, e.g. <ip> for TCPIP resource
+        url (str, default=''): optional device identifier on selected resource, e.g. <ip> for TCPIP resource
         backend (str, default=''): Visa library backend, e.g. '@sim' for pyvisa-sim backend
     """
 
     type = attr.ib(validator=attr.validators.instance_of(str))
-    url = attr.ib(validator=attr.validators.instance_of(str))
+    url = attr.ib(default="", validator=attr.validators.instance_of(str))
+    backend = attr.ib(default="", validator=attr.validators.instance_of(str))
+
+@target_factory.reg_resource
+@attr.s(eq=False)
+class NetworkPyVISADevice(NetworkResource):
+    """The NetworkPyVISADevice describes a remote test stimuli device controlled  with PyVISA
+
+    Args:
+        type (str): device resource type following the pyVISA resource syntax, e.g. ASRL, TCPIP...
+        url (str, default=''): optional device identifier on selected resource, e.g. <ip> for TCPIP resource
+        backend (str, default=''): Visa library backend, e.g. '@sim' for pyvisa-sim backend
+    """
+    type = attr.ib(validator=attr.validators.instance_of(str))
+    url = attr.ib(default="", validator=attr.validators.instance_of(str))
     backend = attr.ib(default="", validator=attr.validators.instance_of(str))

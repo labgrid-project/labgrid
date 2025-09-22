a279e5272040256653a99b5904df0a1a68fcebd3
Bastian Krause <bst@pengutronix.de>
2025-09-22 17:27:17 +0200

    remote/client: move ensure_event_loop() to labgrid.util.loop
    Moving the function and the ContextVar to labgrid.util.loop will allow
    non-client code to use it as well. Otherwise usage will result in
    circular imports such as:
    
          from labgrid import Target
      labgrid/__init__.py:1: in <module>
          from .target import Target
      labgrid/target.py:10: in <module>
          from .driver import Driver
      labgrid/driver/__init__.py:4: in <module>
          from .serialdriver import SerialDriver
      labgrid/driver/serialdriver.py:10: in <module>
          from ..util.proxy import proxymanager
      labgrid/util/proxy.py:7: in <module>
          from ..resource.common import Resource
      labgrid/resource/__init__.py:2: in <module>
          from .ethernetport import SNMPEthernetPort
      labgrid/resource/ethernetport.py:9: in <module>
          from ..remote.client import ensure_event_loop
      labgrid/remote/client.py:41: in <module>
          from .. import Environment, Target, target_factory
      E   ImportError: cannot import name 'Environment' from partially initialized module 'labgrid' (most likely due to a circular import) (labgrid/__init__.py)
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -4,7 +4,6 @@ coordinator, acquire a place and interact with the connected resources"""
 import argparse
 import asyncio
 import contextlib
-from contextvars import ContextVar
 import enum
 import os
 import pathlib
@@ -48,6 +47,7 @@ from ..resource.remote import RemotePlaceManager, RemotePlace
 from ..util import diff_dict, flat_dict, dump, atomic_replace, labgrid_version, Timeout
 from ..util.proxy import proxymanager
 from ..util.helper import processwrapper
+from ..util.loop import ensure_event_loop
 from ..driver import Mode, ExecutionError
 from ..logging import basicConfig, StepLogger
 
@@ -1618,42 +1618,6 @@ class ClientSession:
 
     def print_version(self):
         print(labgrid_version())
-
-
-_loop: ContextVar["asyncio.AbstractEventLoop | None"] = ContextVar("_loop", default=None)
-
-
-def ensure_event_loop(external_loop=None):
-    """Get the event loop for this thread, or create a new event loop."""
-    # get stashed loop
-    loop = _loop.get()
-
-    # ignore closed stashed loop
-    if loop and loop.is_closed():
-        loop = None
-
-    if external_loop:
-        # if a loop is stashed, expect it to be the same as the external one
-        if loop:
-            assert loop is external_loop
-        _loop.set(external_loop)
-        return external_loop
-
-    # return stashed loop
-    if loop:
-        return loop
-
-    try:
-        # if called from async code, try to get current's thread loop
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        # no previous, external or running loop found, create a new one
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-    # stash it
-    _loop.set(loop)
-    return loop
 
 
 def start_session(

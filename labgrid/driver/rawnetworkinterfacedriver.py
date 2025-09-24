labgrid-project/labgrid

commit fe4ec8b3728f5924d04b2a8db755d550ccea8879

driver: rawnetworkinterfacedriver: Add managed attribute
Adds an attribute that controls if the driver fully manages the
network interface, which causes it to setup and teardown the interface
on activate and deactivate. This allows users to disable this behavior
if they are externally managing the interface.

Closes: #1728

Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>
[bst: make documentation more consistent]
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/rawnetworkinterfacedriver.py
@@ -18,9 +18,18 @@ from ..resource.common import NetworkResource
 @target_factory.reg_driver
 @attr.s(eq=False)
 class RawNetworkInterfaceDriver(Driver):
+    """RawNetworkInterface - Manage a network interface and interact with it at a low level
+
+    Args:
+        manage_interface (bool, default=True): if True this driver will
+        setup/teardown the interface on activate/deactivate. Set this to False
+        if you are managing the interface externally.
+    """
+
     bindings = {
         "iface": {"NetworkInterface", "RemoteNetworkInterface", "USBNetworkInterface"},
     }
+    manage_interface = attr.ib(default=True, validator=attr.validators.instance_of(bool))
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
@@ -28,12 +37,14 @@ class RawNetworkInterfaceDriver(Driver):
         self._replay_handle = None
 
     def on_activate(self):
-        self._set_interface("up")
-        self._wait_state("up")
+        if self.manage_inteface:
+            self._set_interface("up")
+            self._wait_state("up")
 
     def on_deactivate(self):
-        self._set_interface("down")
-        self._wait_state("down")
+        if self.manage_interface:
+            self._set_interface("down")
+            self._wait_state("down")
 
     def _wrap_command(self, args):
         wrapper = ["sudo", "labgrid-raw-interface"]

commit 0fdb7d9c602ea604545abe72a2ef29955ef745aa

driver/rawnetworkinterfacedriver: fix AttributeError in on_activate() due to typo
Fixes: fe4ec8b3 ("driver: rawnetworkinterfacedriver: Add managed attribute")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

labgrid/driver/rawnetworkinterfacedriver.py
@@ -37,7 +37,7 @@ class RawNetworkInterfaceDriver(Driver):
         self._replay_handle = None
 
     def on_activate(self):
-        if self.manage_inteface:
+        if self.manage_interface:
             self._set_interface("up")
             self._wait_state("up")
 
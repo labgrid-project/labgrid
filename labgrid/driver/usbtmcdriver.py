labgrid-project/labgrid

commit 1df9371a3240e5149c78aaebe9f5c1f882eaad2e

add support for USB TMC devices
Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

labgrid/driver/usbtmcdriver.py
@@ -1,0 +1,102 @@
+# pylint: disable=no-member
+import attr
+import subprocess
+from importlib import import_module
+from decimal import Decimal
+
+from .common import Driver
+from ..factory import target_factory
+from ..resource.remote import NetworkUSBTMC
+from ..resource.udev import USBTMC
+from ..exceptions import InvalidConfigError
+from ..util.agentwrapper import AgentWrapper, b2s, s2b
+
+@target_factory.reg_driver
+@attr.s(cmp=False)
+class USBTMCDriver(Driver):
+    bindings = {
+        "tmc": {USBTMC, NetworkUSBTMC},
+    }
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.wrapper = None
+        self.backend = None
+
+        assert self.tmc.path.startswith('/dev/usbtmc')
+        self.index = int(self.tmc.path[11:], 10)
+        assert self.tmc.path == '/dev/usbtmc'+str(self.index)
+
+    def on_activate(self):
+        assert self.wrapper is None
+        self.wrapper = AgentWrapper(self.tmc.host)
+
+        match = (self.tmc.vendor_id, self.tmc.model_id)
+        if match == (0x0957, 0x1798):
+            model = 'keysight_dsox2000'
+        else:
+            raise InvalidConfigError("Unkown USB TMC device {:04x}:{:04x}".format(*match))
+
+        # TODO: allow backends to register models with other names
+        self.backend = import_module(
+            ".usbtmc.{}".format(model),
+            __package__
+        )
+
+    def on_deactivate(self):
+        assert self.wrapper is not None
+        self.wrapper.close()
+        self.wrapper = None
+        self.backend = None
+
+    @Driver.check_active
+    def command(self, cmd, binary=False):
+        assert isinstance(cmd, str)
+        cmd = b2s(cmd.encode('ASCII')+b'\n')
+        self.wrapper.usbtmc(self.index, cmd, read=False)
+
+    @Driver.check_active
+    def query(self, cmd, binary=False):
+        assert isinstance(cmd, str)
+        cmd = b2s(cmd.encode('ASCII')+b'\n')
+        res = s2b(self.wrapper.usbtmc(self.index, cmd, read=True))
+        if binary:
+            assert res[0:1] == b'#'
+            digits = int(res[1:2], 10)
+            count = int(res[2:2+digits], 10)
+            return res[2+digits:2+digits+count]
+        else:
+            assert res[-1:] == b'\n'
+            return res[:-1].decode('ASCII')
+
+    @Driver.check_active
+    def identify(self):
+        return self.query('*IDN?')
+
+    @Driver.check_active
+    def get_channel_info(self, channel):
+        return self.backend.get_channel_info(self, channel)
+
+    @Driver.check_active
+    def get_channel_values(self, channel):
+        return self.backend.get_channel_values(self, channel)
+
+    @Driver.check_active
+    def get_screenshot(self):
+        return self.backend.get_screenshot_png(self)
+
+    @Driver.check_active
+    def get_bool(self, cmd):
+        return bool(int(self.query(cmd).strip()))
+
+    @Driver.check_active
+    def get_int(self, cmd):
+        return int(self.query(cmd).strip())
+
+    @Driver.check_active
+    def get_decimal(self, cmd):
+        return Decimal(self.query(cmd).strip())
+
+    @Driver.check_active
+    def get_str(self, cmd):
+        return self.query(cmd)

labgrid/driver/usbtmc/keysight_dsox2000.py
@@ -1,0 +1,29 @@
+def get_channel_info(driver, channel):
+    info = {}
+    info['BWLIMIT'] = driver.get_bool(":CHANnel%i:BWLimit?" % channel)
+    info['COUPLING'] = driver.get_str(":CHANnel%i:COUPling?" % channel).strip()
+    info['LABEL'] = driver.get_str(":CHANnel%i:LABel?" % channel)
+    info['PROBE'] = driver.get_decimal(":CHANnel%i:PROBe?" % channel)
+    info['PROBE:HEAD'] = driver.get_str(":CHANnel%i:PROBe:HEAD?" % channel).strip()
+    info['PROBE:ID'] = driver.get_str(":CHANnel%i:PROBe:ID?" % channel).strip()
+    info['PROBE:SKEW'] = driver.get_decimal(":CHANnel%i:PROBe:SKEW?" % channel)
+    info['PROBE:STYPE'] = driver.get_str(":CHANnel%i:PROBe:STYPe?" % channel).strip()
+    info['RANGE'] = driver.get_decimal(":CHANnel%i:RANGe?" % channel)
+    info['SCALE'] = driver.get_decimal(":CHANnel%i:SCALe?" % channel)
+    info['UNITS'] = driver.get_str(":CHANnel%i:UNITS?" % channel).strip()
+    return info
+
+def get_channel_values(driver, channel):
+    driver.command(":MEASURE:SOURCE CHANNEL%i" % channel)
+    info = {}
+    info['DUTYCYCLE'] = driver.get_decimal(":MEASURE:DUTYcycle?")
+    info['FREQUENCY'] = driver.get_decimal(":MEASURE:FREQuency?")
+    info['PERIOD'] = driver.get_decimal(":MEASURE:PERiod?")
+    info['VMAX'] = driver.get_decimal(":MEASURE:VMAX?")
+    info['VMIN'] = driver.get_decimal(":MEASURE:VMIN?")
+    info['VPP'] = driver.get_decimal(":MEASURE:VPP?")
+    return info
+
+def get_screenshot_png(driver):
+    driver.command(":HARDcopy:INKSaver OFF")
+    return driver.query(':DISPlay:DATA? PNG, COLor', binary=True)

labgrid/remote/client.py
@@ -826,6 +826,64 @@ class ClientSession(ApplicationSession):
         else:
             drv.stream(quality)
 
+    def _get_tmc(self):
+        place = self.get_acquired_place()
+        target = self._get_target(place)
+        from ..driver.usbtmcdriver import USBTMCDriver
+        from ..resource.remote import NetworkUSBTMC
+        drv = None
+        for resource in target.resources:
+            if isinstance(resource, NetworkUSBTMC):
+                try:
+                    drv = target.get_driver(USBTMCDriver)
+                except NoDriverFoundError:
+                    drv = USBTMCDriver(target, name=None)
+                break
+        if not drv:
+            raise UserError("target has no compatible resource available")
+        target.activate(drv)
+        return drv
+
+    def tmc_command(self):
+        drv = self._get_tmc()
+        command = ' '.join(self.args.command)
+        if not command:
+            raise UserError("no command given")
+        if '?' in command:
+            result = drv.query(command)
+            print(result)
+        else:
+            drv.command(command)
+
+    def tmc_query(self):
+        drv = self._get_tmc()
+        query = ' '.join(self.args.query)
+        if not query:
+            raise UserError("no query given")
+        result = drv.query(query)
+        print(result)
+
+    def tmc_screen(self):
+        drv = self._get_tmc()
+        action = self.args.action
+        if action in ['show', 'save']:
+            data = drv.get_screenshot()
+            filename = 'tmc-screen_{0:%Y-%m-%d}_{0:%H:%M:%S}.png'.format(datetime.now())
+            open(filename, 'wb').write(data)
+            print("Saved as {}".format(filename))
+            if action == 'show':
+                subprocess.call(['xdg-open', filename])
+
+    def tmc_channel(self):
+        drv = self._get_tmc()
+        channel = self.args.channel
+        action = self.args.action
+        if action == 'info':
+            data = drv.get_channel_info(channel)
+        elif action == 'values':
+            data = drv.get_channel_values(channel)
+        for k, v in sorted(data.items()):
+            print("{:<16s} {:<10s}".format(k, str(v)))
 
 def start_session(url, realm, extra):
     from autobahn.wamp.types import ComponentConfig
@@ -1073,6 +1131,35 @@ def main():
     subparser.add_argument('-q', '--quality', type=str,
                            help="select a video quality (use 'list' to show options)")
     subparser.set_defaults(func=ClientSession.video)
+
+    subparser = subparsers.add_parser('tmc', help="control a USB TMC device")
+    subparser.set_defaults(func=lambda _: subparser.print_help())
+    tmc_subparsers = subparser.add_subparsers(
+        dest='subcommand',
+        title='available subcommands',
+        metavar="SUBCOMMAND",
+    )
+
+    tmc_subparser = tmc_subparsers.add_parser('cmd',
+                                              aliases=('c',),
+                                              help="execute raw command")
+    tmc_subparser.add_argument('command', nargs='+')
+    tmc_subparser.set_defaults(func=ClientSession.tmc_command)
+
+    tmc_subparser = tmc_subparsers.add_parser('query',
+                                              aliases=('q',),
+                                              help="execute raw query")
+    tmc_subparser.add_argument('query', nargs='+')
+    tmc_subparser.set_defaults(func=ClientSession.tmc_query)
+
+    tmc_subparser = tmc_subparsers.add_parser('screen', help="show or save a screenshot")
+    tmc_subparser.add_argument('action', choices=['show', 'save'])
+    tmc_subparser.set_defaults(func=ClientSession.tmc_screen)
+
+    tmc_subparser = tmc_subparsers.add_parser('channel', help="use a channel")
+    tmc_subparser.add_argument('channel', type=int)
+    tmc_subparser.add_argument('action', choices=['info', 'values'])
+    tmc_subparser.set_defaults(func=ClientSession.tmc_channel)
 
     # make any leftover arguments available for some commands
     args, leftover = parser.parse_known_args()

labgrid/resource/remote.py
@@ -185,3 +185,11 @@ class NetworkUSBVideo(RemoteUSBResource):
     def __attrs_post_init__(self):
         self.timeout = 10.0
         super().__attrs_post_init__()
+
+@target_factory.reg_resource
+@attr.s(cmp=False)
+class NetworkUSBTMC(RemoteUSBResource):
+    """The NetworkUSBTMC describes a remotely accessible USB TMC device"""
+    def __attrs_post_init__(self):
+        self.timeout = 10.0
+        super().__attrs_post_init__()

labgrid/util/agent.py
@@ -1,0 +1,80 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+import base64
+
+def b2s(b):
+    return base64.b85encode(b).decode('ascii')
+
+def s2b(s):
+    return base64.b85decode(s.encode('ascii'))
+
+class Agent:
+    def __init__(self):
+        self.methods = {}
+
+    def _send(self, data):
+        sys.stdout.write(json.dumps(data)+'\n')
+        sys.stdout.flush()
+
+    def register(self, name, func):
+        assert name not in self.methods
+        self.methods[name] = func
+
+    def run(self):
+        for line in sys.stdin:
+            if not line:
+                continue
+
+            try:
+                request = json.loads(line)
+            except:
+                self._send({'error': 'request parsing failed'})
+                break
+
+            if request.get('close', False):
+                break
+
+            name = request['method']
+            args = request['args']
+            kwargs = request['kwargs']
+            try:
+                response = self.methods[name](*args, **kwargs)
+                self._send({'result': response})
+            except Exception as e:
+                self._send({'exception': repr(e)})
+                break
+
+def handle_test(*args, **kwargs):
+    return args[::-1]
+
+def handle_error(message):
+    raise RuntimeError(message)
+
+def handle_usbtmc(index, cmd, read=False):
+    assert isinstance(index, int)
+    cmd = s2b(cmd)
+    fd = os.open('/dev/usbtmc{}'.format(index), os.O_RDWR)
+    os.write(fd, cmd)
+    if not read:
+        os.close(fd)
+        return
+    data = []
+    while True:
+        data.append(os.read(fd, 4096))
+        if len(data[-1]) < 4096:
+            break
+    os.close(fd)
+    return b2s(b''.join(data))
+
+def main():
+    a = Agent()
+    a.register('test', handle_test)
+    a.register('error', handle_error)
+    a.register('usbtmc', handle_usbtmc)
+    a.run()
+
+if __name__=="__main__":
+    main()

labgrid/util/agentwrapper.py
@@ -1,0 +1,87 @@
+import base64
+import hashlib
+import json
+import os.path
+import subprocess
+import time
+
+def b2s(b):
+    return base64.b85encode(b).decode('ascii')
+
+def s2b(s):
+    return base64.b85decode(s.encode('ascii'))
+
+class AgentError(Exception):
+    pass
+
+class AgentException(Exception):
+    pass
+
+class AgentWrapper:
+    class Proxy:
+        def __init__(self, wrapper, name):
+            self.wrapper = wrapper
+            self.name = name
+
+        def __call__(self, *args, **kwargs):
+            return self.wrapper.call(self.name, *args, **kwargs)
+
+    def __init__(self, host):
+        agent = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'agent.py')
+        agent_data = open(agent, 'rb').read()
+        agent_hash = hashlib.sha256(agent_data).hexdigest()
+        agent_remote = '.labgrid_agent_{}.py'.format(agent_hash)
+        ssh_opts = 'ssh -x -o ConnectTimeout=5 -o PasswordAuthentication=no'.split()
+        subprocess.check_call(
+            ['rsync', '-e', ' '.join(ssh_opts), '-tq', agent, '{}:{}'.format(host, agent_remote)],
+        )
+        self.agent = subprocess.Popen(
+            ssh_opts + [host, '--', 'python3', agent_remote],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE)
+
+    def __del__(self):
+        self.close()
+
+    def __getattr__(self, name):
+        return self.Proxy(self, name)
+
+    def call(self, method, *args, **kwargs):
+        request = {
+            'method': method,
+            'args': args,
+            'kwargs': kwargs,
+            }
+        request = json.dumps(request)
+        request = request.encode('ASCII')
+        self.agent.stdin.write(request+b'\n')
+        self.agent.stdin.flush()
+        response = self.agent.stdout.readline()
+        response = response.decode('ASCII')
+        response = json.loads(response)
+        if 'result' in response:
+            return response['result']
+        elif 'exception' in response:
+            self.agent.wait()
+            self.agent = None
+            raise AgentException(response['exception'])
+        else:
+            self.agent.wait()
+            self.agent = None
+            raise AgentError(response['error'])
+
+    def close(self):
+        if self.agent is None:
+            return
+        request = {
+            'close': True,
+            }
+        request = json.dumps(request)
+        request = request.encode('ASCII')
+        self.agent.stdin.write(request+b'\n')
+        self.agent.stdin.flush()
+        self.agent.wait()
+        self.agent = None
+

setup.py
@@ -39,6 +39,7 @@ setup(
         'labgrid.autoinstall',
         'labgrid.driver',
         'labgrid.driver.power',
+        'labgrid.driver.usbtmc',
         'labgrid.external',
         'labgrid.protocol',
         'labgrid.provider',

tests/test_agent.py
@@ -1,0 +1,68 @@
+import pytest
+from py.path import local
+
+import labgrid.util.agentwrapper
+from labgrid.util.agentwrapper import AgentError, AgentException, AgentWrapper, b2s, s2b
+
+@pytest.fixture(scope='function')
+def subprocess_mock(mocker):
+    import subprocess
+
+    original = subprocess.Popen
+
+    agent = local(labgrid.util.agentwrapper.__file__).dirpath('agent.py')
+
+    def run(args, **kwargs):
+        assert args[0] in ['rsync', 'ssh']
+        if args[0] == 'rsync':
+            src = local(args[-2])
+            assert src == agent
+            dst = args[-1]
+            assert ':' in dst
+            dst = dst.split(':', 1)[1]
+            assert '/' not in dst
+            assert dst.startswith('.labgrid_agent')
+            return original(['true'], **kwargs)
+        elif args[0] == 'ssh':
+            assert '--' in args
+            args = args[args.index('--')+1:]
+            assert len(args) == 2
+            assert args[0] == 'python3'
+            assert args[1].startswith('.labgrid_agent')
+            # we need to use the original here to get the coverage right
+            return original(['python3', str(agent)], **kwargs)
+
+    mocker.patch('subprocess.Popen', run)
+
+def test_create(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    aw.close()
+
+def test_call(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    assert aw.call('test') == []
+    assert aw.call('test', 0) == [0]
+    assert aw.call('test', 0, 1) == [1, 0]
+    assert aw.call('test', 'foo') == ['foo']
+    assert aw.call('test', '{') == ['{']
+
+def test_proxy(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    assert aw.test() == []
+    assert aw.test( 0, 1) == [1, 0]
+
+def test_bytes(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    assert s2b(aw.test(b2s(b'\x00foo'))[0]) == b'\x00foo'
+
+def test_exception(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    with pytest.raises(AgentException) as excinfo:
+        aw.error('foo')
+    assert excinfo.value.args == ("RuntimeError('foo',)",)
+
+def test_error(subprocess_mock):
+    aw = AgentWrapper('localhost')
+    aw.agent.stdin.write(b'\x00')
+    with pytest.raises(AgentError):
+        aw.test()

commit c62db93129ea9fbef116af0bfbe9441e55006cff

drivers: usbtmc: resolve index conflict with PowerProtocol
Fix a naming collision between the USBTMC device index and the
labgrid's PowerProtocol channel index.

Fixes: 1df9371a3240 ("add support for USB TMC devices")
Signed-off-by: Martin Sabol <martin.sabol@seznam.cz>

labgrid/driver/usbtmcdriver.py
@@ -21,8 +21,8 @@ class USBTMCDriver(Driver):
         self.backend = None
 
         assert self.tmc.path.startswith("/dev/usbtmc")
-        self.index = int(self.tmc.path[11:], 10)
-        assert self.tmc.path == "/dev/usbtmc" + str(self.index)
+        self.dev_index = int(self.tmc.path[11:], 10)
+        assert self.tmc.path == "/dev/usbtmc" + str(self.dev_index)
 
     def on_activate(self):
         assert self.wrapper is None
@@ -49,13 +49,13 @@ class USBTMCDriver(Driver):
     def command(self, cmd):
         assert isinstance(cmd, str)
         cmd = b2s(cmd.encode("ASCII") + b"\n")
-        self.wrapper.usbtmc(self.index, cmd, read=False)
+        self.wrapper.usbtmc(self.dev_index, cmd, read=False)
 
     @Driver.check_active
     def query(self, cmd, binary=False, raw=False):
         assert isinstance(cmd, str)
         cmd = b2s(cmd.encode("ASCII") + b"\n")
-        res = s2b(self.wrapper.usbtmc(self.index, cmd, read=True))
+        res = s2b(self.wrapper.usbtmc(self.dev_index, cmd, read=True))
 
         if raw:
             return res
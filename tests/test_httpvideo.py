645694888b09f8c0ffebdce2043bd7e82e8e5689
Guillaume Betous <gbetous@ford.com>
2026-04-30 09:22:24 +0200

    HTTPVideo: Improve test coverage
    Test URL using:
    - http://
    - https://
    - http:// with port in URL
    
    Signed-off-by: Guillaume Betous <guillaume@missingno.tech>

--- tests/test_httpvideo.py
+++ tests/test_httpvideo.py
@@ -2,7 +2,17 @@ from labgrid.resource.httpvideostream import HTTPVideoStream
 from labgrid.driver.httpvideodriver import HTTPVideoDriver
 
 
-def test_ipvideo_create(target):
+def test_ipvideo_create_http(target):
     r = HTTPVideoStream(target, name=None, url="http://localhost/")
+    d = HTTPVideoDriver(target, name=None)
+    assert isinstance(d, HTTPVideoDriver)
+
+def test_ipvideo_create_https(target):
+    r = HTTPVideoStream(target, name=None, url="https://localhost/")
+    d = HTTPVideoDriver(target, name=None)
+    assert isinstance(d, HTTPVideoDriver)
+
+def test_ipvideo_create_with_port(target):
+    r = HTTPVideoStream(target, name=None, url="http://localhost:8080/")
     d = HTTPVideoDriver(target, name=None)
     assert isinstance(d, HTTPVideoDriver)

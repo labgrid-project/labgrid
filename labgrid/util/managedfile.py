labgrid-project/labgrid

commit 18646f748892988f63ea4d95be32c669c68c2d2b

util/managedfile: replace ssh usage for local case
In case we operate on the local machine, there is no need to force the
user to have a setup where they can connect via SSH to localhost.
Instead implement the symlink functionality via pure python os module
functionality.

Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

labgrid/util/managedfile.py
@@ -68,18 +68,23 @@ class ManagedFile:
                     f"{self.rpath}{os.path.basename(self.local_path)}"
                 )
         else:
-            conn = sshmanager.open("localhost")
             self.rpath = os.path.dirname(self.local_path) + "/"
 
         if symlink is not None:
             self.logger.info("Linking")
-            try:
-                conn.run_check(f"test ! -e {symlink} -o -L {symlink}")
-            except ExecutionError:
-                raise ManagedFileError(f"Path {symlink} exists but is not a symlink.")
-            # use short options to be compatible with busybox
-            # --symbolic --force --no-dereference
-            conn.run_check(f"ln -sfn {self.rpath}{os.path.basename(self.local_path)} {symlink}")
+            if isinstance(self.resource, NetworkResource):
+                try:
+                    conn.run_check(f"test ! -e {symlink} -o -L {symlink}")
+                except ExecutionError:
+                    raise ManagedFileError(f"Path {symlink} exists but is not a symlink.")
+                # use short options to be compatible with busybox
+                # --symbolic --force --no-dereference
+                conn.run_check(f"ln -sfn {self.rpath}{os.path.basename(self.local_path)} {symlink}")
+            else:
+                if os.path.exists(symlink) and not os.path.islink(symlink):
+                    raise ManagedFileError(f"Path {symlink} exists but is not a symlink.")
+                os.symlink(f"{self.rpath}{os.path.basename(self.local_path)}", symlink)
+
 
     def _on_nfs(self, conn):
         if self._on_nfs_cached is not None:

tests/test_util.py
@@ -354,9 +354,7 @@ Test
     assert str(t) == mf.get_remote_path()
 
 def test_network_managedfile_no_sync(target, tmpdir):
-    import hashlib
-
-    res = NetworkResource(target, "localhost", "test")
+    res = NetworkResource(target, "test", "localhost")
     t = tmpdir.join("test")
     t.write(
 """
@@ -369,6 +367,33 @@ Test
     with pytest.raises(ManagedFileError, match=expected) as e:
         mf.get_remote_path()
 
+def test_local_managedfile_symlink(target, tmpdir):
+    res = Resource(target, "test")
+    t = tmpdir.join("test")
+    t.write(
+"""
+Test
+"""
+    )
+    mf = ManagedFile(t, res, detect_nfs=False)
+    mf.sync_to_resource(symlink=tmpdir.join("link"))
+
+    assert str(t) == mf.get_remote_path()
+    assert os.path.islink(tmpdir.join("link"))
+
+@pytest.mark.localsshmanager
+def test_remote_managedfile_symlink(target, tmpdir):
+    res = NetworkResource(target, "test", "localhost")
+    t = tmpdir.join("test")
+    t.write(
+"""
+Test
+"""
+    )
+    mf = ManagedFile(t, res, detect_nfs=False)
+    mf.sync_to_resource(symlink=tmpdir.join("link"))
+
+    assert os.path.islink(tmpdir.join("link"))
 
 def test_find_dict():
     dict_a = {"a": {"a.a": {"a.a.a": "a.a.a_val"}}, "b": "b_val"}

commit e0575975912e04c95413e81a89d3a9516a74fbb5

util/managedfile: fix symlink creation for local case
When a HTTPProvider driver is used a second time the link to the file
already exists, so instead of just creating it we must replace it as
done with the ln -f flag in the remote case. In the local case we
currently get a FileExistsError when creating the link over an already
existing one. Fix this by removing the old link before re-creating it.

Fixes: 18646f74 ("util/managedfile: replace ssh usage for local case")
Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

labgrid/util/managedfile.py
@@ -81,7 +81,9 @@ class ManagedFile:
                 # --symbolic --force --no-dereference
                 conn.run_check(f"ln -sfn {self.rpath}{os.path.basename(self.local_path)} {symlink}")
             else:
-                if os.path.exists(symlink) and not os.path.islink(symlink):
+                if os.path.islink(symlink):
+                    os.unlink(symlink)
+                elif os.path.exists(symlink):
                     raise ManagedFileError(f"Path {symlink} exists but is not a symlink.")
                 os.symlink(f"{self.rpath}{os.path.basename(self.local_path)}", symlink)
 